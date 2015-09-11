2eff689f86adaad1fedd66b8883c191185068add
Wesley Moore <wmoore@jlab.org>
2015-09-11 10:30:11 -0400

    scalers: working Joerger test, classc4

---
+++ apps/iocBoot/iocscalerTest/st.cmd
@@ -0,0 +1,6 @@
+## Example vxWorks startup file
+
+## The following is needed if your board support package doesn't at boot time
+## automatically cd to the directory containing its startup script
+cd "/home/wmoore/workspaces/clas12-import-dev/apps/iocBoot/iocscalerTest"
+

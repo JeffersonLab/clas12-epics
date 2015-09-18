353eb93d22f4a11b35b6373bd5d47418a780166c
Wesley Moore <wmoore@jlab.org>
2015-09-18 17:32:53 -0400

    iocclassc4: working replacement of HPS classc4

---
+++ apps/iocBoot/iocclassc4/save_restore.cmd
@@ -0,0 +1,26 @@
+## General settings
+save_restoreSet_status_prefix("iocclassc4:")
+save_restoreSet_Debug(0)
+save_restoreSet_IncompleteSetsOk(1)
+save_restoreSet_DatedBackupFiles(1)
+save_restoreSet_NumSeqFiles(3)
+save_restoreSet_SeqPeriodInSeconds(30)
+
+## Specify paths
+set_savefile_path(iocdata,     "autosave/iocclassc4")
+
+set_requestfile_path(startup,  "")
+set_requestfile_path(top,      "req")
+set_requestfile_path(autosave, "asApp/Db")
+set_requestfile_path(calc,     "calcApp/Db")
+set_requestfile_path(mca,      "mcaApp/Db")
+set_requestfile_path(sscan,    "sscanApp/Db")
+set_requestfile_path(std,      "stdApp/Db")
+
+## Restore passes
+set_pass0_restoreFile("info_positions.sav")
+set_pass0_restoreFile("info_settings.sav")
+set_pass1_restoreFile("info_settings.sav")
+set_pass1_restoreFile("joerger_classc4_settings.sav")
+set_pass1_restoreFile("sixty_hz_settings.sav")
+

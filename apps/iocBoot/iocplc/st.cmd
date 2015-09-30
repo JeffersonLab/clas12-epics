6218711f87fc5f010ea0295c0be74ac798fd5369
Wesley Moore <wmoore@jlab.org>
2015-09-30 10:13:36 -0400

    plcApp: added additional L5X for testing

--- apps/iocBoot/iocplc/st.cmd
+++ apps/iocBoot/iocplc/st.cmd
@@ -3,7 +3,8 @@
 < envPaths
 
 # This is a prefix for all PVs on this IOC in case we want a second instance
-epicsEnvSet("PREF","HPS_SVT:PLC")
+#epicsEnvSet("PREF","HPS_SVT:PLC")
+epicsEnvSet("PREF","TEST:PLC")
 
 cd ${TOP}
 
@@ -14,25 +15,30 @@ plc2epics_registerRecordDeviceDriver pdbbase
 # Initialize EtherIP driver, define PLCs
 EIP_buffer_limit(450)
 drvEtherIP_init()
-drvEtherIP_define_PLC("${PREF}", "hpsplc", 0)
+#drvEtherIP_define_PLC("${PREF}", "hpsplc", 0)
+drvEtherIP_define_PLC("${PREF}", "129.57.36.159", 0)
 
 ## Load record instances
-dbLoadRecords("db/iocAdminSoft.db","IOC=iocsvtPlc")
-dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")
-dbLoadRecords("db/HPS_SVT_Interlock_v5_PLCin.db","IOC=${PREF}:i,PLCID=${PREF}")
-dbLoadRecords("db/HPS_SVT_Interlock_v5_PLCout.db","IOC=${PREF}:o,PLCID=${PREF}")
+#dbLoadRecords("db/iocAdminSoft.db","IOC=iocsvtPlc")
+#dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")
+#dbLoadRecords("db/HPS_SVT_Interlock_v5_PLCin.db","IOC=${PREF}:i,PLCID=${PREF}")
+#dbLoadRecords("db/HPS_SVT_Interlock_v5_PLCout.db","IOC=${PREF}:o,PLCID=${PREF}")
+dbLoadRecords("db/TestLogic_v2_PLCarr.db","IOC=${PREF}:i,PLCID=${PREF}")
+dbLoadRecords("db/TestLogic_v2_PLCin.db","IOC=${PREF}:i,PLCID=${PREF}")
+dbLoadRecords("db/TestLogic_v2_PLCout.db","IOC=${PREF}:o,PLCID=${PREF}")
+
 
 cd ${TOP}/iocBoot/${IOC}
 
 ## autosave setup
-< save_restore.cmd
+#< save_restore.cmd
 
 iocInit
 
 # autosave startup
-create_monitor_set("HPS_SVT_Interlocks.req", 30, "PREF=${PREF}")
+#create_monitor_set("HPS_SVT_Interlocks.req", 30, "PREF=${PREF}")
 
 # Handle autosave 'commands' contained in loaded databases.
-makeAutosaveFiles()
-create_monitor_set("info_positions.req", 5, "P=xxx:")
-create_monitor_set("info_settings.req", 30, "P=xxx:")
+#makeAutosaveFiles()
+#create_monitor_set("info_positions.req", 5, "P=xxx:")
+#create_monitor_set("info_settings.req", 30, "P=xxx:")

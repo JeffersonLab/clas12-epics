70265236b496f3c6a8842c3df9d29e50a7b11289
baltzell <baltzell@gmx.com>
2015-11-24 20:36:59 -0500

    changing underscore to colon for final pv naming scheme

--- apps/caenHvApp/op/opi/board_on.js
+++ apps/caenHvApp/op/opi/board_on.js
@@ -7,8 +7,8 @@ var nchan = widget.getMacroValue("NCHAN");
 
 for (var chan=0; chan<nchan; chan++)
 {
-  if (argh == 1) { pv = "B_" + crate + "_HV000_" + slot + "_" + chan + "_pwonoff"; }
-  else           { pv = "B_" + crate + "_Sl" + slot + "_Ch" + chan + "_pwonoff"; }
-  //var pv = "B_" + crate + "_Sl" + slot + "_Ch" + chan + "_pwonoff";
+  if (argh == 1) { pv = "B_" + crate + "_HV000_" + slot + "_" + chan + ":pwonoff"; }
+  else           { pv = "B_" + crate + "_Sl" + slot + "_Ch" + chan + ":pwonoff"; }
+  //var pv = "B_" + crate + "_Sl" + slot + "_Ch" + chan + ":pwonoff";
   PVUtil.writePV(pv,1);
 }

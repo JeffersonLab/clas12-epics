3318109bbd1ada8dc3b8f7869513983bd4591547
baltzell <baltzell@gmx.com>
2015-12-14 11:15:39 -0500

    adding MIBDIRS to env in startup script

--- apps/iocBoot/iocftaglv/st.cmd
+++ apps/iocBoot/iocftaglv/st.cmd
@@ -2,6 +2,10 @@
 
 < envPaths
 
+#
+epicsEnvSet("MIBDIRS","$(DEVSNMP)/mibs:/usr/share/snmp/mibs")
+epicsEnvSet("MIBS","ALL")
+
 # just to decrease string size in record:
 epicsEnvSet("MIB","WIENER_CRATE-MIB::")
 epicsEnvSet("WO","WIENER-CRATE-MIB::output")

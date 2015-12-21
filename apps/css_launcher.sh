ed5e477968a9bd2a68b7e5834535203bc9c9a162
baltzell <baltzell@gmx.com>
2015-12-21 18:58:02 -0500

    enable splash screen, else editor does not work correctly

--- apps/css_launcher.sh
+++ apps/css_launcher.sh
@@ -20,6 +20,7 @@ fi
 echo Running css ...
 css \
     -pluginCustomization $css_share_path/common/prefs/plugin_customization.ini \
-	-share_link $css_share_path=/CLAS12_Share \
-	-nosplash &
+	-share_link $css_share_path=/CLAS12_Share
+#\
+#	-nosplash &
 

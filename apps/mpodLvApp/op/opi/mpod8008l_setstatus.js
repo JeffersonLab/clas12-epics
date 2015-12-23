208562bddcebd7f00f4e21c9c4aeed08c63f6691
baltzell <baltzell@gmx.com>
2015-12-23 17:39:56 -0500

    removing debug printout

--- apps/mpodLvApp/op/opi/mpod8008l_setstatus.js
+++ apps/mpodLvApp/op/opi/mpod8008l_setstatus.js
@@ -2,7 +2,7 @@ importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
 
 var status = PVUtil.getString(pvs[0]);
 
-java.lang.System.err.println("mpodstatus:  "+pvs[0]+"   a"+status+"a");
+//java.lang.System.err.println("mpodstatus:  "+pvs[0]+"   a"+status+"a");
 
 if (status == "00 ") {
     widget.setPropertyValue("text","OFF");

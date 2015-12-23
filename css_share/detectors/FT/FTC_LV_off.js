4382624188d9182e6a6f559607222dd4f5a20f7d
baltzell <baltzell@gmx.com>
2015-12-23 17:41:45 -0500

    FT.opi

---
+++ css_share/detectors/FT/FTC_LV_off.js
@@ -0,0 +1,15 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+var prefix = "B_DET_FT_LV_";
+var qg=["Q1Q4","Q2Q3"];
+var pn=["P","N"];
+
+for (var iqg=0; iqg<2; iqg++)
+{
+    for (var ipn=0; ipn<2; ipn++)
+    {
+      pv = prefix + qg[iqg] + pn[ipn] + ":switch";
+        //java.lang.System.out.println(pv);
+        PVUtil.writePV(pv,0);
+  }
+}

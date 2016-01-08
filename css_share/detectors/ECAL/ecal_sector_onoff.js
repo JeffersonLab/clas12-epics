2dddc96a7a2e3a56ff5ab1e3af14f79bf33f131d
baltzell <baltzell@gmx.com>
2016-01-07 20:30:23 -0500

    moving this out of caenhv to a more appropriate place

---
+++ css_share/detectors/ECAL/ecal_sector_onoff.js
@@ -0,0 +1,22 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+var onoff = widget.getMacroValue("ONOFF");
+var sec   = widget.getMacroValue("SECTOR");
+
+var io = ["I","O"];
+var uvw = ["U","V","W"];
+
+for (var iuvw=0; iuvw<uvw.length; iuvw++)
+{
+  if (iuvw<10) iuivw="0"+iuvw;
+  for (var iio=0; iio<io.length; iio++)
+  {
+    if (iio<10) iio="0"+iio;
+    for (var chan=1; chan<=36; chan++)
+    {
+      if (chan<10) chan="0"+chan;
+      var pv = B_HV_ECAL_Sec + sec + "_" + uvw[iuvw] + io[iio] + chan;
+      PVUtil.writePV(pv,onoff);
+    }
+  }
+}

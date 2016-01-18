38b675992761c2cf99b733bd04dd2e8ad13899d1
baltzell <baltzell@gmx.com>
2016-01-18 17:33:20 -0500

    changing HV backup script to use burt

--- bin/hvbackup.py
+++ bin/hvbackup.py
@@ -2,56 +2,79 @@
 import pygtk,gtk
 import sys,time,os,commands
 import getpass,grp,pwd
+import subprocess,re
 import epics
-
-FIELDS=[':vset',':iset',':trip',':rup',':rdn','.NAME']
-
-DATADIR='/usr/clas12/DATA/hvbackup'
 
 def exit(text):
   mess = gtk.MessageDialog(buttons=gtk.BUTTONS_OK)
   mess.set_markup(text)
   mess.run()
   mess.destroy()
-  sys.exit(text)
+#  sys.exit(text)
 
-def getChannels(det,sector):
+DETS=['CTOF_HV','FTOF_HV','ECAL_HV','PCAL_HV','FTC_HV']
+FIELDS=[':vset',':vmax',':iset',':trip',':rup',':rdn']
+DATADIR='/usr/clas12/DATA/burt'
+SCRIPTPATH=os.path.dirname(os.path.realpath(__file__))
+RELEASEPATH=re.search('(^.*/release/\d[\.\d]+)',SCRIPTPATH)
+if RELEASEPATH==None: exit('Cannot Find hvbackup.py Path')
+RELEASEPATH=RELEASEPATH.group(1)
+REQDIR=RELEASEPATH+'/epics/apps/scripts/hvburt/req'
+if not os.path.exists(REQDIR): exit('Missing REQDIR:  '+REQDIR)
+
+def getChannels(det,sector=None):
   prefixes=[]
   sectors=[]
   for ss in range(1,7):
-    if sector==None or ss==sector:
+    if sector not in [1,2,3,4,5,6] or ss==sector:
       sectors.append(ss)
-  if det=='CTOF':
+  if det=='CTOF_HV':
     for ud in ['U','D']:
       for ii in range(48):
         prefixes.append('B_DET_CTOF_HV_'+ud+'%.2d'%(ii+1))
-  elif det=='FTOF':
+  elif det=='FTOF_HV':
     nn={'1A':23,'1B':62,'2':5}
     for ss in sectors:
       for pp in ['1A','1B','2']:
         for lr in ['L','R']:
           for ii in range(nn[pp]):
             prefixes.append('B_DET_FTOF_HV_SEC%d_PANEL%s_%s_E%.2d'%(ss,pp,lr,ii+1))
-  elif det=='ECAL':
+  elif det=='ECAL_HV':
     for ss in sectors:
       for uvw in ['U','V','W']:
         for io in ['I','O']:
           for ii in range(36):
             prefixes.append('B_DET_ECAL_HV_SEC%d_%s%s_E%.2d'%(ss,uvw,io,ii+1))
-  elif det=='PCAL':
+  elif det=='PCAL_HV':
     nn={'U':68,'V':62,'W':62}
     for ss in sectors:
       for uvw in ['U','V','W']:
         for ii in range(nn[uvw]):
           prefixes.append('B_DET_PCAL_HV_SEC%d_%s_E%.2d'%(ss,uvw,ii+1))
-  elif det=='FTC':
+  elif det=='FTC_HV':
     for qq in range(4):
       for gg in range(9):
         prefixes.append('B_DET_FTC_HV_Q%dG%d'%(qq+1,gg+1))
   return prefixes
 
+def printPVs(det,sector=None):
+  for channel in getChannels(det,sector):
+    for field in FIELDS:
+      print channel+field
 
+def saveBurt(snpFilename,det,sector=None):
+  reqFilename=REQDIR+'/'+det+'.req'
+  if not os.path.exists(reqFilename): exit('Missing burt REQ file:  '+reqFilename)
+  burtopts='-f '+reqFilename+' -o '+snpFilename
+  p = subprocess.Popen(['burtrb', burtopts], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+  out, err = p.communicate()
+  return [out,err]
 
+def restoreBurt(snpFilename):
+  burtopts='-f '+snpFilename
+  p = subprocess.Popen(['burtwb', burtopts], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+  out, err = p.communicate()
+  return [out,err]
 
 class SaveRestore:
 
@@ -60,7 +83,7 @@ class SaveRestore:
   progressBar = None
   filename = None
 
-  def oldBackup(self,det):
+  def chooseOldBackup(self,det):
     chooser = gtk.FileChooserDialog(
       title='RESTORE HV BACKUP',
       parent=None,
@@ -74,16 +97,13 @@ class SaveRestore:
     chooser.set_current_folder(DATADIR+'/'+det)
     self.filename = None
     response = chooser.run()
-    if response == gtk.RESPONSE_OK:
-      self.filename = chooser.get_filename()
+    if response == gtk.RESPONSE_OK: self.filename = chooser.get_filename()
     chooser.destroy()
-    if self.filename==None:
-      exit('RESTORE CANCELLED.')
-    if not os.path.exists(self.filename):
-      exit('FILE D.N.E.\n\nRESTORE CANCELLED.')
+    if self.filename==None: exit('RESTORE CANCELLED.')
+    if not os.path.exists(self.filename): exit('FILE D.N.E.\n\nRESTORE CANCELLED.')
     return self.filename
 
-  def newBackup(self,det,sec):
+  def chooseNewBackup(self,det,sec):
     win = gtk.Window(gtk.WINDOW_TOPLEVEL)
     win.set_default_size(400,100)
     win.set_title('CREATE HV BACKUP')
@@ -98,7 +118,7 @@ class SaveRestore:
     box.pack_start(text2)
     text.set_markup(DATADIR+'/'+det+'\n')
     box.pack_start(text)
-    basename = time.strftime(det+'-%Y_%m_%d-%H_%M_%S.txt')
+    basename = time.strftime(det+'-%Y_%m_%d-%H_%M_%S.snp')
     self.entry.set_text(basename)
     box.pack_start(self.entry,True,True,0)
     self.entry.show()
@@ -150,7 +170,15 @@ class SaveRestore:
     for xx in lines: print >> file, xx
     file.close()
     os.chmod(filename,0444) # set it unwriteable
-    exit('BACKEDUP VOLTAGES TO:\n\n'+filename)
+    exit('BACKEDUP SETTINGS TO:\n\n'+filename)
+
+  def saveBurt(self,snpFilename,det,sector):
+    [out,err]=saveBurt(snpFilename,det,sector)
+    exit('BACKEDUP SETTINGS TO:\n\n'+snpFilename)
+
+  def restoreBurt(self,snpFilename):
+    [out,err]=restoreBurt(snpFilename)
+    exit('RESTORED SETTINGS FROM\n\n'+snpFilename)
 
   def restore(self,filename):
     caputs=[]
@@ -176,29 +204,39 @@ class SaveRestore:
 #      gtk.main_iteration()
     #for xx in caputs:
       #epics.caput(xx[0],xx[1])
-    exit('RESTORED '+str(len(lines))+' VOLTAGES FROM:\n\n'+filename)
+    exit('RESTORED '+str(len(lines))+' SETTINGS FROM:\n\n'+filename)
 
 def main():
 
   scriptName=sys.argv.pop(0)
-  usage=scriptName+' [sector=#] det=detectorName save/restore'
+  usage=scriptName+' [sec=S#] det=detectorName save/restore'
 
   det=None
   sector=None
   saverestore=None
-
-  for arg in sys.argv:
-    if arg=='-h' or arg=='--help': sys.exit(usage)
+  printOnly=False
 
   for arg in sys.argv[:]:
-    if arg.find('=')<0: continue
-    (key,val)=arg.split('=',1)
-    if   key=='det':    det=val
-    elif key=='sector': sector=val
-    else: sys.exit(usage)
-    sys.argv.remove(arg)
+    if arg=='-h' or arg=='--help':
+      sys.exit(usage)
+    elif arg=='-p':
+      printOnly=True
+      sys.argv.remove(arg)
+    elif arg.find('=')>=0:
+      (key,val)=arg.split('=',1)
+      if   key=='det': det=val
+      elif key=='sec': sector=val
+      else: sys.exit(usage)
+      sys.argv.remove(arg)
 
+  if sector != None: sector=sector.replace('S','')
   if (det==None): sys.exit(usage)
+  if (det not in DETS): sys.exit(usage)
+
+  if printOnly:
+    printPVs(det)
+    sys.exit()
+
   if len(sys.argv)!=1: sys.exit(usage)
 
   saverestore=sys.argv.pop()
@@ -206,17 +244,17 @@ def main():
   if saverestore!='save' and saverestore!='restore': sys.exit(usage)
 
   user = pwd.getpwuid(os.getuid())[0]
-  if os.getgid() != grp.getgrnam('clas-3').gr_gid:
-      exit('BACKUPS REQUIRE MEMBERSHIP IN GROUP clas-3.\n\n'+user+' IS NOT A MEMBER.\n\nEXITING.')
+  if os.getgid() != grp.getgrnam('onliners').gr_gid:
+      exit('BACKUPS REQUIRE MEMBERSHIP IN GROUP onliners.\n\n'+user+' IS NOT A MEMBER.\n\nEXITING.')
 
   backup = SaveRestore()
 
   if saverestore=='save':
-    filename = backup.newBackup(det,sector)
-    backup.save(filename,det,sector)
+    filename = backup.chooseNewBackup(det,sector)
+    backup.saveBurt(filename,det,sector)
   elif saverestore=='restore':
-    filename = backup.oldBackup(det)
-    if filename != None: backup.restore(filename)
+    filename = backup.chooseOldBackup(det)
+    if filename != None: backup.restoreBurt(filename)
 
 
 if __name__ == '__main__': main()

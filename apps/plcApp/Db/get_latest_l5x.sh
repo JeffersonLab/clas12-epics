6aa9fad18b09de5f264dca90aaa769754ec7a6b8
Wesley Moore <wmoore@jlab.org>
2016-03-22 11:42:15 -0400

    plcApp: rm'd L5X files, not being used to generate db's

--- apps/plcApp/Db/get_latest_l5x.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/jeffersonlab/clas12-plc
-printf "=%.0s" $(seq 1 `tput cols`)
-printf "Copying found L5X's...\n"
-find clas12-plc -name "*L5X" -exec cp -v {} . \;
-rm -rf clas12-plc
-

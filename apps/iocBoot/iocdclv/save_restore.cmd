89d194f65b32f8f3c54b962c8bb0dbeb70ef1dc6
baltzell <baltzell@gmx.com>
2016-03-28 08:48:36 -0400

    iocdclv: add save_restore.cmd

---
+++ apps/iocBoot/iocdclv/save_restore.cmd
@@ -0,0 +1,19 @@
+  ## General settings
+  save_restoreSet_status_prefix("$(IOC):")
+  
+  save_restoreSet_Debug(0)
+  save_restoreSet_IncompleteSetsOk(1)
+  save_restoreSet_DatedBackupFiles(1)
+  save_restoreSet_NumSeqFiles(3)
+  save_restoreSet_SeqPeriodInSeconds(30)
+
+  ## Specify paths
+  set_savefile_path("$(IOCDATA)/autosave/$(IOC)")
+  set_requestfile_path(".")
+  set_requestfile_path("$(TOP)/req")
+
+  ## Restores
+  #set_pass0_restoreFile("info_positions.sav")
+  #set_pass1_restoreFile("info_settings.sav")
+
+  set_pass1_restoreFile("iocdclv_settings.sav")

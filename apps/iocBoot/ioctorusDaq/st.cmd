93e59d78922d7d5a6eb518171a27ee0f9d544dd9
Wesley Moore <wmoore@jlab.org>
2016-03-28 11:42:22 -0400

    ioctorusDaq/wf2rootApp: updated db names and added autosave

--- apps/iocBoot/ioctorusDaq/st.cmd
+++ apps/iocBoot/ioctorusDaq/st.cmd
@@ -5,16 +5,26 @@
 cd "${TOP}"
 
 ## Register all support components
-dbLoadDatabase("dbd/wf2root.dbd"(
+dbLoadDatabase("dbd/wf2root.dbd")
 wf2root_registerRecordDeviceDriver(pdbbase)
 
 ## Load record instances
+dbLoadRecords("${DEVIOCSTATS}/db/iocAdminSoft.db","IOC=${IOC}")
+dbLoadRecords("${AUTOSAVE}/asApp/Db/save_restoreStatus.db", "P=${IOC}:")
+#
 dbLoadTemplate("iocBoot/${IOC}/wf2root.substitutions")
 
 cd "${TOP}/iocBoot/${IOC}"
+
+## autosave setup
+< save_restore.cmd
+
 iocInit
 
-#dbpf("B_TORUS:DAQ_REC:ctrl:daq.INPC", "torus")
-#dbpf("B_TORUS:DAQ_REC:ctrl:file_limit", "500")
-dbpf("B_TORUS:DAQ_REC:ctrl:daq_state", "0")
+## autosave startup
+## Handle autosave 'commands' contained in loaded databases.
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+create_monitor_set("info_settings.req", 30, "P=${IOC}:")
+create_monitor_set("wf2root_settings.req", 30, "P=B_TORUS:DAQ_REC:")
 

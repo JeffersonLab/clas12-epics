3c5bed6bcec725311fc34ba7ca89c8e5928458f1
Wesley Moore <wmoore@jlab.org>
2016-04-15 16:02:32 -0400

    torus: added IOC for testing load cells and strain gages

---
+++ apps/iocBoot/ioctorusMag/st.cmd
@@ -0,0 +1,37 @@
+#!../../bin/linux-x86/plc2epics
+############################################################################
+< envPaths
+############################################################################
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase("dbd/plc2epics.dbd")
+plc2epics_registerRecordDeviceDriver(pdbbase)
+
+## Initialize EtherIP driver, define PLCs
+## NOTE: if buffer limit is left at 500, read errors will occur.
+EIP_buffer_limit(450)
+drvEtherIP_init()
+drvEtherIP_define_PLC("PLC_TORUS", "129.57.96.15", 0)
+
+## Debugging [7-10]
+#EIP_verbosity(7)
+
+## Load record instances
+dbLoadRecords("${DEVIOCSTATS}/db/iocAdminSoft.db","IOC=${IOC}")
+dbLoadRecords("${AUTOSAVE}/asApp/Db/save_restoreStatus.db", "P=${IOC}:")
+dbLoadTemplate("db/torus_mag.substitutions")
+
+cd ${TOP}/iocBoot/${IOC}
+
+## autosave setup
+< save_restore.cmd
+
+iocInit
+
+## autosave startup
+## Handle autosave 'commands' contained in loaded databases.
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5, "P=xxx:")
+create_monitor_set("info_settings.req", 30, "P=xxx:")
+

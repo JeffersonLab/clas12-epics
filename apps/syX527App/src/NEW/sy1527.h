cbe7cd5d02377d798b1b5ed7f8584cf445dd8e00
baltzell <baltzell@gmx.com>
2016-05-19 10:30:27 -0400

    syX57: prepping for merger

---
+++ apps/syX527App/src/NEW/sy1527.h
@@ -0,0 +1,239 @@
+
+/* sy1527.h - EPICS driver support for CAEN SY1527 HV mainframe */
+
+/* data structures */
+
+#define MAX_CAEN_NAME  81
+#define MAX_SLOT  16
+#define MAX_CHAN  30
+#define MAX_PARAM 40
+/// my_n_smi:
+#define MAX_BOARDPARTS 24
+
+#define MAX_HVPS        50
+#define CAENHV_OK       0
+#define CAENHV_SYSERR   1
+
+#define LINKTYPE_TCPIP              0
+#define LINKTYPE_RS232              1
+#define LINKTYPE_CAENET             2
+
+#define PARAM_TYPE_NUMERIC          0
+#define PARAM_TYPE_ONOFF            1
+#define PARAM_TYPE_CHSTATUS         2
+#define PARAM_TYPE_BDSTATUS         3
+
+///#define CRATE_PREFIX "sy1527_%03d"  /// my:
+#ifdef USE_CAEN527
+#define CRATE_LABEL "B_DCRB_HV%03d" /// my:
+#else
+#define CRATE_LABEL "B_HV%03d" /// my:
+#endif
+
+/// statuses: my: smi 
+/// Channel is on
+#define BIT_ON 1 
+/// 1 Channel is ramping up
+#define BIT_RAMPUP 1 << 1 
+/// 2 Channel is ramping down
+#define BIT_RAMPDOWN 1 << 2
+/// 3 Channel is in overcurrent
+#define BIT_OVERCUR 1 << 3 
+/// 4 Channel is in overvoltage
+#define BIT_OVERVOLT 1 << 4 
+/// 5 Channel is in undervoltage
+#define BIT_UNDERVOLT 1 << 5 
+/// 6 Channel is in external trip
+#define BIT_EXTTRIP 1 << 6 
+/// 7 Channel is in max V
+#define BIT_MAXVOLT 1 << 7 
+/// 8 Channel is in external disable
+#define BIT_EXTDISABLED 1 << 8 
+/// 9 Channel is in internal trip
+#define BIT_INTTRIP 1 << 9 
+/// 10 Channel is in calibration error
+#define BIT_CALIBERROR 1 << 10 
+/// 11 Channel is unplugged
+#define BIT_CHUNPLAGGED 1 << 11
+/// my: 
+#define BIT_OFF 1 << 11
+/// my_n_smi:
+#define BIT_CRATE_OFF 1 << 12
+#define BIT_BOARD_NOT_PRESENT 1 << 13 
+#define BIT_CRATE_OFF_ON_WAY 1 << 14
+
+/// 12 ... 31 Reserved, forced to 0
+
+
+
+///==============================================================================
+int  CAENHVInitSystem(const char *SystemName, int LinkType, void *Arg,
+                      const char *UserName, const char *Passwd);
+
+int  CAENHVDeinitSystem(const char *SystemName);
+
+char *CAENHVGetError(const char *SystemName);
+
+int  CAENHVGetCrateMap(const char *SystemName, ushort *NrOfSlot,
+                      ushort **NrofChList, char **ModelList,
+                      char **DescriptionList, ushort **SerNumList,
+                      unsigned char **FmwRelMinList, 
+                      unsigned char **FmwRelMaxList);
+
+int  CAENHVGetChParam(const char *SystemName, ushort slot, const char *ParName,
+                      ushort ChNum, const ushort *ChList, void *ParValList);
+
+int  CAENHVSetChParam(const char *SystemName, ushort slot, const char *ParName,
+                      ushort ChNum, const ushort *ChList, void *ParValue);
+
+int  CAENHVGetChParamProp(const char *SystemName, ushort slot, ushort Ch,
+                      const char *ParName, const char *PropName, void *retval);
+
+int  CAENHVGetSysProp(const char *SystemName, const char *PropName, void *Result);
+
+///==============================================================================
+
+
+
+typedef struct thread
+{
+  int threadid;
+  struct timespec   start;
+  struct timespec   end;
+} THREAD;
+
+
+typedef struct channel
+{
+  char          name[MAX_CAEN_NAME];
+  float         fval[MAX_PARAM];
+  unsigned long lval[MAX_PARAM];
+  int           setflag[MAX_PARAM]; /* if 1, need to write */
+} CHANNEL;
+
+typedef struct board
+{
+  int           nchannels;
+  char          modelname[MAX_CAEN_NAME];
+  char          description[MAX_CAEN_NAME];
+  int           sernum;
+  int           relmax;
+  int           relmin;
+  int           nparams;
+  char          parnames[MAX_PARAM][MAX_CAEN_NAME];
+  unsigned long partypes[MAX_PARAM];
+  CHANNEL       channel[MAX_CHAN];
+  int           setflag; /* if 1, need to write */
+} BOARD;
+
+typedef struct sys
+{
+  int   id;
+  char  name[MAX_CAEN_NAME];
+  int   nslots;
+  BOARD board[MAX_SLOT];
+  int   setflag; /* if 1, need to write */
+  char  IPADDR[MAX_CAEN_NAME]; /// my:
+} HV;
+
+int boards_status[MAX_HVPS][MAX_SLOT][MAX_BOARDPARTS]; /// my: smi temporal: should be dynamic
+
+
+/* functions */
+
+int
+sy1527Measure2Demand(unsigned int id, unsigned int board);
+int
+sy1527GetBoard(unsigned int id, unsigned int board);
+int
+sy1527SetBoard(unsigned int id, unsigned int board);
+int
+sy1527GetMap(unsigned int id);
+int
+sy1527PrintMap(unsigned int id);
+void *
+sy1527MainframeThread(void *arg);
+int
+sy1527Init();
+int
+sy1527Start(unsigned id, char *ip_address);
+int
+sy1527Stop(unsigned id);
+
+
+int
+sy1527SetMainframeOnOff(unsigned int id, unsigned int on_off);
+
+int /// my_n: adding heart beat
+sy1527GetHeartBeat(unsigned int id, unsigned int board,
+                           unsigned int chan);
+
+int /// my: smi
+sy1527SetBoardOnOff(unsigned int id, unsigned int slot, unsigned int on_off); 
+
+int sy1527BoardSmiMonitor ///  my: smi
+(char *smi_obj_name, unsigned int id, unsigned int board, unsigned int first_channel, unsigned int ch_numbers);
+
+int sy1527CrateSmiInit ///  my: smi  my_n_smi
+(char *smi_obj_name, unsigned int id);
+
+int sy1527BoardSmiControl ///  my: smi
+(char *smi_obj_name, unsigned int id, unsigned int first_board,
+unsigned int first_channel, unsigned int ch_numbers, unsigned int onoff);
+
+int
+sy1527GetMainframeStatus(unsigned int id, int *active, int *onoff, int *alarm);
+
+int
+sy1527SetChannelDemandVoltage(unsigned int id, unsigned int board,
+                              unsigned int chan, float u);
+float
+sy1527GetChannelDemandVoltage(unsigned int id, unsigned int board,
+                              unsigned int chan);
+int
+sy1527SetChannelMaxVoltage(unsigned int id, unsigned int board,
+                           unsigned int chan, float u);
+float
+sy1527GetChannelMaxVoltage(unsigned int id, unsigned int board,
+                           unsigned int chan);
+int
+sy1527SetChannelMaxCurrent(unsigned int id, unsigned int board,
+                           unsigned int chan, float u);
+float
+sy1527GetChannelMaxCurrent(unsigned int id, unsigned int board,
+                           unsigned int chan);
+float
+sy1527GetChannelMeasuredVoltage(unsigned int id, unsigned int board,
+                                unsigned int chan);
+float
+sy1527GetChannelMeasuredCurrent(unsigned int id, unsigned int board,
+                                unsigned int chan);
+int
+sy1527SetChannelRampUp(unsigned int id, unsigned int board, unsigned int chan,
+                       float u);
+float
+sy1527GetChannelRampUp(unsigned int id, unsigned int board, unsigned int chan);
+int
+sy1527SetChannelRampDown(unsigned int id, unsigned int board,
+                         unsigned int chan, float u);
+float
+sy1527GetChannelRampDown(unsigned int id, unsigned int board,
+                         unsigned int chan);
+int
+sy1527SetChannelOnOff(unsigned int id, unsigned int board,
+                      unsigned int chan, unsigned int u);
+unsigned int
+sy1527GetChannelOnOff(unsigned int id, unsigned int board,
+                      unsigned int chan);
+int
+sy1527SetChannelEnableDisable(unsigned int id, unsigned int board,
+                              unsigned int chan, unsigned int u);
+unsigned int
+sy1527GetChannelEnableDisable(unsigned int id, unsigned int board,
+                              unsigned int chan);
+unsigned int
+sy1527GetChannelStatus(unsigned int id, unsigned int board,
+                       unsigned int chan);
+float
+sy1527GetChannelTripTime(unsigned int id, unsigned int board,
+                         unsigned int chan);
---
+++ apps/syX527App/src/NEW/sy527_test.c
@@ -0,0 +1,149 @@
+
+/* sy1527_test.c - generic test for sy1527 driver */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define UINT32 unsigned int
+#define UINT16 unsigned short
+#define UINT8 unsigned char
+
+#include "v288.h"
+#include "sy1527.h"
+
+int
+main()
+{
+  int i, id = 0, board = 0, channel = 2;
+  char *ip_address="sy527_0x100000_3";
+
+  float u, uset, uget;
+  unsigned int l, lget, active, onoff, alarm, itmp;
+
+  printf("\n\n=============== CAEN mainframe SY1527 test ===============\n");
+  printf("===== ip_address= 129.57.69.101, mainframe number 5 =====\n\n\n");
+
+  sy1527Start(id, ip_address);
+
+  /*
+  while(1){};
+  */
+
+  sy1527PrintMap(0);
+
+  /*
+  return 1;
+  */
+
+  sy1527GetBoard(id, board);
+
+  sy1527Measure2Demand(id, board);
+
+  sy1527SetBoard(id, board);
+
+  
+  printf("sleep1 ..\n");fflush(stdout);
+  sleep(3);
+
+  printf("==> 0x%08x\n",sy1527GetChannelStatus(id, board, channel));
+
+  u = 9.8;
+  l = 1;
+
+
+
+printf("--> id=%d board=%d channel=%d\n",id,board,channel);
+
+
+printf("!!!!!\n");
+sy1527SetChannelEnableDisable(id, board, channel, 1);
+printf("!!!!!\n");
+sleep(5);
+
+printf("0: itmp=%d\n",itmp);
+
+itmp=sy1527GetChannelEnableDisable(id, board, channel);
+
+printf("1: itmp=%d\n",itmp);
+
+
+
+
+sleep(5);
+exit(0);
+
+
+
+
+sy1527SetChannelEnableDisable(id, board, channel, 1);
+itmp=sy1527GetChannelEnableDisable(id, board, channel);
+printf("1: itmp=%d\n",itmp);
+
+sy1527SetChannelEnableDisable(id, board, channel, 0);
+itmp=sy1527GetChannelEnableDisable(id, board, channel);
+printf("2: itmp=%d\n",itmp);
+
+sy1527SetChannelEnableDisable(id, board, channel, 1);
+itmp=sy1527GetChannelEnableDisable(id, board, channel);
+printf("3: itmp=%d\n",itmp);
+
+
+
+
+
+  sy1527SetChannelDemandVoltage(id, board, channel, u);
+  sy1527SetChannelOnOff(id, board, channel, l);
+  printf("==> 0x%08x\n",sy1527GetChannelStatus(id, board, channel));
+  printf("set U=%f OnOff=%u\n",u,l);
+  printf("==> 0x%08x\n",sy1527GetChannelStatus(id, board, channel));
+  sleep(1);
+  printf("==> 0x%08x\n",sy1527GetChannelStatus(id, board, channel));
+  sleep(1);
+  printf("==> 0x%08x\n",sy1527GetChannelStatus(id, board, channel));
+  sleep(1);
+  printf("==> 0x%08x\n",sy1527GetChannelStatus(id, board, channel));
+  sleep(3);
+
+  
+  for(i=0; i<10; i++)
+  {
+    sleep(1);
+    if(i==3) sy1527SetChannelOnOff(id, board, channel, 1);
+    uset = sy1527GetChannelDemandVoltage(id, board, channel);
+    uget = sy1527GetChannelMeasuredVoltage(id, board, channel);
+    lget = sy1527GetChannelOnOff(id, board, channel);
+    printf("got U(set)=%f, U(get)=%f OnOff=%u\n",uset,uget,lget);
+  }
+  sleep(3);
+  
+
+  /*  
+  printf(".. done1\n");fflush(stdout);
+  sy1527SetMainframeOnOff(id, 1);
+  printf("done !\n");fflush(stdout);
+  sleep(3);
+  */
+  sy1527GetMainframeStatus((unsigned int) id, (int *) &active, (int *) &onoff, (int *) &alarm);
+  printf("3status: %d %d %d\n",active, onoff, alarm);
+  sleep(1);
+  sy1527SetMainframeOnOff(id, 0);
+  sleep(3);
+  sy1527GetMainframeStatus((unsigned int) id, (int *) &active, (int *) &onoff, (int *) &alarm);
+  printf("3status: %d %d %d\n",active, onoff, alarm);
+  sleep(1);
+
+  printf("STATUS=[0]=0x%08x\n",sy1527GetChannelStatus(id, 2, 0));
+  printf("STATUS=[1]=0x%08x\n",sy1527GetChannelStatus(id, 2, 1));
+  printf("STATUS=[2]=0x%08x\n",sy1527GetChannelStatus(id, 2, 2));
+  sleep(1);
+  sy1527Stop(id);
+
+  printf("sleep2 ..\n");fflush(stdout);
+  sleep(3);
+  printf(".. done2\n");fflush(stdout);
+
+  exit(0);
+} 

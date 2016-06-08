f031c3a7dc08d1776d056cfb1effd0d0d914f8fb
baltzell <baltzell@gmx.com>
2016-06-08 16:17:17 -0400

    adding StripTool automator script

---
+++ apps/scripts/stripTool.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+if [ -z "$1" ]
+then
+    echo 'Usage: stripTool.sh pv1 [pv2 [pv3 [...]]]'
+    exit
+fi
+
+tmpdir=/tmp/StripTool
+if [[ ! -e $tmpdir ]]; then
+    mkdir -p $tmpdir -m 777
+fi
+
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cfgTemplate=$scriptDir/stripToolTemplate.stp
+
+cfg=`mktemp -p $tmpdir $1.XXXXXXXX`
+
+# same for all PVs:
+grep -v Strip.Curve $cfgTemplate >> $cfg
+
+# PV-specific:
+npv=0
+for pv in $@
+do
+    grep Strip.Curve.0 $cfgTemplate | \
+        sed "s/_____PV_____/$pv/" | \
+        sed "s/Strip.Curve.0/Strip.Curve.$npv/" \
+        >> $cfg
+    let npv=npv+1
+done
+
+exec StripTool $cfg &
+

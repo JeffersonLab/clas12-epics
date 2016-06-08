0a9a0c12c7266510c5a9a0b6be894e050312df2f
Wesley Moore <wmoore@jlab.org>
2016-06-08 09:14:51 -0400

    csv2beast_tree.py: draft using elementtree for xml support

---
+++ apps/plcApp/Db/csv2beast_tree.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+#
+# Convert CSV to XML for BEAST configuration
+#
+# Usage:
+#   csv2beast.py [FILE...] [DIR...]
+#
+#   Excepts combinations of files and directories.  Output file(s) will be the
+#   same as the input, only with xml extension.
+#
+# Input format:
+#   Line 1: <config name>, <component 0>, <component 1>, <component N>, ...
+#   Line 2: column headers (Must start with pv)
+#   Line 3: data (Must start with pv name)
+#
+# Input example:
+#   HallB, Torus, Vacuum
+#   pv, description, latching, annunciating, display title, display details
+#   B_TORUS:FOR:CCM_A:LC817A1, Torus Load Cell, true, true, Open Force GUI, /CLAS12_Share/blah-blah
+#
+# Output example:
+#   <?xml version="1.0"?>
+#   <config name="HallB">
+#	    <component name=" Torus">
+#	    <component name=" Vacuum">
+#   		<pv name="B_TORUS:FOR:CCM_A:LC817A1">
+#   			<description>Torus Load Cell</description>
+#   			<latching>true</latching>
+#   			<annunciating>true</annunciating>
+#               <display>
+#                   <title>Open Force GUI</title>
+#                   <details</CLAS12_Share/blah-blah</details>
+#               </display>
+#   		</pv>
+#       </component>
+#       </component>
+#   </config>
+#
+# Author: Wesley Moore (wmoore@jlab.org)
+# Date:   May 2016
+#
+
+import sys
+import os
+import csv
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+
+csvFiles = []
+
+if len(sys.argv) < 2:
+    csvFiles = [f for f in os.listdir("./") if f.endswith('.csv') or f.endswith('.CSV')]
+else:
+    for arg in sys.argv[1:]:
+        if os.path.isfile(arg):
+            csvFiles.append(arg)
+        elif os.path.isdir(arg):
+            csvFiles = [f for f in os.listdir(arg) if f.endswith('.csv') or f.endswith('.CSV')]
+        else:
+            print "Warning: " + arg + " not found"
+
+if len(csvFiles) == 0:
+    print "No files to be processed"
+    exit(0)
+
+print "Processing files:"
+
+for csvFile in csvFiles:
+    print "  " + csvFile
+    xmlFile = csvFile[:-4] + '.xml'
+    csvData = csv.reader(open(csvFile))
+    try:
+        xmlData = open(xmlFile, 'w')
+        xmlData.write('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n')
+    except IOError:
+        print "Write permission denied: " + xmlFile
+        exit(1)
+
+    rowNum = 0
+    for row in csvData:
+        row = map(str.strip, row)
+        #print row
+        if len(row) == 0:
+            break
+
+        # set config and component levels
+        if rowNum == 0:
+            levels = row
+
+            for i in range(len(levels)):
+                if i == 0:
+                    tree = Element('config', name=levels[i])
+                    xmlData.write('<config name="' + levels[i] + '">\n')
+                    i+=1
+                    comp = SubElement(tree, 'component', name=levels[i])
+                elif levels[i] != "":
+                    comp = SubElement(comp, 'component', name=levels[i])
+                    xmlData.write('\t<component name="' + levels[i] + '">\n')
+
+        # set tags
+        elif rowNum == 1:
+            tags = row
+
+        # fill pv's
+        elif rowNum > 1:
+            vals = row
+            i = 0
+            while i < len(tags):
+                if tags[i] == "pv":
+                    pv = SubElement(comp, 'pv', name=vals[i])
+                    xmlData.write('\t\t<' + tags[i] + ' name="' + vals[i] + '">\n')
+                elif (' ' in tags[i]):
+                    # nested element
+                    elem = tags[i].split()
+                    top  = elem[0]
+                    z = SubElement(pv, elem[0])
+                    z2 = SubElement(z,  elem[1])
+                    z2.text = vals[i]
+                    xmlData.write('\t\t\t<' + elem[0] + '>\n')
+                    xmlData.write('\t\t\t\t<' + elem[1] + '>' + vals[i] + '</' + elem[1] + '>\n')
+                    j = i+1
+                    while j < len(tags):
+                        # search until not a sub-element
+                        elem = tags[j].split()
+                        if elem[0] == top:
+                            z3 = SubElement(z, elem[1])
+                            z3.text = vals[j]
+                            xmlData.write('\t\t\t\t<' + elem[1] + '>' + vals[j] + '</' + elem[1] + '>\n')
+                            j+=1
+                        else:
+                            break
+                    xmlData.write('\t\t\t</' + top + '>\n')
+                    i = j-1 # skip to this column
+                else:       # pv elements
+                    xmlData.write('\t\t\t<' + tags[i] + '>' + vals[i] + '</' + tags[i] + '>\n')
+                i+=1
+            xmlData.write('\t\t</' + tags[0] + '>\n')
+        rowNum += 1
+
+    # close components and config
+    levels.reverse()
+    for i in range(len(levels)-1):
+        if levels[i] != "":
+            xmlData.write('\t</component>\t<!-- ' + levels[i] + ' -->\n')
+
+    xmlData.write('</config>\t<!-- ' + levels[-1] + ' -->\n')
+    xmlData.close()
+    print tostring(tree, encoding="us-ascii", method="xml")

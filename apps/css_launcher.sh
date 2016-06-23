34fb85ae8e2509b1aa63a02df4c696f22dbac88b
Wesley Moore <wmoore@jlab.org>
2016-06-23 14:45:20 -0400

    css_launcher.sh: removed commented out lines

--- apps/css_launcher.sh
+++ apps/css_launcher.sh
@@ -24,7 +24,4 @@ fi
 echo Running css ...
 css \
     -pluginCustomization $css_share_path/common/prefs/plugin_customization.ini \
-	-share_link $css_share_path=/CLAS12_Share
-#\
-#	-nosplash &
-
+	-share_link $css_share_path=/CLAS12_Share &

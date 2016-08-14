21f92fe12bbea50d7612d87e9303dec99fb0aa4d
baltzell <baltzell@gmx.com>
2016-08-14 18:55:02 -0400

    css_share/detectors/CTOF:  use DET instead of HW hieararchy

--- css_share/detectors/CTOF/scripts/CTOF_HV_details_builder.js
+++ css_share/detectors/CTOF/scripts/CTOF_HV_details_builder.js
@@ -4,11 +4,10 @@ importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
 
 var novice = PVUtil.getDouble(pvs[0]);
 
+/*
 var crate = "HVCTOF0";
 var nchan = 24;
-
 var ichan=0;
-
 for (var slot=0; slot<4; slot++)
 {
   if (slot<10) slot="0"+slot;
@@ -31,3 +30,31 @@ for (var slot=0; slot<4; slot++)
     widget.addChildToBottom(lc);
   }
 }
+*/
+
+var ud=["U","D"];
+for (var ich=1; ich<=48; ich++)
+{
+    for (var iud=0; iud<ud.length; iud++)
+    {
+        var ch=ich;
+        if (ich<10) ch="0"+ich;
+        var pvprefix="B_DET_CTOF_HV_"+ud[iud]+ch;
+        
+        var lc = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");
+        if (novice>0) { lc.setPropertyValue("opi_file","/CLAS12_Share/apps/caenHvApp/caenhv_channel_novice.opi"); }
+        else          { lc.setPropertyValue("opi_file","/CLAS12_Share/apps/caenHvApp/caenhv_channel.opi"); }
+        //try   { lc.setPropertyValue("resize_behaviour",1); }
+        //catch (err) { lc.setPropertyValue("auto_size",true); }
+        lc.setPropertyValue("auto_size",true);
+        lc.setPropertyValue("zoom_to_fit",false);
+        lc.setPropertyValue("border_style",0);
+        lc.setPropertyValue("background_color","Header_Background");
+        lc.addMacro("C",ch);
+        lc.addMacro("P",pvprefix);
+        widget.addChildToBottom(lc);
+
+    }
+}
+
+

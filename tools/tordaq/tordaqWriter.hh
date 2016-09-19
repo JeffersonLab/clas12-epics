088e5c6d1c082b23fc0b0124c748d1effaeb177b
baltzell <baltzell@gmx.com>
2016-09-19 18:40:56 -0400

    add writer class, to be part of bigger unfinished reorg/cleanup

---
+++ tools/tordaq/tordaqWriter.hh
@@ -0,0 +1,45 @@
+#include "tordaqData.hh"
+class tordaqWriter {
+public:
+    const char* separator=",";
+    tordaqWriter(){};
+    ~tordaqWriter(){};
+    TNtupleD* ntuple=NULL;;
+    std::vector <TH1*> histos;
+    bool writeAsciiFileFromNtuple()
+    {
+        return true;
+    }
+    bool writeAsciiFileFromHistos()
+    {
+        return true;
+    }
+    bool writeAsciiFile(const char* filename)
+    {
+        ntuple=NULL;
+        histos.clear();
+        TObject *xx=gDirectory-Get("tordaq");
+        if (xx)
+        {
+            ntuple=(TNtupleD*)xx;
+            return writeAsciiFileFromNtuple();
+        }
+        else {
+            int ivt=0;
+            while (1)
+            {
+                TObject *xx=gDirectory->Get(Form("h%d",ivt1+1));
+                if (!xx) break;
+                histos.push_back((TH1*)xx);
+                ivt++;
+            }
+            if (ivt!=tordaqData::NVT)
+            {
+                std::cerr<<"tordaqWriter:  Wrong number of histograms."<<std::endl;
+                return false;
+            }
+            return writeAsciiFileFromHistograms();
+        }
+    }
+};
+

f018ffab7e0ae78df1bd514e8d3b8027acc76521
baltzell <baltzell@gmx.com>
2016-09-19 18:33:28 -0400

    tordaqData.hh:  cRio changed to 2kHz readings

--- tools/tordaq/tordaqData.hh
+++ tools/tordaq/tordaqData.hh
@@ -11,7 +11,8 @@
 class tordaqData {
 public :
    static const int NVT=22;
-   static const int FREQUENCY=3846;
+   //static const int FREQUENCY=3846;
+   static const int FREQUENCY=2000;
    static const int WFLENGTH=2000;
    TTree          *fChain;
    Int_t           fCurrent;

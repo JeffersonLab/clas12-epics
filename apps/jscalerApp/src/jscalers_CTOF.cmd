0463445e0fab34b99fded3b48fc3458234d85034
baltzell <baltzell@gmx.com>
2016-09-23 21:24:17 -0400

    need to remove startups from src

---
+++ apps/jscalerApp/src/jscalers_CTOF.cmd
@@ -0,0 +1,23 @@
+#!../../bin/linux-x86_64/iocscalers
+< envPaths
+cd ${TOP}
+
+Init_SCALERS()
+
+Start_SCALERS_CRATE("0","CTOF1")
+
+
+## Register all support components
+dbLoadDatabase("dbd/iocscalers.dbd")
+iocscalers_registerRecordDeviceDriver(pdbbase)
+
+## Load record instances
+dbLoadRecords("db/iocAdminSoft.db", "IOC=${IOC}")
+
+dbLoadTemplate("db/jscalers_CTOF_FADC.sub")
+
+
+cd ${TOP}/iocBoot/${IOC}
+
+iocInit
+

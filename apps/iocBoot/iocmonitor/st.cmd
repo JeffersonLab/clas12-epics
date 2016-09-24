699df70745e61fefb465e2edf25b8dfe48097337
baltzell <baltzell@gmx.com>
2016-09-24 17:44:52 -0400

    iocmonitor/st.cmd:  add hv sums

--- apps/iocBoot/iocmonitor/st.cmd
+++ apps/iocBoot/iocmonitor/st.cmd
@@ -8,13 +8,35 @@ cd ${TOP}
 dbLoadDatabase "dbd/monitor.dbd"
 monitor_registerRecordDeviceDriver pdbbase
 
-dbLoadRecords("db/heartbeatCalc.db","P=B_HW_,R=CRIO_DEV_,DLY=60")
-dbLoadRecords("db/heartbeatCalc.db","P=B_HW_,R=CRIO_HTCC_,DLY=60")
-dbLoadRecords("db/heartbeatCalc.db","P=B_HW_,R=CRIO_SVT_,DLY=60")
+#dbLoadRecords("db/heartbeatCalc.db","P=B_HW_,R=CRIO_DEV_,DLY=60")
+#dbLoadRecords("db/heartbeatCalc.db","P=B_HW_,R=CRIO_HTCC_,DLY=60")
+#dbLoadRecords("db/heartbeatCalc.db","P=B_HW_,R=CRIO_SVT_,DLY=60")
+
+dbLoadRecords("db/caenhv_FTOF_sums.db","SEC=1")
+dbLoadRecords("db/caenhv_FTOF_sums.db","SEC=2")
+dbLoadRecords("db/caenhv_FTOF_sums.db","SEC=3")
+dbLoadRecords("db/caenhv_FTOF_sums.db","SEC=4")
+dbLoadRecords("db/caenhv_FTOF_sums.db","SEC=5")
+dbLoadRecords("db/caenhv_FTOF_sums.db","SEC=6")
+
+dbLoadRecords("db/caenhv_ECAL_sums.db","SEC=1")
+dbLoadRecords("db/caenhv_ECAL_sums.db","SEC=2")
+dbLoadRecords("db/caenhv_ECAL_sums.db","SEC=3")
+dbLoadRecords("db/caenhv_ECAL_sums.db","SEC=4")
+dbLoadRecords("db/caenhv_ECAL_sums.db","SEC=5")
+dbLoadRecords("db/caenhv_ECAL_sums.db","SEC=6")
+
+dbLoadRecords("db/caenhv_PCAL_sums.db","SEC=1")
+dbLoadRecords("db/caenhv_PCAL_sums.db","SEC=2")
+dbLoadRecords("db/caenhv_PCAL_sums.db","SEC=3")
+dbLoadRecords("db/caenhv_PCAL_sums.db","SEC=4")
+dbLoadRecords("db/caenhv_PCAL_sums.db","SEC=5")
+dbLoadRecords("db/caenhv_PCAL_sums.db","SEC=6")
 
 dbLoadRecords("db/iocAdminSoft.db","IOC=${IOC}")
 
 cd ${TOP}/iocBoot/${IOC}
 
 iocInit();
+
 

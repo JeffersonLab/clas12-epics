2739567d9b1f709ee67ed546c990ce7c84bab589
baltzell <baltzell@gmx.com>
2016-09-23 20:40:53 -0400

    FTC_HV_on.js:  add confirm dialog

--- css_share/detectors/FTC/scripts/FTC_HV_on.js
+++ css_share/detectors/FTC/scripts/FTC_HV_on.js
@@ -2,6 +2,12 @@ importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
 
 var prefix = "B_DET_FTC_HV_";
 
+var label = "Turn ON ALL HV for FTC?"
+
+var response = GUIUtil.openConfirmDialog(label);
+if (response)
+{
+
 for (var qq=1; qq<=4; qq++)
 {
   for (var gg=1; gg<=9; gg++)
@@ -10,4 +16,6 @@ for (var qq=1; qq<=4; qq++)
       pv = prefix + "Q" + qq + "G" + gg + ":pwonoff";
       PVUtil.writePV(pv,1);
   }
+}
+
 }

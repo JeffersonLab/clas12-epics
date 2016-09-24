983f244b7003bca9d3f431182b715a279e0671e5
baltzell <baltzell@gmx.com>
2016-09-23 20:43:06 -0400

    syX527App/sy1527.c:  add 1737 and 1536 boards (still need to get polarity on 1536)

--- apps/syX527App/src/sy1527.c
+++ apps/syX527App/src/sy1527.c
@@ -60,9 +60,10 @@ int NCFEDOWNERR[MAX_HVPS];
 #define TripExt 14
 #define PDwn  15   // my: PDwn replaced Tdrift
 
-static int  nA1536param = 16;
 static int  nA1520param = 16; // my: 16
 static int  nA1535param = 16;
+static int  nA1737param = 16;
+static int  nA1536HDMparam = 18;
 
 //static char A1520param[MAX_PARAM][MAX_CAEN_NAME] = {
 //                "V0Set","I0Set","V1Set","I1Set","RUp","RDWn","Trip","SVMax",
@@ -73,9 +74,12 @@ static char A1535param[MAX_PARAM][MAX_CAEN_NAME] = {
 static char A1520param[MAX_PARAM][MAX_CAEN_NAME] = {
                 "V0Set","I0Set","V1Set","I1Set","RUp","RDWn","Trip","SVMax",
                 "VMon","IMon","Status","Pw","PwEn","TripInt","TripExt","Tdrift"};
-static char A1536param[MAX_PARAM][MAX_CAEN_NAME] = {
+static char A1737param[MAX_PARAM][MAX_CAEN_NAME] = {
                 "V0Set","I0Set","V1Set","I1Set","RUp","RDWn","Trip","SVMax",
                 "VMon","IMon","Status","Pw","POn","TripInt","TripExt","PDwn"};
+static char A1536HDMparam[MAX_PARAM][MAX_CAEN_NAME] = {
+                "V0Set","I0Set","V1Set","I1Set","RUp","RDWn","Trip","SVMax",
+                "VMon","IMon","Status","Pw","POn","TripInt","TripExt","PDwn","ImRange","Pol"};
 ///----------------- for HV parameters finding ------------------- 
 
 #include <linux/limits.h>
@@ -690,7 +694,9 @@ sy1527GetMap(unsigned int id)
         /* get params info */
         for(j=0; j<Demand[id].board[i].nchannels; j++)
           ChList[j] = (unsigned short)j;
-        if(!strcmp(Measure[id].board[i].modelname,"A1535") || !strcmp(Measure[id].board[i].modelname,"A1733") ) // my: was A1520
+        if(!strcmp(Measure[id].board[i].modelname,"A1535") ||
+           !strcmp(Measure[id].board[i].modelname,"A1733") ||
+           !strcmp(Measure[id].board[i].modelname,"A1737")) // my: was A1520
         {
           printf("---> found board %s\n",Measure[id].board[i].modelname);
           Measure[id].board[i].nparams = nA1535param;
@@ -747,12 +753,12 @@ sy1527GetMap(unsigned int id)
         else if( !strcmp(Measure[id].board[i].modelname,"A1536HDM")) // my: was A1520
         {
           printf("---> found board %s\n",Measure[id].board[i].modelname);
-          Measure[id].board[i].nparams = nA1536param;
-          Demand[id].board[i].nparams = nA1536param;
+          Measure[id].board[i].nparams = nA1536HDMparam;
+          Demand[id].board[i].nparams = nA1536HDMparam;
           for(j=0; j<Measure[id].board[i].nparams; j++)
           {
-            strcpy(Measure[id].board[i].parnames[j],A1536param[j]);
-            strcpy(Demand[id].board[i].parnames[j],A1536param[j]);
+            strcpy(Measure[id].board[i].parnames[j],A1536HDMparam[j]);
+            strcpy(Demand[id].board[i].parnames[j],A1536HDMparam[j]);
 
             strcpy(ParName,Measure[id].board[i].parnames[j]);
             ret=CAENHVGetChParamProp(name,i,ChList[0],ParName,"Type",&tipo);

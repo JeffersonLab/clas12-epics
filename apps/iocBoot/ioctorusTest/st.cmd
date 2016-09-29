4bfbf0a20f648d75728c386155ce6bb612adea24
Wesley Moore <wmoore@jlab.org>
2016-09-29 10:20:23 -0400

    torus: added ioc for testing changes

---
+++ apps/iocBoot/ioctorusTest/st.cmd
@@ -0,0 +1,27 @@
+#!../../bin/linux-x86/plc2epics
+############################################################################
+< envPaths
+############################################################################
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase("dbd/plc2epics.dbd")
+plc2epics_registerRecordDeviceDriver(pdbbase)
+
+## Initialize EtherIP driver, define PLCs
+## NOTE: if buffer limit is left at 500, read errors will occur.
+EIP_buffer_limit(450)
+drvEtherIP_init()
+drvEtherIP_define_PLC("PLC_CRYO",  "129.57.96.17", 0)
+drvEtherIP_define_PLC("PLC_TORUS", "129.57.96.15", 0)
+
+## Debugging [7-10]
+#EIP_verbosity(7)
+
+## Load record instances
+dbLoadTemplate("db/torus_import.substitutions")
+
+cd ${TOP}/iocBoot/${IOC}
+
+dbl > pv.list
+iocInit

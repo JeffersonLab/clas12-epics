d8a01180f756de7d178196e59d014d1996a02d38
Wesley Moore <wmoore@jlab.org>
2016-10-06 10:22:22 -0400

    torus force: adding EM forces - draft

---
+++ apps/plcApp/op/opi/scripts/datatype_straingage_em.js
@@ -0,0 +1,64 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+
+var run = PVUtil.getDouble(pvs[0]);
+
+var P = widget.getMacroValue("P");
+var R = widget.getMacroValue("R");
+
+var ELM = ["HB_US_S1", "HB_US_S2", "HB_US_S3", "HB_US_S4", "HB_US_S5", "HB_US_S6", 
+		   "HB_DS_S1", "HB_DS_S2","HB_DS_S3","HB_DS_S4","HB_DS_S5", "HB_DS_S6"];
+var ID  = ["U1HB", "U2HB", "U3HB", "U4HB", "U5HB", "U6HB",
+		   "D1HB", "D2HB", "D3HB", "D4HB", "D5HB", "D6HB"];
+var CNT = 2;
+
+for (var i=0; i<ELM.length; i++) {
+	for (var n=1; n<=CNT; n++) {
+		var lc = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");
+		lc.setPropertyValue("opi_file","/CLAS12_Share/apps/plcApp/datatype_straingage_em_row.opi");
+		lc.setPropertyValue("auto_size",true);
+		lc.setPropertyValue("zoom_to_fit",false);
+		lc.setPropertyValue("border_style",0);
+		lc.setPropertyValue("background_color","OPI_Background");
+		lc.addMacro("ELM", ELM[i]);
+		lc.addMacro("ID", ID[i]);
+		lc.addMacro("N", n);
+		widget.addChildToBottom(lc);
+	}
+}
+
+var ELM = ["HB_US_S1_VS", "HB_US_S4_VS", "HB_DS_S1_VS", "HB_DS_S4_VS"];
+var ID  = ["U1HB", "U4HB", "D1HB", "D4HB"];
+
+for (var i=0; i<ELM.length; i++) {
+	for (var n=3; n<=4; n++) {
+		var lc = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");
+		lc.setPropertyValue("opi_file","/CLAS12_Share/apps/plcApp/datatype_straingage_em_row.opi");
+		lc.setPropertyValue("auto_size",true);
+		lc.setPropertyValue("zoom_to_fit",false);
+		lc.setPropertyValue("border_style",0);
+		lc.setPropertyValue("background_color","OPI_Background");
+		lc.addMacro("ELM", ELM[i]);
+		lc.addMacro("ID", ID[i]);
+		lc.addMacro("N", n);
+		widget.addChildToBottom(lc);
+	}
+}
+
+ELM = ["CCM_B_AS", "CCM_D_AS", "CCM_F_AS"];
+ID  = ["B", "D", "F"];
+
+for (i=0; i<ELM.length; i++) {
+	for (n=5; n<=6; n++) {
+		var lc = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");
+		lc.setPropertyValue("opi_file","/CLAS12_Share/apps/plcApp/datatype_straingage_em_row.opi");
+		lc.setPropertyValue("auto_size",true);
+		lc.setPropertyValue("zoom_to_fit",false);
+		lc.setPropertyValue("border_style",0);
+		lc.setPropertyValue("background_color","OPI_Background");
+		lc.addMacro("ELM", ELM[i]);
+		lc.addMacro("ID", ID[i]);
+		lc.addMacro("N", n);
+		widget.addChildToBottom(lc);
+	}
+}

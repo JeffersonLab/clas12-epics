2cae52bfcc1ed3c428206f6f9d74a7c70a26d172
Wesley Moore <wmoore@jlab.org>
2016-10-06 10:15:41 -0400

    ioctorusTest: testing new EM Force add-ons

--- apps/iocBoot/ioctorusTest/st.cmd
+++ apps/iocBoot/ioctorusTest/st.cmd
@@ -12,14 +12,15 @@ plc2epics_registerRecordDeviceDriver(pdbbase)
 ## NOTE: if buffer limit is left at 500, read errors will occur.
 EIP_buffer_limit(450)
 drvEtherIP_init()
-drvEtherIP_define_PLC("PLC_CRYO",  "129.57.96.17", 0)
+#drvEtherIP_define_PLC("PLC_CRYO",  "129.57.96.17", 0)
 drvEtherIP_define_PLC("PLC_TORUS", "129.57.96.15", 0)
 
 ## Debugging [7-10]
 #EIP_verbosity(7)
 
 ## Load record instances
-dbLoadTemplate("db/torus_import.substitutions")
+#dbLoadTemplate("db/torus_import.substitutions")
+dbLoadTemplate("db/torus_force_addons.substitutions")
 
 cd ${TOP}/iocBoot/${IOC}
 

c1571755111ee125f5c34a9d54d1fb5c92ccff18
Wesley Moore <wmoore@jlab.org>
2016-10-28 14:10:54 -0400

    solnoid fastdaq: draft ioc config

--- apps/iocBoot/iocsolenoidDaq/st.cmd
+++ apps/iocBoot/iocsolenoidDaq/st.cmd
@@ -1,44 +1,35 @@
-#!../../bin/linux-x86_64/plc
+#!../../bin/linux-x86_64/wf2root
 ############################################################################
 < envPaths
+# TODO: set IP
+epicsEnvSet("EPICS_CA_ADDR_LIST", "129.57.96.36")
 ############################################################################
 cd "${TOP}"
 
 ## Register all support components
-dbLoadDatabase("dbd/plc.dbd")
-plc_registerRecordDeviceDriver(pdbbase)
-
-## Initialize EtherIP driver, define PLCs
-## NOTE: if buffer limit is left at 500, read errors will occur.
-EIP_buffer_limit(450)
-drvEtherIP_init()
-drvEtherIP_define_PLC("PLC_SOL",  "129.57.96.999", 0)
-
-## Debugging [7-10]
-#EIP_verbosity(7)
+dbLoadDatabase("dbd/wf2root.dbd")
+wf2root_registerRecordDeviceDriver(pdbbase)
 
 ## Load record instances
 dbLoadRecords("${DEVIOCSTATS}/db/iocAdminSoft.db","IOC=${IOC}")
 dbLoadRecords("${AUTOSAVE}/asApp/Db/save_restoreStatus.db", "P=${IOC}:")
-## PLC_SOL
-dbLoadTemplate("db/solenoid_LHe.substitutions")
-## CA Security
-dbLoadRecords("db/cas.db", "P=B_SOL:,R=CRYO:,ASG=DEFAULT")
-asSetFilename("${TOP}/iocBoot/acf/solenoid_cryo.acf")
-asSetSubstitutions("P=B_SOL:,R=CRYO:")
+#
+dbLoadTemplate("iocBoot/${IOC}/wf2root.substitutions")
 
-cd ${TOP}/iocBoot/${IOC}
+dbLoadRecords("db/cRIO_heartbeat.db","P=B_HW_,R=CRIO_SOLDAQ_,DLY=60")
+
+cd "${TOP}/iocBoot/${IOC}"
 
 ## autosave setup
 < save_restore.cmd
 
 dbl > pv.list
 iocInit
-caPutLogInit("clonioc1:7011")
 
 ## autosave startup
 ## Handle autosave 'commands' contained in loaded databases.
 makeAutosaveFiles()
-create_monitor_set("info_positions.req", 5, "P=xxx:")
-create_monitor_set("info_settings.req", 30, "P=xxx:")
+create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+create_monitor_set("info_settings.req", 30, "P=${IOC}:")
+create_monitor_set("wf2root_settings.req", 30, "P=B_SOL:DAQ_REC:")
 

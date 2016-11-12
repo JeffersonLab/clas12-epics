955ea41646c255fafb0b76cf6fdd23f0787b8950
baltzell <baltzell@gmx.com>
2016-11-11 19:29:34 -0500

    download_camera.sh:  bugfix/comments

--- css_share/cameras/download_camera.sh
+++ css_share/cameras/download_camera.sh
@@ -4,6 +4,7 @@ host=$1
 cd /usr/clas12/DATA/cameras
 
 # setup the symlink:
+# (cs-studio needs to toggle between jpgs to force screen refreshes, so we use a symlink)
 rm -f ${host}_link.jpg
 ln -s ${host}.jpg ${host}_link.jpg
 
@@ -11,7 +12,7 @@ while [ 1 ]
 do
   # wget is slower than mv, and cs-studio doesn't like a partial file,
   # so wget to a temporary file, then mv
-  wget -O ${host}_tmp.jpg http://cctv6/axis-cgi/jpg/image.cgi
+  wget -O ${host}_tmp.jpg http://${host}/axis-cgi/jpg/image.cgi
   mv -f ${host}_tmp.jpg ${host}.jpg
   sleep 1
 done

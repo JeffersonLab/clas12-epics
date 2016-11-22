687843976440ff74d48f05208ab3cd2e4c0648c9
Wesley Moore <wmoore@jlab.org>
2016-11-22 14:42:19 -0500

    solenoid: added iocsoleniodQD

---
+++ apps/iocBoot/iocsolenoidQD/st.cmd
@@ -0,0 +1,34 @@
+#!../../bin/linux-x86_64/plc2epics
+############################################################################
+< envPaths
+epicsEnvSet("EPICS_CA_ADDR_LIST", "129.57.96.18")
+############################################################################
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase("dbd/plc2epics.dbd")
+plc2epics_registerRecordDeviceDriver(pdbbase)
+
+## Initialize EtherIP driver, defined PLCs
+drvEtherIP_init()
+drvEtherIP_define_PLC("PLC_SOL", "129.57.96.30", 0)
+
+## Load record instances
+dbLoadRecords("${DEVIOCSTATS}/db/iocAdminSoft.db","IOC=${IOC}")
+dbLoadRecords("${AUTOSAVE}/asApp/Db/save_restoreStatus.db", "P=${IOC}:")
+dbLoadTemplate("db/solenoid_quench.substitutions")
+
+cd "${TOP}/iocBoot/${IOC}"
+
+## autosave setup
+< save_restore.cmd
+
+dbl > pv.list
+iocInit
+
+## autosave startup
+## Handle autosave 'commands' contained in loaded databases.
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+create_monitor_set("info_settings.req", 30, "P=${IOC}:")
+

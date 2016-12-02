e53eebb29967f487313738b3db27184f668a69ed
baltzell <baltzell@gmx.com>
2016-12-02 18:44:47 -0500

    ioccaenhv/procServ-HVDC*:  split for two iocs

--- apps/iocBoot/ioccaenhv/hvdc_procServ.sh
+++
@@ -1,1 +0,0 @@
-procServ -n ioccaenhv_HVDC -i^D^C --logfile /usr/clas12/DATA/logs/ioccaenhv_HVDC.log --logstamp -c /usr//clas12/release/pro/epics/apps/iocBoot/ioccaenhv/ 20001 ./st.cmd.HVDC
---
+++ apps/iocBoot/ioccaenhv/procServ-HVDCa.sh
@@ -0,0 +1,1 @@
+procServ -n ioccaenhv_HVDCa -i^D^C --logfile /usr/clas12/DATA/logs/ioccaenhv_HVDCa.log --logstamp -c /usr//clas12/release/pro/epics/apps/iocBoot/ioccaenhv/ 20001 ./st.cmd.HVDC-dc23

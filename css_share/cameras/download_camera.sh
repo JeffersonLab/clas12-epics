43e635ab84791d2e6ffbaa1593a2a2b2d0a00dbe
baltzell <baltzell@gmx.com>
2016-12-09 19:53:45 -0500

    camera:  quiet caput

--- css_share/cameras/download_camera.sh
+++ css_share/cameras/download_camera.sh
@@ -8,12 +8,12 @@ cd /usr/clas12/DATA/cameras
 rm -f ${host}_link.jpg
 ln -s ${host}.jpg ${host}_link.jpg
 
-ii = 0
+ii=0
 while [ 1 ]
 do
   # wget is slower than mv, and cs-studio doesn't like a partial file,
   # so wget to a temporary file, then mv
-  wget -O ${host}_tmp.jpg http://${host}/axis-cgi/jpg/image.cgi
+  wget -q -O ${host}_tmp.jpg http://${host}/axis-cgi/jpg/image.cgi
   mv -f ${host}_tmp.jpg ${host}.jpg
   sleep 0.5
   
@@ -22,7 +22,7 @@ do
   kk=`echo "scale=0;$ii/2" | bc`
   if [ "$jj" = "0" ]
   then
-    caput B_HW_CAMS_cctv6:HEARTBEAT $kk &
+    caput -t B_HW_CAMS_cctv6:HEARTBEAT $kk &
   fi
   
 done

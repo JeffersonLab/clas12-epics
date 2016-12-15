4813e84f5c28cc030eef3a50951f7e6f1c42773a
baltzell <baltzell@gmx.com>
2016-12-14 19:29:53 -0500

    caen527HvApp:  final setter script

--- apps/caenHv527App/op/scripts/dchvset.sh
+++ apps/caenHv527App/op/scripts/dchvset.sh
@@ -1,7 +1,69 @@
 #!/bin/bash
-P=B_DET_DC_HV
+
 USAGE="Usage:\n\tsetdc.sh sector region layer wires setter value"
 
+##########################################################################
+#
+# Loops over channels and caputs.
+# 
+# All arguments must be supplied and in the correct order.
+# Argument values are checked for validity.
+#
+# Interactive user input is requested for confirmation after printing the
+# given configuration.  To override this, one can pipe a 'y' to this script,
+# e.g. `echo y | dchvset.sh`.
+#
+# Valid argment values:
+#
+# Detector Components
+# sector = 1,2,3,4,5,6
+# region = 1,2,3
+# layer  = 1,2,3,4,5,6 (depends on region)
+# wires  = S,F,G
+# all    = 0 (loops over all possibilities given above)
+#
+# Setter:
+# pwonoff, vset, iset, rup, rdn, vmax, trip
+#
+#
+#
+# This script exists because:
+# 1. Mass caputs occaisonally timeout on the ioc for CEAN 527
+#    e.g. hundreds of successive caputs without any delays
+# 2. High speed writes (e.g. via burt) occaisonally result in the
+#    CAENET v288 cards failing to succesfully transmit to the HV
+#    board.  And currently our driver does not check whether the
+#    transmission failed and retry (and that would be pretty
+#    complicated in its current design).
+# On #2, records to compare inputs and outputs work to fix the
+#    issue, but could be risky if a bad readback exists.  And also
+#    this will get more complicated when we introduce group writes.
+# Group output operations fully get rid of both #1 and #2, but
+#    introduce the additional issue that single channel output
+#    records do not update after a group output operation.
+# 
+#########################################################################
+
+STARTTIME=`date`
+
+# PV PREFIX FOR THE DC HV:
+P=B_DET_DC_HV
+
+# PAUSE OCCAISONALLY FOR BETTER PERFORMANCE:
+NCAPUTS=0
+function CAPUT {
+  pv=$1
+  value=$2
+  caput -w 2 $pv $value
+  let NCAPUTS=$NCAPUTS+1
+  # this is the parameter to tune
+  if [ $NCAPUTS -gt 5 ]; then
+    NCAPUTS=0
+    sleep 1
+  fi
+}
+
+# CHECK #ARGUMENTS:
 if [ -z $6 ]; then
   echo -e "Missing Argments.\n$USAGE" ; exit
 elif ! [ -z $7 ]; then
@@ -15,7 +77,7 @@ wires=$4
 setter=$5
 value=$6
 
-# CHECK ARGUMENTS:
+# CHECK ARGUMENT VALUES:
 if [ "$setter" != "vset" ] && [ "$setter" != "iset" ] && [ "$setter" != "pwonoff" ] && [ "$setter" != "rup" ] && [ "$setter" != "rdn" ] && [ "$setter" != "trip" ] && [ "$setter" != "vmax" ]; then
   echo -e "\nInvalid setter:  $setter\n\n$USAGE" ; exit
 fi
@@ -31,17 +93,19 @@ fi
 if [ "$wires" != "0" ] && [ "$wires" != "S" ] && [ "$wires" != "F" ] && [ "$wires" != "G" ]; then
   echo -e "\nInvalid Wires:  $wires\n\n$USAGE" ; exit
 fi
-if [ "$region" == "1" ]; then
-  if [ "$layer" != "1" ] && [ "$layer" != "2" ]; then
-    echo -e "\nInvalid Region/Layer Combination:  $region/$layer\n\n$USAGE" ; exit
-  fi
-elif [ "$region" == "2" ]; then
-  if [ "$layer" != "3" ] && [ "$layer" != "4" ]; then
-    echo -e "\nInvalid Region/Layer Combination:  $region/$layer\n\n$USAGE" ; exit
-  fi
-elif [ "$region" == "3" ]; then
-  if [ "$layer" != "4" ] && [ "$layer" != "5" ]; then
-    echo -e "\nInvalid Region/Layer Combination:  $region/$layer\n\n$USAGE" ; exit
+if [ "$layer" != "0" ]; then
+  if [ "$region" == "1" ]; then
+    if [ "$layer" != "1" ] && [ "$layer" != "2" ]; then
+      echo -e "\nInvalid Region/Layer Combination:  $region/$layer\n\n$USAGE" ; exit
+    fi
+  elif [ "$region" == "2" ]; then
+    if [ "$layer" != "3" ] && [ "$layer" != "4" ]; then
+      echo -e "\nInvalid Region/Layer Combination:  $region/$layer\n\n$USAGE" ; exit
+    fi
+  elif [ "$region" == "3" ]; then
+    if [ "$layer" != "4" ] && [ "$layer" != "5" ]; then
+      echo -e "\nInvalid Region/Layer Combination:  $region/$layer\n\n$USAGE" ; exit
+    fi
   fi
 fi
 
@@ -70,6 +134,13 @@ else
 fi
 echo
 
+# USER CONFIRMATION
+echo "Enter 'Y' or 'y' to confirm..."
+read answer
+if [ "$answer" != "y" ] && [ "$answer" != "Y" ]; then
+  exit
+fi
+
 # LOOP OVER SECTOR/REGION/LAYER/WIRES AND CAPUT:
 for ss in 1 2 3 4 5 6
 do
@@ -91,16 +162,20 @@ do
         fi
         if [ "$wt" == "S" ] || [ "$wt" == "F" ]; then
           for wn in 01-08 09-16 17-24 25-32 33-48 49-64 65-80 81-112; do
-            caput ${P}_SEC${ss}_R${rr}_SL${ll}_${wt}${wn}:${setter} ${value}
+            CAPUT ${P}_SEC${ss}_R${rr}_SL${ll}_${wt}${wn}:${setter} ${value}
           done
         elif [ "$wt" == "G" ]; then
           for wn in 01-32 33-112; do
-            caput ${P}_SEC${ss}_R${rr}_SL${ll}_${wt}${wn}:${setter} ${value}
+            CAPUT ${P}_SEC${ss}_R${rr}_SL${ll}_${wt}${wn}:${setter} ${value}
           done
         fi
-        sleep 1
       done
     done
   done
 done
+
+ENDTIME=`date`
+
+echo StartTime: $STARTTIME
+echo EndTime:   $ENDTIME
 

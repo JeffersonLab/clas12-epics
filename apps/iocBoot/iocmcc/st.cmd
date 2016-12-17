14b32eda46e92e81ee7161d92fb538ddb0f68250
baltzell <baltzell@gmx.com>
2016-12-16 19:44:22 -0500

    move mcc pv copies to mccApp and softioc

---
+++ apps/iocBoot/iocmcc/st.cmd
@@ -0,0 +1,31 @@
+#!../../bin/linux-x86_64/mcc
+
+< envPaths
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/mcc.dbd"
+mcc_registerRecordDeviceDriver pdbbase
+
+dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")
+dbLoadRecords("db/iocAdminSoft.db","IOC=${IOC}")
+
+dbLoadRecords("db/mcc_vacuum.db")
+dbLoadRecords("db/mcc_bpm.db")
+dbLoadRecords("db/mcc_cryo.db")
+
+cd ${TOP}/iocBoot/${IOC}
+
+< save_restore.cmd
+
+iocInit();
+
+caPutLogInit("clonioc1:7011")
+
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+create_monitor_set("info_settings.req", 30, "P=${IOC}:")
+
+#dbl > pv.list
+

072191bac81a9c8156feba1c7569b62d23c71bd1
baltzell <baltzell@gmx.com>
2017-01-06 11:09:40 -0500

    setup DC VME crate screens

---
+++ apps/vmeCratesApp/op/opi/scripts/vmeCratesBuilder_novice-DC.js
@@ -0,0 +1,27 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+for (var ss=1; ss<=6; ss++)
+{
+    var line = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.Label");
+    line.setPropertyValue("width",1);
+    line.setPropertyValue("height",10);
+    line.setPropertyValue("text","");
+    widget.addChildToBottom(line);
+
+for (var rr=1; rr<=3; rr++)
+{
+  var crateName="dc"+ss+rr;
+
+        var lc = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");
+        lc.setPropertyValue("opi_file","vmeCrate_novice.opi");
+        //try   { lc.setPropertyValue("resize_behaviour",1); }
+        //catch (err) { lc.setPropertyValue("auto_size",true); }
+        lc.setPropertyValue("auto_size",true);
+        lc.setPropertyValue("zoom_to_fit",false);
+        lc.setPropertyValue("border_style",0);
+        lc.setPropertyValue("background_color","OPI_Background");
+        lc.addMacro("P",crateName);
+        widget.addChildToBottom(lc);
+    }
+}
+

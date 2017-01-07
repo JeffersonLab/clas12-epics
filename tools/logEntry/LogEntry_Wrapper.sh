72785dc055b5a3d53065592295da0318e0e7bfad
baltzell <baltzell@gmx.com>
2017-01-07 17:42:24 -0500

    LogEntryWrapper.sh:  named vars for inputs, remove special case for clonpc19

--- tools/logEntry/LogEntry_Wrapper.sh
+++ tools/logEntry/LogEntry_Wrapper.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+windowID=$1
+windowTitle=$2
+
 # force java version
 
-if [ "$HOSTNAME" == "clonpc19.jlab.org" ]; then
-echo "Running on clonpc19"
-  export JAVA_HOME=/usr/lib/jvm/java-1.8.0-oracle.x86_64
-  #export JAVA_HOME=/usr/lib/jvm/java-1.7.0-oracle.x86_64
-else
-  export JAVA_HOME=/usr/lib/jvm/java-1.8.0-oracle
-fi
-
+#if [ "$HOSTNAME" == "clonpc19.jlab.org" ]; then
+#echo "Running on clonpc19"
+#  export JAVA_HOME=/usr/lib/jvm/java-1.8.0-oracle.x86_64
+#  #export JAVA_HOME=/usr/lib/jvm/java-1.7.0-oracle.x86_64
+#else
+#  export JAVA_HOME=/usr/lib/jvm/java-1.8.0-oracle
+#fi
+  
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-oracle
 export PATH=$JAVA_HOME/bin:$PATH
 
 CLASSPATH="${EPICS}/tools/logEntry/src/main/java"
@@ -17,8 +21,5 @@ CLASSPATH="${CLASSPATH}:${EPICS}/tools/logEntry/lib/jlog2.jar"
 CLASSPATH="${CLASSPATH}:${EPICS}/tools/logEntry/lib/mysql-connector-java-5.1.32-bin.jar"
 export CLASSPATH
 
-echo $JAVA_HOME
-echo $CLASSPATH
-
-java org/jlab/AutoLogEntry/MakeLogEntry $1 $2
+java org/jlab/AutoLogEntry/MakeLogEntry $windowID $windowTitle
 

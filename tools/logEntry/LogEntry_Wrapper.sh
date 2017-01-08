d81114980eac9553b8a9b2cfe1f4af04564fb353
baltzell <baltzell@gmx.com>
2017-01-08 15:37:59 -0500

    logEntryWrapper:  remove old java stuff

--- tools/logEntry/LogEntry_Wrapper.sh
+++ tools/logEntry/LogEntry_Wrapper.sh
@@ -3,16 +3,6 @@
 #windowID=$1
 #windowTitle=$2
 
-# force java version
-
-#if [ "$HOSTNAME" == "clonpc19.jlab.org" ]; then
-#echo "Running on clonpc19"
-#  export JAVA_HOME=/usr/lib/jvm/java-1.8.0-oracle.x86_64
-#  #export JAVA_HOME=/usr/lib/jvm/java-1.7.0-oracle.x86_64
-#else
-#  export JAVA_HOME=/usr/lib/jvm/java-1.8.0-oracle
-#fi
-  
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-oracle
 export PATH=$JAVA_HOME/bin:$PATH
 

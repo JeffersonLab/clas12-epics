6cc29963940372f25c44e71a5f8a65cbcf77dd2a
baltzell <baltzell@gmx.com>
2017-01-07 20:30:18 -0500

    logEntry:  overhaul this so it can eventually be used more generally (i.e. without an xterm)

--- tools/logEntry/src/main/java/org/jlab/AutoLogEntry/MakeLogEntry.java
+++ tools/logEntry/src/main/java/org/jlab/AutoLogEntry/MakeLogEntry.java
@@ -1,191 +1,172 @@
 package org.jlab.AutoLogEntry;
-
 import java.awt.*;
 import java.awt.event.*;
-//import javax.*;
 import javax.swing.*;
-
-import org.jlab.elog.LogEntry;
 import java.awt.Window;
 import java.awt.Frame ;
-
 import java.io.*;
 import java.sql.*;
-
 import java.util.Date;
+import org.jlab.elog.LogEntry;
 
 public class MakeLogEntry
 {
-    String winid;        // Window ID, that we need to get an image from
-    String medm_name;    // The name of MEDM (.adl) file
-    String img_path;     // Path of the Image, that will be submited to logbook
-    String file_name = "NO FILENAME";           // An .adl file nemae without ".adl", e.g. tagger_dump_lamp or hiv_alarm etc
-    String fit_parameters = "";
-    String AdditionalText = "";
-    String PV_list = ""; // List of Process Variables
-    String medm_dir = "";
-    String elog_Title = "";
-    JTextArea jta2 = new JTextArea("Comments", 20, 40);  // Text are where user can type comments
-    JFrame frame;
-    String comments_to_submit;     // Comments in addition to the 
-    long lognumber;
-    int run_number = -1;
-    JRadioButton rb_important;    
+    String LOGBOOKNAME="TLOG";
+    String WINID=null;      // Window ID, that we need to get an image from
+    String MEDMNAME=null; // The name of MEDM (.adl) file
+    String IMGPATH;       // Path of the Image, that will be submited to logbook
+    String FILENAME = "NO FILENAME";           // An .adl file nemae without ".adl", e.g. tagger_dump_lamp or hiv_alarm etc
+    JTextArea LOGTEXT = new JTextArea("Comments", 20, 40);  // Text are where user can type comments
+    JTextArea LOGTITLE = new JTextArea("Title", 20, 40);  // Text are where user can type comments
+    JFrame FRAME;
 
     public static void main( String[] args )
     {
         System.err.println("Running MakeLogEntry ...");
         try {
-            MakeLogEntry obj = new MakeLogEntry();
-            obj.main_run(args);
+          MakeLogEntry obj = new MakeLogEntry();
+          obj.run(args);
         }
-        catch( Exception e)
-        {
-            e.printStackTrace();
-        }
+        catch( Exception e) { e.printStackTrace(); }
     }
 
-    public void main_run(String[] args) throws Exception
+    public void run(String[] args)
     {
-        try{
-            Init();
-            winid = args[0];
-            medm_name = args[1];
-            System.out.println(winid);
-            System.out.println(medm_name);
-
-            img_path = getImgPath(winid, medm_name);
-            System.out.println("Img Full path = " + img_path);
-
-            PV_list = PV_list + getPVs("chan");
-            PV_list = PV_list + getPVs("rdbk");
-            PV_list = PV_list + getPVs("ydata");
-
-            run_number = getRunNumber();
-            elog_Title = "Snapshot of " + file_name + " : Run " + run_number;
-
+          try {
+            if (args.length>0) WINID = args[0];
+            if (args.length>1) MEDMNAME = args[1];
+            IMGPATH = getImgPath(WINID, MEDMNAME);
             addComments();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
+          }
+          catch (Exception e) { e.printStackTrace(); }
+    }
+    
+    public static String getTimeStamp()
+    {
+        Date date = new Date();
+        String year = String.format("%tY", date);
+        String month = String.format("%tm", date);
+        String day = String.format("%td", date);
+        String hour = String.format("%tH", date);
+        String min = String.format("%tM", date);
+        String sec = String.format("%tS", date);
+        return year+"_"+month+"_"+day+"_"+hour+"_"+min+"_"+sec;
     }
 
-    public void Init()
+    public String getWindowID()
     {
-        medm_dir = System.getenv("EPICS_DISPLAY_PATH");
-        System.out.println("EPICS_DISPLAY_PATH is " + medm_dir);
+      String windowID=null;
+      try {
+        String[] cmd = {"/usr/bin/xwininfo"};
+        Process proc = Runtime.getRuntime().exec(cmd);
+        proc.waitFor();
+        InputStreamReader isr = new InputStreamReader(proc.getInputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        String line;
+        while ((line=br.readLine()) != null) {
+          // here's the parsing of xwininfo output:
+          if (line.startsWith("xwininfo: Window id: ")) {
+            String[] parts=line.split(" ");
+            windowID=parts[3];
+            break;
+          }
+        }
+        br.close();
+      }
+      catch (Exception e) { e.printStackTrace(); }
+      return windowID;
     }
 
-    public String getImgPath( String winid, String medm_name)
+    public String getImgPath(String winid, String medmName)
     {
-        String IMG_path = null;
-        try{
-            Process p;
-            BufferedReader stdInput;
-            
-            String[] asdf = {"xwininfo"};
-            p = Runtime.getRuntime().exec(asdf);
-            stdInput = new BufferedReader(new InputStreamReader( p.getInputStream() ));
-            System.err.println(stdInput);
-            
-
-            String[] file_name_command = {"/bin/sh", "-c", "echo " + medm_name + " | sed -e s/.adl//"};
-            String time_stamp = getTimeStamp(); // time in a format YYYY_MM_DD_HH_mm_SS (year month day hour min sec)
-
-            p = Runtime.getRuntime().exec(file_name_command);
-            stdInput = new BufferedReader(new InputStreamReader( p.getInputStream() ));
-            file_name = stdInput.readLine();
-
-            p = Runtime.getRuntime().exec("pwd" );
-            stdInput = new BufferedReader(new InputStreamReader( p.getInputStream() ));
-            String cur_dir = stdInput.readLine();
-
-            String home_dir = System.getenv("HOME");
-            String snapshot_dir = home_dir+"/screenshots";
-            String[] cmd_convert = {"/bin/sh", "-c", "xwd -id " + winid + " | convert - " + snapshot_dir + "/" + file_name+"_"+time_stamp+".gif"};
-            p = Runtime.getRuntime().exec(cmd_convert );
-
-            IMG_path = snapshot_dir+"/"+file_name+"_"+time_stamp+".gif";
+        String imgPath = null;
+        try {
+            String windowID = getWindowID();
+            String stub = medmName; // command-line argument
+            String dir = System.getenv("HOME")+"/screenshots/";
+            String timeStamp = getTimeStamp();
+            imgPath = dir+stub+"_"+timeStamp+".gif";
+            System.err.println(imgPath);
+            String[] cmd = {"/bin/sh", "-c", "xwd -id " + windowID + " | convert - " + imgPath };
+            Runtime.getRuntime().exec(cmd);
         }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
-        return IMG_path;
+        catch (Exception e) { e.printStackTrace(); java.lang.System.exit(0); }
+        return imgPath;
     }
 
     public void addComments()
     {
-        frame = new JFrame("Additional Comments");
-        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        FRAME = new JFrame("Hall B Logbook Entry");
+        FRAME.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // log title instructions:
+        JTextArea logTitleInst = new JTextArea("Enter Log Title:");
+        logTitleInst.setVisible(true);
+        logTitleInst.setEditable(false);
+        logTitleInst.setFont(new Font("Diaglog.plain",0,20));
+        
+        // log title entry:
+        LOGTITLE.setText("Run #"+Integer.toString(getRunNumber())+":  ");
+        LOGTITLE.setVisible(true);
+        LOGTITLE.setEditable(true);
+        LOGTITLE.setForeground(Color.BLUE);
+        LOGTITLE.setFont(new Font("Diaglog.plain",0,20));
+        LOGTITLE.setMaximumSize(new Dimension(10, 50));
 
-        JTextArea jta1 = new JTextArea("Please add additional information below");
-        jta1.setVisible(true);
-        jta1.setEditable(false);
-        Font f2 = new Font("Dialog.plain", 0, 25);
-        jta1.setFont(f2);
-        jta1.setForeground(Color.BLUE);
+        // log text instructions:
+        JTextArea logTextInst = new JTextArea("Please add additional information below:");
+        logTextInst.setVisible(true);
+        logTextInst.setEditable(false);
+        logTextInst.setFont(new Font("Dialog.plain", 0, 20));
 
-        Font f_comment = new Font("Dialog.plain", 0, 20);
-        jta2.setLineWrap(true);
-        jta2.setWrapStyleWord(true);
-        jta2.setMinimumSize(new Dimension(10, 10));
-        jta2.setFont(f_comment);
+        // log text entry:
+        LOGTEXT.setLineWrap(true);
+        LOGTEXT.setWrapStyleWord(true);
+        LOGTEXT.setMinimumSize(new Dimension(10, 10));
+        LOGTEXT.setForeground(Color.BLUE);
+        LOGTEXT.setFont(new Font("Dialog.plain", 0, 20));
+        ScrollablePanel sp = new ScrollablePanel();
+        BoxLayout layout = new BoxLayout(sp, BoxLayout.Y_AXIS);
+        sp.setLayout(layout);
+        sp.add(LOGTEXT);
+        sp.add(Box.createRigidArea(new Dimension(0, 10)));
+        JScrollPane jsp = new JScrollPane(sp);
+        jsp.getVerticalScrollBar().setUnitIncrement(16);
+        jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        jsp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        FRAME.getContentPane().add(jsp, BorderLayout.CENTER);
 
-        ScrollablePanel innerContainer = new ScrollablePanel();
-        BoxLayout layout = new BoxLayout(innerContainer, BoxLayout.Y_AXIS);
-        innerContainer.setLayout(layout);
+        // buttons:
+        JButton butSubmit = new JButton();
+        butSubmit.setText("Submit To Logbook");
+        butSubmit.addActionListener(new SubmitAction());
+        JButton butScreenshot = new JButton();
+        butScreenshot.setText("Add Screenshot");
+        //butScreenshot.addActionListener(new ScreenshotAction());
+        JPanel buttonPanel = new JPanel();
+        FRAME.getContentPane().add(buttonPanel, "South");
+        buttonPanel.add(butScreenshot);
+        buttonPanel.add(butSubmit);
 
-        innerContainer.add(jta1);
-        innerContainer.add(Box.createRigidArea(new Dimension(0, 10)));
+        JPanel titlePanel = new JPanel();
+        titlePanel.add(logTitleInst);
+        titlePanel.add(LOGTITLE);
+        //titlePanel.setMaximumSize(new Dimension(10, 20));
+        
+        JPanel topPanel = new JPanel();
+        //topPanel.setLayout(new GridLayout(2,1));
+        topPanel.add(titlePanel);
+        topPanel.add(logTextInst);
+        topPanel.setMaximumSize(new Dimension(10, 50));
 
-        innerContainer.add(jta2);
-        innerContainer.add(Box.createRigidArea(new Dimension(0, 10)));
+        JPanel instructPanel = new JPanel();
+        instructPanel.add(logTextInst);
 
-        JScrollPane scrollPane = new JScrollPane(innerContainer);
+        FRAME.getContentPane().add(instructPanel, "North");
+        //FRAME.getContentPane().add(topPanel, "North");
 
-        scrollPane.getVerticalScrollBar().setUnitIncrement(16);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-
-        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-
-
-        // ================= A radiobutton "Needs attention" ===================
-        rb_important = new JRadioButton("Needs special attention?");
-
-        // ============== The Submit button =================
-
-        JButton but_submit = new JButton();
-        but_submit.setText("Submit");
-        but_submit.addActionListener(new Action());
-
-        JPanel down_panel = new JPanel();
-        frame.getContentPane().add(down_panel, "South");
-        //down_panel.add(rb_important);
-        down_panel.add(but_submit);
-
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    public static String getTimeStamp()
-    {
-        Date date = new Date();
-
-        String str_year = String.format("%tY", date);
-        String str_month = String.format("%tm", date);
-        String str_day = String.format("%td", date);
-        String str_hour = String.format("%tH", date);
-        String str_min = String.format("%tM", date);
-        String str_sec = String.format("%tS", date);
-
-        String time_stamp = str_year+"_"+str_month+"_"+str_day+"_"+str_hour+"_"+str_min+"_"+str_sec;
-        return time_stamp;
+        FRAME.pack();
+        FRAME.setVisible(true);
     }
 
 
@@ -193,150 +174,72 @@ public class MakeLogEntry
     {
         System.err.println("NOT SUBMITTING ELOG");
 /*
-        //LogEntry entry = new LogEntry("", "TLOG");
-        LogEntry entry = new LogEntry("", "HBLOG");
+        LogEntry entry = new LogEntry("", LOGBOOKNAME);
         //entry.setEmailNotify("rafopar@jlab.org");
 
         try {
-            entry.addAttachment( img_path, "Snapshot of " + file_name + "window");
-            System.out.println("The title is: " + elog_Title);
+            entry.addAttachment( IMGPATH, "Snapshot of " + FILENAME + "window");
             entry.setTitle(elog_Title);	
-            String Setaratition_line = "\n ================= PVs ==================== \n";
-            entry.setBody("[figure:1] \n" + comments_to_submit + Setaratition_line + AdditionalText + "\n \n" + PV_list);
-
-            System.out.println("The PV list is " + PV_list);
-
+            entry.setBody(LOGTEXT.getText());
             if( rb_important.isSelected() ) // Checks whether this entry needs special attention
             {
                 entry.setSticky(true);
                 System.out.println("Needs Special Attention");
             }
-
-            lognumber = entry.submitNow();
+            long logNumber = entry.submitNow();
             //entry.submit();
-            System.out.println("Entry was submited under the lognumber " + lognumber);
+            System.out.println("Entry was submited under the logNumber " + logNumber);
             //System.out.println(entry.getXML());
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
         }
+        catch (Exception e) { System.out.println(e.getMessage()); }
 */
     }
 
-
-    // This method return list of values PVs corresponding to a particular keyword e.g chan, rdbg ydata
-    public String getPVs(String keyword)
-    {
-        Process p;
-        BufferedReader stdInput;
-        BufferedReader stdError;
-        String s = null;
-        String PV_sub_list = "";
-
-        try{
-            String[] command = {"/bin/sh", "-c", "caget `cat " + medm_dir+"/"+medm_name + " | grep " + keyword + "= | cut -d '=' -f2 | sort | sed -e s/" + "\\" + "\"// | sed -e s/" + "\\" + "\"//`"};
-            p = Runtime.getRuntime().exec(command);
-            stdInput = new BufferedReader(new InputStreamReader( p.getInputStream() ));
-
-            String line = null;
-
-            while ((line = stdInput.readLine())!= null)
-            {
-                System.out.println("line = " + line);
-                PV_sub_list = PV_sub_list +"\n" + line;
-            }
-
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        return PV_sub_list;
-    }
-
-//    private String rundbConn="jdbc:mysql://clondb1:3306/daq_clasrun";
-//    private String rundbUser="clasrun";
-//    private String rundbSessionName="clashps";
-
-    // This method should return current run number. 
-    //It will take the information from the "daq_clasdev" databas on clondb1 machine from the table sessions
     public int getRunNumber()
     {
         int cur_runnumber = 0;
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-        }catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } 
-
-        try{
+        try { Class.forName("com.mysql.jdbc.Driver"); }
+        catch (ClassNotFoundException e) { e.printStackTrace(); } 
+        try {
             BufferedReader stdInput;
             //Connection conn = DriverManager.getConnection("jdbc:mysql://clondb1:3306/daq_clasdev", "clasrun", "");
             Connection conn = DriverManager.getConnection("jdbc:mysql://clondb1:3306/daq_clasrun", "clasrun", "");
             Statement stmt = conn.createStatement();
             String query = "select runnumber from sessions where name=\"clashps\" ;" ;
-
             ResultSet rs = stmt.executeQuery(query) ;
-
-            while( rs.next() )
-            {
-                cur_runnumber =  rs.getInt("runnumber");
-            }
+            while (rs.next()) cur_runnumber=rs.getInt("runnumber");
         }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
+        catch (Exception e) { e.printStackTrace(); }
         return cur_runnumber;
     }
 
-
-    class Action implements ActionListener
+    class SubmitAction implements ActionListener
     {
         public void actionPerformed (ActionEvent e)
         {
-            comments_to_submit = jta2.getText();
-
-            comments_to_submit = comments_to_submit + fit_parameters;
-
-            System.out.println(comments_to_submit);
-
             SubmitElog();
-            //frame.setVisible(false);
-            frame.dispose();
-            //System.exit(0);
+            FRAME.dispose();
         }
     }
 
-
-    public void AddAdditionalText(String a)
+    /*
+    class ScreenshotAction implements ActionListener
     {
-        AdditionalText = AdditionalText + a;
-        System.out.println("Additional text is \n" + a);
+        public void actionPerformed (ActionEvent e)
+        {
+            TakeScreenshot();
+            FRAME.dispose();
+        }
     }
+    */
 
-
-    private static class ScrollablePanel extends JPanel implements Scrollable{
-        public Dimension getPreferredScrollableViewportSize() {
-            return super.getPreferredSize();
-        }
-
-        public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
-            return 16;
-        }
-
-        public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
-            return 16;
-        }
-
-        public boolean getScrollableTracksViewportWidth() {
-            return true;
-        }
-
-        public boolean getScrollableTracksViewportHeight() {
-            return false;
-        }
+    private static class ScrollablePanel extends JPanel implements Scrollable
+    {
+        public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction)  { return 16; }
+        public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) { return 16; }
+        public Dimension getPreferredScrollableViewportSize() { return super.getPreferredSize(); }
+        public boolean getScrollableTracksViewportWidth()  { return true; }
+        public boolean getScrollableTracksViewportHeight() { return false; }
     }
 
 }

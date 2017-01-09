25274c2b3c5e9909946e3923162efb5539079cb0
baltzell <baltzell@gmx.com>
2017-01-09 10:13:16 -0500

    harp_fitter.sh:  do not require harp_fitter to be in path

--- tools/harp_fitter/harp_fitter.sh
+++ tools/harp_fitter/harp_fitter.sh
@@ -2,4 +2,4 @@
 motorName=$1
 dataDir=/home/epics/DATA/HARP_SCANS
 lastFile=`ls -lht $dataDir/$motorName | grep $motorName | awk '{print$9}' | head -n1`
-harp_fitter $motorName/$lastFile
+$EPICS/tools/harp_fitter/harp_fitter $motorName/$lastFile

8acaec7782604cbfbe693f21b4613836a3c29b4a
baltzell <baltzell@gmx.com>
2017-01-11 20:15:29 -0500

    ioc_vme_reboot.js:  add wrapper script to run roc_reboot.tcl with confirm dialog

---
+++ css_share/iocs/scripts/ioc_vme_reboot.js
@@ -0,0 +1,14 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+iocName=widget.getMacroValue("ioc");
+
+// get rid of the "ioc" prefix, if it exists:
+host=iocName
+if (host.startsWith("ioc") host=iocName.slice(3)
+
+if (GUIUtil.openConfirmDialog("Really Reboot "+iocName+" ?"))
+{
+  cmd = "ssh clasrun@clon10 remote_reboot.tcl "+host;
+  ScriptUtil.executeSystemCommand(cmd,0);
+}
+

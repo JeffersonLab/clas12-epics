83a310fa1b84d0cffcf88c16b93e296fb7186356
baltzell <baltzell@gmx.com>
2017-01-25 19:00:53 -0500

    iocdclv_S2R2|S6R2:  swap in 2 spare gpib-eth converters (1 repaired)

--- apps/iocBoot/iocdclv_S2R2/st.cmd
+++ apps/iocBoot/iocdclv_S2R2/st.cmd
@@ -1,6 +1,6 @@
 #!../../bin/linux-x86_64/A6551
 
-epicsEnvSet("GPIB","hallb-gpib06")
+epicsEnvSet("GPIB","hallb-gpib12")
 epicsEnvSet("PORT","S2R2")
 epicsEnvSet("SECREG","SEC2_R2")
 
--- apps/iocBoot/iocdclv_S6R2/st.cmd
+++ apps/iocBoot/iocdclv_S6R2/st.cmd
@@ -1,6 +1,6 @@
 #!../../bin/linux-x86_64/A6551
 
-epicsEnvSet("GPIB","hallb-gpib04")
+epicsEnvSet("GPIB","hallb-gpib17")
 epicsEnvSet("PORT","S6R2")
 epicsEnvSet("SECREG","SEC6_R2")
 

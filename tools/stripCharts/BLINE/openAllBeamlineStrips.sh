046b7722b0228e00e2ec0b28dbd5f02d945f7ecd
baltzell <baltzell@gmx.com>
2017-02-04 09:20:15 -0500

    openAllBeamStrips.sh:  fix, stps got corrupted

--- tools/stripCharts/BLINE/openAllBeamlineStrips.sh
+++ tools/stripCharts/BLINE/openAllBeamlineStrips.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-d=$EPICS/tools/stripCharts/BLINE
+d=$EPICS/tools/stripCharts
 
 x='-xrm StripTool.StripGraph.width:1000'
 y='-xrm StripTool.StripGraph.height:300'
 
-StripTool $x $y $d/beamline_xpos.stp    >& /dev/null &
-StripTool $x $y $d/beamline_ypos.stp    >& /dev/null &
-StripTool $x $y $d/beamline_current.stp >& /dev/null &
-StripTool $x $y $d/beamline_scalers.stp >& /dev/null &
+#StripTool $x $y $d/beamline_xpos.stp    >& /dev/null &
+#StripTool $x $y $d/beamline_ypos.stp    >& /dev/null &
+#StripTool $x $y $d/beamline_current.stp >& /dev/null &
+#StripTool $x $y $d/beamline_scalers.stp >& /dev/null &
+
+StripTool $x $y $d/BLINE2/Current    >& /dev/null &
+StripTool $x $y $d/BLINE2/Scalers    >& /dev/null &
+StripTool $x $y $d/BLINE2/X-Position >& /dev/null &
+StripTool $x $y $d/BLINE2/Y-Position >& /dev/null &
 

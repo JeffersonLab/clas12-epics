4172a1311086f37f81f588c934dc5e43fa0672a3
baltzell <baltzell@gmx.com>
2017-02-06 15:39:35 -0500

    iocbta:  add old bta setup

---
+++ apps/iocBoot/iocbta/st.cmd
@@ -0,0 +1,29 @@
+#!../../bin/linux-x86_64/bta
+
+## You may have to change bta to something else
+## everywhere it appears in this file
+
+< envPaths
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/bta.dbd"
+bta_registerRecordDeviceDriver pdbbase
+
+dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db", "IOC=$(IOC)")
+#dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:"
+
+#dbLoadRecords("db/bta_suppl.db","hall=Z,ioc=classc6");
+
+dbLoadRecords("db/mcc_fsd.db")
+
+cd "${TOP}/iocBoot/${IOC}"
+
+#< save_restore.cmd
+
+iocInit
+
+#makeAutosaveFiles()
+#create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+#create_monitor_set("info_settings.req", 30, "P=${IOC}:")
+

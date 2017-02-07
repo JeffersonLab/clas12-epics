6c51e100044cbcae5d0c608cd53573dc145b44b9
baltzell <baltzell@gmx.com>
2017-02-07 16:05:18 -0500

    iocbta/st.cmd:  enable autsave, prepare for classc6 decommissioning

--- apps/iocBoot/iocbta/st.cmd
+++ apps/iocBoot/iocbta/st.cmd
@@ -11,17 +11,17 @@ dbLoadDatabase "dbd/bta.dbd"
 bta_registerRecordDeviceDriver pdbbase
 
 dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db", "IOC=$(IOC)")
-#dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:"
+dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:"
 
-#dbLoadRecords("db/bta_suppl.db","hall=Z,ioc=classc6");
+dbLoadRecords("db/bta_suppl.db","hall=B,ioc=classc6");
 
 cd "${TOP}/iocBoot/${IOC}"
 
-#< save_restore.cmd
+< save_restore.cmd
 
 iocInit
 
-#makeAutosaveFiles()
-#create_monitor_set("info_positions.req", 5, "P=${IOC}:")
-#create_monitor_set("info_settings.req", 30, "P=${IOC}:")
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+create_monitor_set("info_settings.req", 30, "P=${IOC}:")
 

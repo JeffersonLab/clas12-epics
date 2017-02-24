8bc846ead922f0383954c59c9214c8b289d5d2f3
baltzell <baltzell@gmx.com>
2017-02-24 12:07:24 -0500

    iocstruckDaq:  import from hps

---
+++ apps/iocBoot/iocstruckDaq/st.cmd
@@ -0,0 +1,33 @@
+#!../../bin/linux-x86_64/wf2root
+############################################################################
+< envPaths
+############################################################################
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase("dbd/wf2root.dbd")
+wf2root_registerRecordDeviceDriver(pdbbase)
+
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES","1000000")
+
+## Load record instances
+dbLoadRecords("${DEVIOCSTATS}/db/iocAdminSoft.db","IOC=${IOC}")
+#dbLoadRecords("${AUTOSAVE}/asApp/Db/save_restoreStatus.db", "P=${IOC}:")
+#
+dbLoadTemplate("iocBoot/${IOC}/wf2root.substitutions")
+
+cd "${TOP}/iocBoot/${IOC}"
+
+## autosave setup
+#< save_restore.cmd
+
+dbl > pv.list
+iocInit
+
+## autosave startup
+## Handle autosave 'commands' contained in loaded databases.
+#makeAutosaveFiles()
+#create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+#create_monitor_set("info_settings.req", 30, "P=${IOC}:")
+#create_monitor_set("wf2root_settings.req", 30, "P=B_TORUS:DAQ_REC:")
+

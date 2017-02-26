3e5ce32dd8f53ce738ebe74e5998ad35ada3a947
baltzell <baltzell@gmx.com>
2017-02-25 19:46:56 -0500

    iocsvtR1/st.cmd:  fix hostname (vmetlsvt#)

--- apps/iocBoot/iocsvtR1/st.cmd
+++ apps/iocBoot/iocsvtR1/st.cmd
@@ -1,5 +1,5 @@
 
-dbLoadRecords("svtWienerCrate.db","HOST=svtvme1")
+dbLoadRecords("svtWienerCrate.db","HOST=vmetlsvt1")
 dbLoadRecords("svtR1-mpv-iseg.substitutions")
 dbLoadRecords("svtR1-hfcb.substitutions")
 

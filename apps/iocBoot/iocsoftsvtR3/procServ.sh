f37a5d4c137aa618f49a8ab4e40d2b23f050ac62
baltzell <baltzell@gmx.com>
2017-03-17 19:19:02 -0400

    ioc*svt*/procServ.sh:  run from pro

--- apps/iocBoot/iocsoftsvtR3/procServ.sh
+++ apps/iocBoot/iocsoftsvtR3/procServ.sh
@@ -3,7 +3,7 @@
 ioc=iocsoftsvtR3
 port=20004
 logdir=/usr/clas12/DATA/logs
-epics=/home/baltzell/clas12-epics-svtiocs
+epics=/usr/clas12/release/pro/epics
 
 procServ -n $ioc -i^D^C --logfile $logdir/$ioc.log --logstamp -c $epics/apps/iocBoot/$ioc $port ./st.cmd
 

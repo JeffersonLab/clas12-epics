ad6beee1103ebb2207d1bc7e07ffcdb1ae30a4c1
baltzell <baltzell@gmx.com>
2017-04-02 16:04:29 -0400

    iocsvtChiller->iocsoftsvtChiller:  rename to old ops name (to avoid conflict with hps)

--- apps/iocBoot/iocsvtChiller/procServ.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ioc=iocsvtChiller
-port=20001
-logdir=/usr/clas12/DATA/logs
-epics=/usr/clas12/release/pro/epics
-
-procServ -n $ioc -i^D^C --logfile $logdir/$ioc.log --logstamp -c $epics/apps/iocBoot/$ioc $port ./st.cmd
-

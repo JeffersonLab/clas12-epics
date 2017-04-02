e03acab8fceb6cda4b64d7b679ead709ec10b01a
baltzell <baltzell@gmx.com>
2017-04-01 20:00:37 -0400

    iocsoftsvtIntlk:  add summing records

--- apps/iocBoot/iocsoftsvtIntlk/st.cmd
+++ apps/iocBoot/iocsoftsvtIntlk/st.cmd
@@ -9,6 +9,10 @@ dbLoadDatabase("dbd/svtCtrlApp.dbd")
 svtCtrlApp_registerRecordDeviceDriver pdbbase
 
 dbLoadTemplate("db/svtIntlk.substitutions")
+
+dbLoadTemplate("db/svt-sums.substitutions")
+dbLoadRecords("db/svt-sums-alarm.db")
+
 dbLoadRecords("db/iocAdminSoft.db", "IOC=${IOC}")
 
 cd "${TOP}/iocBoot/${IOC}"

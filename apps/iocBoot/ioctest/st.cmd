46d713fadabd26e9e73982ac9ba209a045c80133
baltzell <baltzell@gmx.com>
2017-04-08 19:12:03 -0400

    ioctest/st.cmd:  increase EPICS_CA_MAX_ARRAY_BYTES

--- apps/iocBoot/ioctest/st.cmd
+++ apps/iocBoot/ioctest/st.cmd
@@ -5,6 +5,7 @@ dbLoadDatabase("dbd/testApp.dbd")
 testApp_registerRecordDeviceDriver pdbbase
 dbLoadRecords("db/iocAdminSoft.db", "IOC=${IOC}")
 
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES","10000000")
 
 # NELM has to be at least as large as #PVs in FNAME
 

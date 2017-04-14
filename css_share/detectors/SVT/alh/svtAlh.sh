ca9affcaca91fb93dcbe1ee5bccece1687f61eea
baltzell <baltzell@gmx.com>
2017-04-14 19:04:59 -0400

    svtAlh.sh  exec alh

--- css_share/detectors/SVT/alh/svtAlh.sh
+++ css_share/detectors/SVT/alh/svtAlh.sh
@@ -1,8 +1,9 @@
+#!/bin/sh
 
 noise=/usr/share/sounds/speech-dispatcher/test.wav 
 logdir=/usr/clas12/DATA/logs/SVT
 epics=/usr/clas12/release/pro/epics/css_share/detectors/SVT/alh
 
 #alh -p $noise -m 1000000 -l $logdir -f $epics SVT.alhConfig
-alh -m 1000000 -l $logdir -f $epics SVT.alhConfig
+exec alh -m 1000000 -l $logdir -f $epics SVT.alhConfig
 

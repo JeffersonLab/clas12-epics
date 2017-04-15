7443478bc6b66801d895cd92c6df41abc1d31217
baltzell <baltzell@gmx.com>
2017-04-15 18:56:49 -0400

    sy1527.c: start support for LV 2518 board

--- apps/syX527App/src/sy1527.c
+++ apps/syX527App/src/sy1527.c
@@ -60,6 +60,7 @@ int NCFEDOWNERR[MAX_HVPS];
 
 static int  nA1520param = 16; // my: 16
 static int  nA1535param = 16;
+static int  nA2518Aparam = 18;
 static int  nA1536HDMparam = 18;
 
 static char A1535param[MAX_PARAM][MAX_CAEN_NAME] = {
@@ -71,7 +72,10 @@ static char A1520param[MAX_PARAM][MAX_CAEN_NAME] = {
 static char A1536HDMparam[MAX_PARAM][MAX_CAEN_NAME] = {
                 "V0Set","I0Set","V1Set","I1Set","RUp","RDWn","Trip","SVMax",
                 "VMon","IMon","Status","Pw","POn","TripInt","TripExt","PDwn","ImRange","Pol"};
-
+static char A2518Aparam[MAX_PARAM][MAX_CAEN_NAME] = {
+                "V0Set","I0Set","RUpTime","RDwTime","UNVThr","OVVThr","VMon",
+                "VCon","IMon","Temp","Status","Pw","TripInt","TripExt",
+                "ChToGroup","OnGrDel","OffGrDel","Intck"};
 
 ///---------------------------------------------------------------
 /* some useful macros */
@@ -450,9 +454,8 @@ sy1527GetMap(unsigned int id)
         if(!strcmp(Measure[id].board[i].modelname,"A1535") ||
             !strcmp(Measure[id].board[i].modelname,"A1536") ||
             !strcmp(Measure[id].board[i].modelname,"A1733") ||
-            !strcmp(Measure[id].board[i].modelname,"A1737")) // my: was A1520
+            !strcmp(Measure[id].board[i].modelname,"A1737"))
         {
-
           printf("---> found board %s\n",Measure[id].board[i].modelname);
           Measure[id].board[i].nparams = nA1535param;
           Demand[id].board[i].nparams = nA1535param;
@@ -516,6 +519,33 @@ sy1527GetMap(unsigned int id)
             strcpy(Demand[id].board[i].parnames[j],A1536HDMparam[j]);
 
             strcpy(ParName,Measure[id].board[i].parnames[j]);
+            ret=CAENHVGetChParamProp(name,i,ChList[0],ParName,"Type",&tipo);
+            if(ret != CAENHV_OK)
+            {
+              printf("CAENHVGetChParamProp error: %s (num. %d) ParName=>%s<\n",
+                  CAENHVGetError(name),ret,ParName);
+              Measure[id].board[i].nchannels = 0;
+              Demand[id].board[i].nchannels = 0;
+              return(CAENHV_SYSERR);
+            }
+            else
+            {
+              Measure[id].board[i].partypes[j] = tipo;
+              Demand[id].board[i].partypes[j] = tipo;
+            }
+          }
+        }
+        else if(!strcmp(Measure[id].board[i].modelname,"A2518A"))
+        {
+          printf("---> found board %s\n",Measure[id].board[i].modelname);
+          Measure[id].board[i].nparams = nA2518Aparam;
+          Demand[id].board[i].nparams = nA2518Aparam;
+          for(j=0; j<Measure[id].board[i].nparams; j++)
+          {
+            strcpy(Measure[id].board[i].parnames[j],A2518Aparam[j]);
+            strcpy(Demand[id].board[i].parnames[j],A2518Aparam[j]);
+            strcpy(ParName,Measure[id].board[i].parnames[j]);
+
             ret=CAENHVGetChParamProp(name,i,ChList[0],ParName,"Type",&tipo);
             if(ret != CAENHV_OK)
             {
@@ -755,7 +785,6 @@ sy1527Start(unsigned id_nowused, char *ip_address)
     strcpy(Measure[id].name, name); 
     Demand[id].id = ret;
     strcpy(Demand[id].name, name);
-    printf("aaaaaaaaaaaaaaaaaaaaaaa %s\n",Measure[id].name);
   }
   else
   {
@@ -1324,3 +1353,5 @@ sy1527BoardSmiControl(char *smi_obj_name, unsigned int id, unsigned int board,
   return CAENHV_OK;
 
 }
+
+

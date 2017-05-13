96a870ddf36568d3887936ea4895d2e6f144d985
baltzell <baltzell@gmx.com>
2017-05-13 16:24:45 -0400

    iocclonioc?:  iocs for mem/cpu loads

---
+++ apps/iocBoot/iocclonioc2/st.cmd
@@ -0,0 +1,23 @@
+#!../../bin/linux-x86_64/pcMonitor
+
+< envPaths
+
+cd ${TOP}
+
+dbLoadDatabase("dbd/pcMonitor.dbd",0,0)
+pcMonitor_registerRecordDeviceDriver
+
+dbLoadRecords("db/iocAdminSoft.db","IOC=${IOC}")
+dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:")
+dbLoadRecords("db/pcMonitor.db","NAME=clonioc2")
+
+cd ${TOP}/iocBoot/${IOC}
+
+< save_restore.cmd
+
+iocInit
+
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+create_monitor_set("info_settings.req", 30, "P=${IOC}:")
+

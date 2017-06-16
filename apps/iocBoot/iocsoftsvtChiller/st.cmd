a4625de1561dae69db26d5f64c1983db645569d5
baltzell <baltzell@gmx.com>
2017-06-16 09:10:49 -0400

    iocsoftsvtChiller:  setup for A40

--- apps/iocBoot/iocsoftsvtChiller/st.cmd
+++ apps/iocBoot/iocsoftsvtChiller/st.cmd
@@ -9,30 +9,33 @@ cd "${TOP}"
 dbLoadDatabase("dbd/svtChiller.dbd")
 svtChiller_registerRecordDeviceDriver(pdbbase)
 
-#drvAsynIPPortConfigure("L0",svtchiller1.acc.jlab.org:10001,0,0,0)
+# For the Lauda ECO chiller (port L0 is hard-coded in svtChiller.db):
 drvAsynIPPortConfigure("L0",svtchiller1.jlab.org:10001,0,0,0)
 
-## debugging...
-#asynSetTraceMask("SER3",-1,0x09)
-#asynSetTraceIOMask("SER3",-1,0x02)
+# For the Anova A40:
+drvAsynIPPortConfigure("L1",hallb-moxa1.jlab.org:4005)
 
-## Load record instances
-dbLoadRecords("db/svtChiller.db")
+# For the Lauda ECO chiller:
+#dbLoadRecords("db/svtChiller.db")
+
+# For the Anova A40:
+dbLoadRecords("db/anova.db","P=B_SVT_,R=CHILLER_,PORT=L1")
+
 
 dbLoadRecords("db/iocAdminSoft.db", "IOC=${IOC}")
 dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")
 
 cd "${TOP}/iocBoot/${IOC}"
 
-## autosave setup
-< save_restore.cmd
+# autosave setup
+#< save_restore.cmd
 
 iocInit
 
-## Handle autosave 'commands' contained in loaded databases.
-makeAutosaveFiles()
-create_monitor_set("info_positions.req", 5, "P=${IOC}:")
-create_monitor_set("info_settings.req", 30, "P=${IOC}:")
+# Handle autosave 'commands' contained in loaded databases.
+#makeAutosaveFiles()
+#create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+#create_monitor_set("info_settings.req", 30, "P=${IOC}:")
 
 dbl > pv.list
 

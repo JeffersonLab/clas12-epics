e059a59f4d9d9f77e4a53b20376ae4186420a73a
baltzell <baltzell@gmx.com>
2017-06-16 16:36:18 -0400

    iocsoftsvtChiller:  setup to swap between chillers

--- apps/iocBoot/iocsoftsvtChiller/st.cmd
+++ apps/iocBoot/iocsoftsvtChiller/st.cmd
@@ -9,18 +9,18 @@ cd "${TOP}"
 dbLoadDatabase("dbd/svtChiller.dbd")
 svtChiller_registerRecordDeviceDriver(pdbbase)
 
-# For the Lauda ECO chiller (port L0 is hard-coded in svtChiller.db):
-drvAsynIPPortConfigure("L0",svtchiller1.jlab.org:10001,0,0,0)
+####################################
+# For the Lauda ECO (port L0 is hard-coded in svtChiller.db):
+#drvAsynIPPortConfigure("L0",svtchiller1.jlab.org:10001,0,0,0)
+#dbLoadRecords("db/svtChiller.db")
+####################################
 
+####################################
 # For the Anova A40:
 drvAsynIPPortConfigure("L1",hallb-moxa1.jlab.org:4005)
-
-# For the Lauda ECO chiller:
-#dbLoadRecords("db/svtChiller.db")
-
-# For the Anova A40:
 dbLoadRecords("db/anova.db","P=B_SVT_,R=CHILLER_,PORT=L1")
-
+dbLoadRecords("db/svtChiller-anova2eco.db","P=B_SVT_CHILLER"))
+####################################
 
 dbLoadRecords("db/iocAdminSoft.db", "IOC=${IOC}")
 dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")
@@ -28,14 +28,14 @@ dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")
 cd "${TOP}/iocBoot/${IOC}"
 
 # autosave setup
-#< save_restore.cmd
+< save_restore.cmd
 
 iocInit
 
 # Handle autosave 'commands' contained in loaded databases.
-#makeAutosaveFiles()
-#create_monitor_set("info_positions.req", 5, "P=${IOC}:")
-#create_monitor_set("info_settings.req", 30, "P=${IOC}:")
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+create_monitor_set("info_settings.req", 30, "P=${IOC}:")
 
 dbl > pv.list
 

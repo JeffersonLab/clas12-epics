c2f421e836df5dcb92c4bb474604bf5cf4207f76
Wesley Moore <wmoore@jlab.org>
2017-06-21 09:13:33 -0400

    mm gas: basic config for bmt/fmt/ftt iocs

---
+++ apps/iocBoot/iocmmGas/st_fmt.cmd
@@ -0,0 +1,45 @@
+#!../../bin/linux-x86_64/mmGas
+############################################################################
+< envPaths
+############################################################################
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase("dbd/mmGas.dbd")
+mmGas_registerRecordDeviceDriver(pdbbase)
+
+## Configure modbus ports
+drvAsynIPPortConfigure("TCP506","129.57.86.139:506",0,0,1)
+
+# modbusInterposeConfig(portName, linkType, timeoutMsec, writeDelayMsec)
+modbusInterposeConfig("TCP506",0,5000,0)
+
+# Debugging...
+#asynSetTraceMask("TCP506",0,9)
+#asynSetTraceIOMask("TCP506",0,4)
+
+#drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction,
+#                       modbusStartAddress, modbusLength, dataType, pollMsec,
+#                       "plcType")
+# 32-bit integers (Function code = 3)
+drvModbusAsynConfigure("R506", "TCP506", 0, 3,  0, 33, 0, 1000, "Siemens")
+drvModbusAsynConfigure("W506", "TCP506", 0, 6,  32, 1, 0, 1000, "Siemens")
+
+## Load record instances
+# dbLoadRecords("db/save_restoreStatus.db", "P=$(IOC):")
+# dbLoadRecords("db/iocAdminSoft.db","P=$(IOC)")
+dbLoadTemplate("db/mmgas_fmt.substitutions")
+
+cd "${TOP}/iocBoot/${IOC}"
+
+## autosave setup
+# < save_restore.cmd
+
+dbl > pv.list
+iocInit
+
+## Handle autosave 'commands' contained in loaded databases.
+#makeAutosaveFiles()
+#create_monitor_set("info_positions.req", 5, "P=:")
+#create_monitor_set("info_settings.req", 30, "P=xxx:")
+
---
+++ apps/iocBoot/iocmmGas/st_ftt.cmd
@@ -0,0 +1,45 @@
+#!../../bin/linux-x86_64/mmGas
+############################################################################
+< envPaths
+############################################################################
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase("dbd/mmGas.dbd")
+mmGas_registerRecordDeviceDriver(pdbbase)
+
+## Configure modbus ports
+drvAsynIPPortConfigure("TCP507","129.57.86.151:507",0,0,1)
+
+# modbusInterposeConfig(portName, linkType, timeoutMsec, writeDelayMsec)
+modbusInterposeConfig("TCP507",0,5000,0)
+
+# Debugging...
+#asynSetTraceMask("TCP507",0,9)
+#asynSetTraceIOMask("TCP507",0,4)
+
+#drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction,
+#                       modbusStartAddress, modbusLength, dataType, pollMsec,
+#                       "plcType")
+# 32-bit integers (Function code = 3)
+drvModbusAsynConfigure("R507", "TCP507", 0, 3,  0, 33, 0, 1000, "Siemens")
+drvModbusAsynConfigure("W507", "TCP507", 0, 6,  32, 1, 0, 1000, "Siemens")
+
+## Load record instances
+# dbLoadRecords("db/save_restoreStatus.db", "P=$(IOC):")
+# dbLoadRecords("db/iocAdminSoft.db","P=$(IOC)")
+dbLoadTemplate("db/mmgas_ftt.substitutions")
+
+cd "${TOP}/iocBoot/${IOC}"
+
+## autosave setup
+# < save_restore.cmd
+
+dbl > pv.list
+iocInit
+
+## Handle autosave 'commands' contained in loaded databases.
+#makeAutosaveFiles()
+#create_monitor_set("info_positions.req", 5, "P=:")
+#create_monitor_set("info_settings.req", 30, "P=xxx:")
+

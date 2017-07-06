814720ccbab7bf20c6d59cb75b1ac850b69566c5
baltzell <baltzell@gmx.com>
2017-07-06 19:14:55 -0400

    saclayTarget:  add status bits

--- apps/iocBoot/iocsaclaytgt/st.cmd
+++ apps/iocBoot/iocsaclaytgt/st.cmd
@@ -18,15 +18,22 @@ modbusInterposeConfig("ETH1",1,1000,0)
 #drvModbusAsynConfigure(
 #  "portName", "tcpPortName", slaveAddress, modbusFunction,
 #  modbusStartAddress, modbusLength, dataType, pollMsec,"plcType")
-# 32-bit integers (Function code = 3)
 
 
-drvModbusAsynConfigure("C370x600", "ETH1", 1, 3, 0x600,  40, 0, 2000, "C370")
-drvModbusAsynConfigure("C370x640", "ETH1", 1, 3, 0x640,   6, 0, 2000, "C370")
-drvModbusAsynConfigure("C370x702", "ETH1", 1, 3, 0x702,   2, 0, 2000, "C370")
+# 32-bit floats:
+drvModbusAsynConfigure("C370x0600","ETH1", 1, 3, 0x0600, 40, 0, 2000, "C370")
+drvModbusAsynConfigure("C370x0640","ETH1", 1, 3, 0x0640,  6, 0, 2000, "C370")
+drvModbusAsynConfigure("C370x0702","ETH1", 1, 3, 0x0702,  2, 0, 2000, "C370")
 drvModbusAsynConfigure("C370x3800","ETH1", 1, 3, 0x3800, 20, 0, 2000, "C370")
 
+# bits:
+drvModbusAsynConfigure("C370x1880","ETH1", 1, 1, 0x1880, 24, 0, 2000, "C370")
+drvModbusAsynConfigure("C370x1920","ETH1", 1, 1, 0x1920, 21, 0, 2000, "C370")
+drvModbusAsynConfigure("C370x6622","ETH1", 1, 1, 0x6622, 32, 0, 2000, "C370")
+drvModbusAsynConfigure("C370x1C49","ETH1", 1, 1, 0x1C49,  1, 0, 2000, "C370")
+
 dbLoadTemplate("db/saclayTarget.substitutions")
+dbLoadRecords("db/saclayTarget-aliases.db","P=B_SACLAYTGT"))
 
 dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db", "IOC=$(IOC)")
 dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:")
@@ -44,4 +51,6 @@ caPutLogInit("clonioc1:7011")
 makeAutosaveFiles()
 create_monitor_set("info_positions.req","","")
 create_monitor_set("info_settings.req","","")
+
+dbl > pv.list
 

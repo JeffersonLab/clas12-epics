f556c953d0a6ca4577c0068fa1c8aeb0500a5433
baltzell <baltzell@gmx.com>
2017-07-07 17:25:00 -0400

    css_share/iocs:  script now accepts comma-separated file list

--- css_share/iocs/scripts/ioc_health_generic.js
+++ css_share/iocs/scripts/ioc_health_generic.js
@@ -1,13 +1,12 @@
 importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
-//widget.removeAllChildren();
 
 // Macro "F" is the name of a file containing a list of ioc names, one per row.
+// It can also be a comma-separated list of file names.
+//
 // If a line doesn't start with "ioc", insert a blank gap instead.  Additional
 // columns are options (the only currently valid one is "noautosave");
 
-filename=widget.getMacroValue("F");
-var lines=FileUtil.readTextFile(filename,widget);
-lines=lines.split("\n")
+var fileNames=widget.getMacroValue("F").split(",");
 
 function insertGap(size)
 {
@@ -18,35 +17,8 @@ function insertGap(size)
     widget.addChildToBottom(line);
 }
 
-var jj=0;
-
-for (var ii=0; ii<lines.length; ii++)
+function insertIoc(iocName,opiFile)
 {
-    if (!lines[ii].startsWith("ioc")) {
-        insertGap(15);
-        continue;
-    }
-
-    var iocName=lines[ii];
-    var autosave=true;
-    var vxworks=false;
-
-    columns=lines[ii].split(" ");
-    if (columns.length>1) {
-        iocName=columns[0];
-        for (var kk=1; kk<columns.length; kk++) {
-            if (columns[kk].equals("noautosave")) autosave=false;
-            else if (columns[kk].equals("vxworks")) vxworks=true;
-        }
-    }
-
-    var opiFile = "ioc_chan";
-    if (!vxworks) opiFile += "_soft";
-    else opiFile += "_vxworks";
-    if (!vxworks && jj%2) opiFile += "_light";
-    if (autosave) opiFile += "_autosave";
-    opiFile += ".opi";
-
     var lc = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");
     lc.setPropertyValue("opi_file",opiFile);
     //try   { lc.setPropertyValue("resize_behaviour",1); }
@@ -57,8 +29,45 @@ for (var ii=0; ii<lines.length; ii++)
     lc.setPropertyValue("background_color","Header_Background");
     lc.addMacro("ioc",iocName);
     widget.addChildToBottom(lc);
+}
 
-    insertGap(1);
-    jj++;
+for (var iFile=0; iFile<fileNames.length; iFile++) {
+
+    if (iFile>0) insertGap(15);
+
+    var lines=FileUtil.readTextFile(fileNames[iFile],widget);
+    lines=lines.split("\n");
+
+    for (var ii=0,jj=0; ii<lines.length; ii++)
+    {
+        if (!lines[ii].startsWith("ioc")) {
+            insertGap(15);
+            continue;
+        }
+
+        var iocName=lines[ii];
+        var autosave=true;
+        var vxworks=false;
+
+        columns=lines[ii].split(" ");
+        if (columns.length>1) {
+            iocName=columns[0];
+            for (var kk=1; kk<columns.length; kk++) {
+                if (columns[kk].equals("noautosave")) autosave=false;
+                else if (columns[kk].equals("vxworks")) vxworks=true;
+            }
+        }
+
+        var opiFile = "ioc_chan";
+        if (!vxworks) opiFile += "_soft";
+        else opiFile += "_vxworks";
+        if (!vxworks && jj%2) opiFile += "_light";
+        if (autosave) opiFile += "_autosave";
+        opiFile += ".opi";
+
+        insertIoc(iocName,opiFile);
+        insertGap(1);
+        jj++;
+    }
 }
 

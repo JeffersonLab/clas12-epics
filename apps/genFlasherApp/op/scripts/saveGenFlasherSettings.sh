56a3bd2eb53f2f678fdb44b512f4c8a5fc02cfd5
baltzell <baltzell@gmx.com>
2017-07-27 19:59:41 -0400

    genFlasherApp:  commit for Ken

--- apps/genFlasherApp/op/scripts/saveGenFlasherSettings.sh
+++ apps/genFlasherApp/op/scripts/saveGenFlasherSettings.sh
@@ -1,24 +1,32 @@
 #!/bin/sh
-P=$1
-outfile=`caget -S -t ${1}:DATA_FILE_SAVE`
-
-
-if [ -e "$outfile" ]; then
-
-    if ! zenity --title="Confirm overwrite" --question --text "$outfile already exists. Overwrite?"; then
-	exit;
-    else
-	/bin/rm $outfile
-    fi
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <module> <0|1>"
+    exit 
 fi
 
+P="$1:F$2"
+today=`date`
 
-for reg in "RO0" "RO1" "RO2" "RO3" "RO4" "RO5" "RO6" "RO7" "RO8" "RO9" "RO10" "J3_FREQ" "J4_FREQ"
+outfile=`caget -S -t ${P}_DATA_FILE_SAVE`
+
+echo "#Settings for ${P} saved on ${today}" > ${outfile}
+echo "#To load these back into flasher F0/F1 of the module, run the following command:" >> ${outfile}
+echo "#./loadGenFlasherSettings.sh ${1} <0|1>" >> ${outfile}
+echo "#These load/save would normally be done by clicking the relevant button on the CSS GUI" >> ${outfile}
+echo  >> ${outfile}
+echo "#The Voltage are the raw numbers on the digital potentiometers in the module" >> ${outfile}
+echo "#The equivalent coltages are shown in the commented lines below" >> ${outfile}
+echo >> ${outfile}
+for pv in "V1_WRITE" "V2_WRITE" "VLED_WRITE" "FREQ_WRITE"
 do
-    chan=${P}:${reg}
-    val=`caget -t ${chan}`
-    echo "saving: $chan $val"
-    echo "$chan $val" >> ${outfile}
+    val=`caget -t ${P}_${pv}`
+    echo "saving: ${P}_$pv $val"
+    echo "$pv $val" >> ${outfile}
+done
+for pv in "V1_SET" "V2_SET" "VLED_SET"
+do
+    val=`caget -t ${P}_${pv}`
+    echo "#$pv $val" >> ${outfile}
 done
 
 exit

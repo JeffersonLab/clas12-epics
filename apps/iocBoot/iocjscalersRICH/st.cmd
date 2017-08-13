def6973f235cac4213c1825dac93f66f8da2d1f9
baltzell <baltzell@gmx.com>
2017-08-13 10:08:35 -0400

    rich:  prepare scaler sums/arrays

--- apps/iocBoot/iocjscalersRICH/st.cmd
+++ apps/iocBoot/iocjscalersRICH/st.cmd
@@ -19,6 +19,11 @@ dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")
 
 dbLoadTemplate("db/jscalers_RICH_SSP.substitutions")
 dbLoadRecords("db/jscaler_RICH_ChannelMap.db")
+dbLoadRecords("db/jscalers_RICH_sums.db")
+
+dbLoadRecords("db/waveformApp.db","P=B_DET_RICH_SCALERS_,R=ROWS:,NELM=23,FTVL=FLOAT,PERIOD=5,FNAME=pvlists/rows.txt")
+dbLoadRecords("db/waveformApp.db","P=B_DET_RICH_SCALERS_,R=PMTS:,NELM=391,FTVL=FLOAT,PERIOD=5,FNAME=pvlists/pmts.txt")
+
 cd ${TOP}/iocBoot/${IOC}
 
 < save_restore.cmd
@@ -28,5 +33,8 @@ iocInit
 makeAutosaveFiles()
 create_monitor_set("info_positions.req", 5, "P=${IOC}:")
 create_monitor_set("info_settings.req", 30, "P=${IOC}:")
+
+#seq waveform, "P=B_DET_RICH_SCALERS_,R=ROWS:"
+#seq waveform, "P=B_DET_RICH_SCALERS_,R=PMTS:"
 
 dbl > pv.list

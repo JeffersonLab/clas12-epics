062cc4b8419398b4c1223127d4bae2d8e05d7165
baltzell <baltzell@gmx.com>
2017-08-17 10:59:10 -0400

    genflasher:  FTOF has no flasher

--- apps/iocBoot/iocgenflasher/st.cmd
+++ apps/iocBoot/iocgenflasher/st.cmd
@@ -28,17 +28,14 @@ drvAsynIPPortConfigure("L1","fastlightpulser1:5000 UDP",0,0,0)
 
 ## Load record instances
 dbLoadRecords("db/iocAdminSoft.db", "IOC=$(IOC)")
-dbLoadRecords("db/genFlasher.db","P=B_HW_FLASHER_HTCC-FTOF,R=:asyn,PORT=L1");
-#dbLoadRecords("db/genFlasher.db","P=B_HW_GENFLASHER_HTCC-FTOF,R=:asyn,PORT=L1");
+dbLoadRecords("db/genFlasher.db","P=B_HW_FLASHER_HTCC-CTOF,R=:asyn,PORT=L1");
+#dbLoadRecords("db/genFlasher.db","P=B_HW_GENFLASHER_HTCC-CTOF,R=:asyn,PORT=L1");
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
-## Start any sequence programs
-#seq sncxxx,"user=klivHost"
-
 #init some values
-dbpf("B_HW_FLASHER_HTCC-FTOF:F0_DATA_FILE_LOAD","/home/clasrun/htcc/flasher/HTCC_FlasherDefault.dat")
-dbpf("B_HW_FLASHER_HTCC-FTOF:F0_DATA_FILE_SAVE","/home/clasrun/htcc/flasher/HTCC_FlasherDefault.dat")
-dbpf("B_HW_FLASHER_HTCC-FTOF:F1_DATA_FILE_LOAD","/home/clasrun/htcc/flasher/FTOF_FlasherDefault.dat")
-dbpf("B_HW_FLASHER_HTCC-FTOF:F1_DATA_FILE_SAVE","/home/clasrun/htcc/flasher/FTOF_FlasherDefault.dat")
+dbpf("B_HW_FLASHER_HTCC-CTOF:F0_DATA_FILE_LOAD","/home/clasrun/htcc/flasher/HTCC_FlasherDefault.dat")
+dbpf("B_HW_FLASHER_HTCC-CTOF:F0_DATA_FILE_SAVE","/home/clasrun/htcc/flasher/HTCC_FlasherDefault.dat")
+dbpf("B_HW_FLASHER_HTCC-CTOF:F1_DATA_FILE_LOAD","/home/clasrun/htcc/flasher/CTOF_FlasherDefault.dat")
+dbpf("B_HW_FLASHER_HTCC-CTOF:F1_DATA_FILE_SAVE","/home/clasrun/htcc/flasher/CTOF_FlasherDefault.dat")

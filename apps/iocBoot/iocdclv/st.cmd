14042a8cf95cd25c076058b71abc37820d94853a
baltzell <baltzell@gmx.com>
2017-09-21 18:49:19 -0400

    iocdclv:  prep for downsize to 1 ioc

--- apps/iocBoot/iocdclv/st.cmd
+++ apps/iocBoot/iocdclv/st.cmd
@@ -2,6 +2,8 @@
 
 < envPaths
 
+< dclvAddresses.env
+
 cd ${TOP}
 
 ## Register all support components
@@ -10,26 +12,26 @@ A6551_registerRecordDeviceDriver pdbbase
 
 epicsEnvSet("STREAM_PROTOCOL_PATH","${TOP}/proto")
 
-epicsEnvSet("scan","2 second")
+epicsEnvSet("scan","10 second")
 
-drvAsynIPPortConfigure("S1R1",hallb-gpib02.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S1R2",hallb-gpib05.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S1R3",hallb-gpib08.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S2R1",hallb-gpib03.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S2R2",hallb-gpib06.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S2R3",hallb-gpib09.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S3R1",hallb-gpib19.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S3R2",hallb-gpib15.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S3R3",hallb-gpib18.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S4R1",hallb-gpib11.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S4R2",hallb-gpib14.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S4R3",hallb-gpib20.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S5R1",hallb-gpib10.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S5R2",hallb-gpib13.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S5R3",hallb-gpib16.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S6R1",hallb-gpib01.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S6R2",hallb-gpib04.jlab.org:1234,0,0,0)
-drvAsynIPPortConfigure("S6R3",hallb-gpib07.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S1R1",${GPIBS1R1}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S1R2",${GPIBS1R2}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S1R3",${GPIBS1R3}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S2R1",${GPIBS2R1}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S2R2",${GPIBS2R2}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S2R3",${GPIBS2R3}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S3R1",${GPIBS3R1}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S3R2",${GPIBS3R2}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S3R3",${GPIBS3R3}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S4R1",${GPIBS4R1}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S4R2",${GPIBS4R2}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S4R3",${GPIBS4R3}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S5R1",${GPIBS5R1}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S5R2",${GPIBS5R2}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S5R3",${GPIBS5R3}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S6R1",${GPIBS6R1}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S6R2",${GPIBS6R2}.jlab.org:1234,0,0,0)
+drvAsynIPPortConfigure("S6R3",${GPIBS6R3}.jlab.org:1234,0,0,0)
 
 dbLoadRecords("db/A6551.db","SCAN=${scan},P=B_DET_DC_LV_SEC1_R1,PORT=S1R1,ADDR=1,IMAX=2000,OMAX=2000")
 dbLoadRecords("db/A6551.db","SCAN=${scan},P=B_DET_DC_LV_SEC1_R2,PORT=S1R2,ADDR=1,IMAX=2000,OMAX=2000")
@@ -50,30 +52,24 @@ dbLoadRecords("db/A6551.db","SCAN=${scan},P=B_DET_DC_LV_SEC6_R1,PORT=S6R1,ADDR=1
 dbLoadRecords("db/A6551.db","SCAN=${scan},P=B_DET_DC_LV_SEC6_R2,PORT=S6R2,ADDR=1,IMAX=2000,OMAX=2000")
 dbLoadRecords("db/A6551.db","SCAN=${scan},P=B_DET_DC_LV_SEC6_R3,PORT=S6R3,ADDR=1,IMAX=2000,OMAX=2000")
 
-#dbLoadRecords("db/prologix.db","P=hallb-gpib02,R=:,DESC=DCLV_S1R1,PORT=S1R1,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib05,R=:,DESC=DCLV_S1R2,PORT=S1R2,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib08,R=:,DESC=DCLV_S1R3,PORT=S1R3,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib03,R=:,DESC=DCLV_S2R1,PORT=S2R1,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib06,R=:,DESC=DCLV_S2R2,PORT=S2R2,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib09,R=:,DESC=DCLV_S2R3,PORT=S2R3,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib19,R=:,DESC=DCLV_S3R1,PORT=S3R1,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib15,R=:,DESC=DCLV_S3R2,PORT=S3R2,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib18,R=:,DESC=DCLV_S3R3,PORT=S3R3,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib11,R=:,DESC=DCLV_S4R1,PORT=S4R1,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib14,R=:,DESC=DCLV_S4R2,PORT=S4R2,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib20,R=:,DESC=DCLV_S4R3,PORT=S4R3,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib10,R=:,DESC=DCLV_S5R1,PORT=S5R1,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib13,R=:,DESC=DCLV_S5R2,PORT=S5R2,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib16,R=:,DESC=DCLV_S5R3,PORT=S5R3,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib01,R=:,DESC=DCLV_S6R1,PORT=S6R1,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib04,R=:,DESC=DCLV_S6R2,PORT=S6R2,")
-#dbLoadRecords("db/prologix.db","P=hallb-gpib07,R=:,DESC=DCLV_S6R3,PORT=S6R3,")
-
-# Non-DC hallb-gpibs:
-##drvAsynIPPortConfigure("00",hallb-gpib00.jlab.org:1234,0,0,0)
-##drvAsynIPPortConfigure("12",hallb-gpib12.jlab.org:1234,0,0,0)
-##drvAsynIPPortConfigure("17",hallb-gpib17.jlab.org:1234,0,0,0)
-##drvAsynIPPortConfigure("21",hallb-gpib21.jlab.org:1234,0,0,0)
+#dbLoadRecords("db/prologix.db","P=${GPIBS1R1},R=:,DESC=DCLV_S1R1,PORT=S1R1,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS1R2},R=:,DESC=DCLV_S1R2,PORT=S1R2,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS1R3},R=:,DESC=DCLV_S1R3,PORT=S1R3,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS2R1},R=:,DESC=DCLV_S2R1,PORT=S2R1,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS2R2},R=:,DESC=DCLV_S2R2,PORT=S2R2,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS2R3},R=:,DESC=DCLV_S2R3,PORT=S2R3,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS3R1},R=:,DESC=DCLV_S3R1,PORT=S3R1,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS3R2},R=:,DESC=DCLV_S3R2,PORT=S3R2,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS3R3},R=:,DESC=DCLV_S3R3,PORT=S3R3,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS4R1},R=:,DESC=DCLV_S4R1,PORT=S4R1,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS4R2},R=:,DESC=DCLV_S4R2,PORT=S4R2,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS4R3},R=:,DESC=DCLV_S4R3,PORT=S4R3,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS5R1},R=:,DESC=DCLV_S5R1,PORT=S5R1,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS5R2},R=:,DESC=DCLV_S5R2,PORT=S5R2,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS5R3},R=:,DESC=DCLV_S5R3,PORT=S5R3,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS6R1},R=:,DESC=DCLV_S6R1,PORT=S6R1,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS6R2},R=:,DESC=DCLV_S6R2,PORT=S6R2,")
+#dbLoadRecords("db/prologix.db","P=${GPIBS6R3},R=:,DESC=DCLV_S6R3,PORT=S6R3,")
 
 dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")
 dbLoadRecords("db/iocAdminSoft.db","IOC=${IOC}")
@@ -81,6 +77,8 @@ dbLoadRecords("db/iocAdminSoft.db","IOC=${IOC}")
 cd ${TOP}/iocBoot/${IOC}
 
 < save_restore.cmd
+
+asSetFilename("../acf/cas.acf")
 
 iocInit();
 

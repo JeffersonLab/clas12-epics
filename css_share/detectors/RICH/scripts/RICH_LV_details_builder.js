bff05db1a254ffa89680e86147c585a97f05be53
baltzell <baltzell@gmx.com>
2017-10-29 22:34:15 -0400

    RICH overview screen and DET-based hv/lv screens

---
+++ css_share/detectors/RICH/scripts/RICH_LV_details_builder.js
@@ -0,0 +1,30 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+//widget.removeAllChildren();
+
+var novice = PVUtil.getDouble(pvs[0]);
+
+var prefix = "B_DET_RICH_LV_GRP";
+var nchan = 40;
+
+  for (var chan=1; chan<=nchan; chan++)
+  {
+    if (chan<10) chan="0"+chan;
+
+    var pv = prefix+chan;
+
+    var lc = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");
+    if (novice>0) { lc.setPropertyValue("opi_file","/CLAS12_Share/apps/caenHvApp/LV/caenhv_channel_novice-lv.opi"); }
+    else          { lc.setPropertyValue("opi_file","/CLAS12_Share/apps/caenHvApp/LV/caenhv_channel-lv.opi"); }
+    //try   { lc.setPropertyValue("resize_behaviour",1); }
+    //catch (err) { lc.setPropertyValue("auto_size",true); }
+    lc.setPropertyValue("auto_size",true);
+    lc.setPropertyValue("zoom_to_fit",false);
+    lc.setPropertyValue("border_style",0);
+    lc.setPropertyValue("background_color","Header_Background");
+    lc.addMacro("C",chan);
+    lc.addMacro("P",pv);
+    widget.addChildToBottom(lc);
+  }
+
+

72dd2fa3241f14da1e3576dc220cd32d1fe4dc91
baltzell <baltzell@gmx.com>
2017-11-08 20:00:25 -0500

    caen 527:  add trip

--- css_share/detectors/CND/scripts/CND_HV_details_builder.js
+++ css_share/detectors/CND/scripts/CND_HV_details_builder.js
@@ -20,7 +20,7 @@ for (var seg=1; seg<=24; seg++)
         
         var lc = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");
         if (novice>0) { lc.setPropertyValue("opi_file","/CLAS12_Share/apps/caenHvApp/caenhv_channel_novice.opi"); }
-        else          { lc.setPropertyValue("opi_file","/CLAS12_Share/apps/caenHvApp/caenhv_channel.opi"); }
+        else          { lc.setPropertyValue("opi_file","/CLAS12_Share/apps/caenHvApp/caenhv_channel-WITHTRIP.opi"); }
         //try   { lc.setPropertyValue("resize_behaviour",1); }
         //catch (err) { lc.setPropertyValue("auto_size",true); }
         lc.setPropertyValue("auto_size",true);

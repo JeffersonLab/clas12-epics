f4512e2079d624b3bd7fdc44ba93fb8382ae31c3
baltzell <baltzell@gmx.com>
2017-11-30 17:17:04 -0500

    add hv bom to cnd527

--- css_share/beamline/scripts/beamline_hv_builder.js
+++ css_share/beamline/scripts/beamline_hv_builder.js
@@ -69,4 +69,18 @@ mkGap();
     }
 }
 
+mkGap();
 
+var lc = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");
+        if (novice>0) { lc.setPropertyValue("opi_file","/CLAS12_Share/apps/caenHvApp/caenhv_channel_novice.opi"); }
+        else          { lc.setPropertyValue("opi_file","/CLAS12_Share/apps/caenHvApp/caenhv_channel.opi"); }
+        //try   { lc.setPropertyValue("resize_behaviour",1); }
+        //catch (err) { lc.setPropertyValue("auto_size",true); }
+        lc.setPropertyValue("auto_size",true);
+        lc.setPropertyValue("zoom_to_fit",false);
+        lc.setPropertyValue("border_style",0);
+        lc.setPropertyValue("background_color","Header_Background");
+        lc.addMacro("C",ii+1);
+        lc.addMacro("P","B_DET_BLINE_HV_BOM");
+        lc.addMacro("TYPE","527");
+        widget.addChildToBottom(lc);

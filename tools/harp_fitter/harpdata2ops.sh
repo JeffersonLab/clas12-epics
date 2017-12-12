f9dd8a6dd3e070d322ae7adc75c47cee6a870bd9
baltzell <baltzell@gmx.com>
2017-12-11 20:54:13 -0500

    harp_fitter:  setup cronjob

---
+++ tools/harp_fitter/harpdata2ops.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+localDir=/home/epics/DATA/HARP_SCANS
+localSubdirs='harp_2c21 harp_2H01 harp_tagger'
+
+remoteUser=hbops
+remoteHost=hlbl00
+remoteDir=/usr/opsdata/harpData/HallB
+
+for dd in $localSubdirs
+do
+    # the trailing slash is important
+    rsync -r -a -v -e ssh $localDir/$dd/ $remoteUser@$remoteHost:$remoteDir
+done
+
+

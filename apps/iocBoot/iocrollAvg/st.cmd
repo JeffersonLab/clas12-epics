62c2adc89d5bbe2f949477e14a519d53b277df88
baltzell <baltzell@gmx.com>
2018-01-09 09:47:44 -0500

    iocrollAvg:  tuning times

--- apps/iocBoot/iocrollAvg/st.cmd
+++ apps/iocBoot/iocrollAvg/st.cmd
@@ -20,7 +20,7 @@ dbLoadRecords("db/timeDerivative.db","P=CLL6763A,R=:deriv,DT=2,SCALE=12080,EGU=g
 
 # live average is over DT*N seconds
 dbLoadRecords("db/liveRollAvg.db","I=LL8210:deriv:dxdt,P=LL8210:dxdt,N=18,EGU=g/s")
-dbLoadRecords("db/liveRollAvg.db","I=CLL6763A:deriv:dxdt,P=CLL6763A:dxdt,N=60,EGU=g/s")
+dbLoadRecords("db/liveRollAvg.db","I=CLL6763A:deriv:dxdt,P=CLL6763A:dxdt,N=150,EGU=g/s")
 
 dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:")
 dbLoadRecords("db/iocAdminSoft.db","IOC=${IOC}")

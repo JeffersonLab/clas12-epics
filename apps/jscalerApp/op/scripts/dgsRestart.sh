49bf72067dd748909d2e092858fdf54f09290919
baltzell <baltzell@gmx.com>
2018-01-13 17:58:56 -0500

    dgs restart scripts cleanup

--- apps/jscalerApp/op/scripts/dgsRestart.sh
+++ apps/jscalerApp/op/scripts/dgsRestart.sh
@@ -1,7 +1,7 @@
 killall DiagGuiServer
 
 # mutexclean, per Sergey's recommendation, added December 16, 2017
-# Totally untested.  I added a sleep just to be safe.  (NAB)
+# I added a sleep just to be safe.  (NAB)
 mutexclean ; sleep 2
 
 exec DiagGuiServer >& /dev/null &
--- bin/dgsIocRestartAll.sh
+++ bin/dgsIocRestartAll.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-# should add T and RICH eventually
+DGSOWNER=clasrun
+if [[ `whoami` != $DGSOWNER ]]
+then
+    echo
+    echo ERROR:  DiagGuiServer is owned by $DGSOWNER, but you are `whoami`.
+    echo Exiting ....
+    echo
+    exit
+fi
 
-for xx in 1 2 3 4 5 6 C
-do
-    dgsIocRestart.sh $xx
-done
+xt='xterm -bg red -fg white'
+
+$xt -e dgsIocRestart.sh 1 &
+$xt -e dgsIocRestart.sh 2 &
+$xt -e dgsIocRestart.sh 3 &
+$xt -e dgsIocRestart.sh 4 &
+$xt -e dgsIocRestart.sh 5 &
+$xt -e dgsIocRestart.sh 6 &
+$xt -e dgsIocRestart.sh C &
+$xt -e dgsIocRestart.sh T &
 

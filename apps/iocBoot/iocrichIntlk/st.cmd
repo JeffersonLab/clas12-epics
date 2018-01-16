185a5059de630b0ecbf6f5e0dd2dcf41cc60a261
baltzell <baltzell@gmx.com>
2018-01-15 20:52:59 -0500

    iocrichIntlk:  add heartbeat alarm record for S1_EP

--- apps/iocBoot/iocrichIntlk/st.cmd
+++ apps/iocBoot/iocrichIntlk/st.cmd
@@ -23,6 +23,7 @@ dbLoadRecords("db/richIntlk-S1EP.db")
 dbLoadRecords("db/richIntlk-sums.db")
 
 dbLoadRecords("db/cRIO_heartbeat_bi.db","P=B_HW_, R=CRIO_RICH_, DLY=60")
+dbLoadRecords("db/cRIO_heartbeat_bi.db","P=B_HW_, R=CRIO_RICH_S1_EP_, DLY=60")
 
 cd ${TOP}/iocBoot/${IOC}
 

6edbcc719500be87dff7d207badc071d4fca5a92
baltzell <baltzell@gmx.com>
2018-01-19 19:41:30 -0500

    iocmmIntlk:  accommodate solenoid polarity

--- apps/iocBoot/iocmmIntlk/st.cmd
+++ apps/iocBoot/iocmmIntlk/st.cmd
@@ -23,3 +23,12 @@ makeAutosaveFiles()
 create_monitor_set("info_positions.req", 5, "P=${IOC}:")
 create_monitor_set("info_settings.req", 30, "P=${IOC}:")
 
+# these don't fit the template, due to solenoid polarity:
+epicsThreadSleep(2)
+dbpf("B_DET_BMT_INTLK_Solenoid:first_check.CALC", "(C<=ABS(A)||ABS(A)<B)&(D#1)?1:0")
+dbpf("B_DET_BMT_INTLK_Solenoid:second_check.CALC","(C<=ABS(A)||ABS(A)<B)&(D#1)?1:0")
+dbpf("B_DET_FMT_INTLK_Solenoid:first_check.CALC", "(C<=ABS(A)||ABS(A)<B)&(D#1)?1:0")
+dbpf("B_DET_FMT_INTLK_Solenoid:second_check.CALC","(C<=ABS(A)||ABS(A)<B)&(D#1)?1:0")
+dbpf("B_DET_FTT_INTLK_Solenoid:first_check.CALC", "(C<=ABS(A)||ABS(A)<B)&(D#1)?1:0")
+dbpf("B_DET_FTT_INTLK_Solenoid:second_check.CALC","(C<=ABS(A)||ABS(A)<B)&(D#1)?1:0")
+

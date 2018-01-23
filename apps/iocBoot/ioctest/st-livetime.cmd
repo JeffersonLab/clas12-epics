da0881dedce37f892ece65be74b1165a645e4cc1
baltzell <baltzell@gmx.com>
2018-01-22 19:08:58 -0500

    temporary livetime setup, should be from amq eventually

---
+++ apps/iocBoot/ioctest/st-livetime.cmd
@@ -0,0 +1,16 @@
+#!../../bin/linux-x86_64/mcc
+
+< envPaths
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/mcc.dbd"
+mcc_registerRecordDeviceDriver pdbbase
+
+dbLoadRecords("db/tsBusy.db")
+
+cd ${TOP}/iocBoot/${IOC}
+
+iocInit();
+

cefcbd1bedc527697c7ba62e3d04ae7926757ce7
baltzell <baltzell@gmx.com>
2018-02-13 19:00:23 -0500

    scalerApp:  deploy autoped

--- apps/iocBoot/ioctest/st-ped.cmd
+++ apps/iocBoot/ioctest/st-ped.cmd
@@ -8,10 +8,19 @@ cd ${TOP}
 dbLoadDatabase "dbd/mcc.dbd"
 mcc_registerRecordDeviceDriver pdbbase
 
-dbLoadRecords("db/scaler-ped.db","OUT=fcup2_offset,P=fcup2_offset,RAW=scalerS2b, REF=myIPM2C21A,REFMAX=0.1,RAWMAX=500,N=10")
-dbLoadRecords("db/scaler-ped.db","OUT=slm2_offset, P=slm2_offset, RAW=scalerS15b,REF=myIPM2C21A,REFMAX=0.1,RAWMAX=500,N=10")
+dbLoadRecords("db/scaler-ped.db","OUT=fcup_offset,P=fcup_offset,RAW=scalerS2b, REF=IPM2C21A,REFMAX=0.1,RAWMAX=500,N=5")
+dbLoadRecords("db/scaler-ped.db","OUT=slm_offset, P=slm_offset, RAW=scalerS15b,REF=IPM2C21A,REFMAX=0.1,RAWMAX=500,N=5")
+
+#dbLoadRecords("db/iocAdminSoft.db", "IOC=${IOC}")
+dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")
 
 cd ${TOP}/iocBoot/${IOC}
 
+< save_restore.cmd
+
 iocInit();
+
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+create_monitor_set("info_settings.req", 30, "P=${IOC}:")
 

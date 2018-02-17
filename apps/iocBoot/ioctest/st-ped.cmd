1916719e0eb7e646b14f102adea616766474ef0d
baltzell <baltzell@gmx.com>
2018-02-16 20:44:24 -0500

    autoped:  slm is 16, not 15

--- apps/iocBoot/ioctest/st-ped.cmd
+++ apps/iocBoot/ioctest/st-ped.cmd
@@ -9,7 +9,7 @@ dbLoadDatabase "dbd/mcc.dbd"
 mcc_registerRecordDeviceDriver pdbbase
 
 dbLoadRecords("db/scaler-ped.db","OUT=fcup_offset,P=fcup_offset,RAW=scalerS2b, REF=IPM2C21A,REFMAX=0.1,RAWMAX=500,N=5")
-dbLoadRecords("db/scaler-ped.db","OUT=slm_offset, P=slm_offset, RAW=scalerS15b,REF=IPM2C21A,REFMAX=0.1,RAWMAX=500,N=5")
+dbLoadRecords("db/scaler-ped.db","OUT=slm_offset, P=slm_offset, RAW=scalerS16b,REF=IPM2C21A,REFMAX=0.1,RAWMAX=500,N=5")
 
 #dbLoadRecords("db/iocAdminSoft.db", "IOC=${IOC}")
 dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")

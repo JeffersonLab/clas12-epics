b2445d63d95670700c730e3b01d701f601b3f691
baltzell <baltzell@gmx.com>
2018-02-23 21:08:11 -0500

    livetime ioc:  add rms

--- apps/iocBoot/ioctest/st-livetime.cmd
+++ apps/iocBoot/ioctest/st-livetime.cmd
@@ -9,7 +9,8 @@ dbLoadDatabase "dbd/mcc.dbd"
 mcc_registerRecordDeviceDriver pdbbase
 
 dbLoadRecords("db/tsBusy.db")
-
+dbLoadRecords("db/liveRollAvg.db","P=B_livetime,I=B_livetime,N=15,SCAN=2")
+        
 cd ${TOP}/iocBoot/${IOC}
 
 iocInit();

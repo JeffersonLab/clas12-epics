c3cd89217c6faecc7381b7144116ab57b54af097
baltzell <baltzell@gmx.com>
2018-02-24 21:55:59 -0500

    liveRollAvg.db:  extra aiRecord for alarms

--- apps/iocBoot/ioctest/st-livetime.cmd
+++ apps/iocBoot/ioctest/st-livetime.cmd
@@ -10,8 +10,18 @@ mcc_registerRecordDeviceDriver pdbbase
 
 dbLoadRecords("db/tsBusy.db")
 dbLoadRecords("db/liveRollAvg.db","P=B_livetime,I=B_livetime,N=15,SCAN=2")
-        
+dbLoadRecords("db/liveRollAvg.db","P=B_IPM2C21A,I=IPM2C21A,  N=30,SCAN=1")
+dbLoadRecords("db/tsBusy-alarm.db")
+
 cd ${TOP}/iocBoot/${IOC}
 
 iocInit();
+
+dbpf("B_livetime:rms.HIGH","2")
+dbpf("B_livetime:rms.HSV","MAJOR")
+dbpf("B_livetime:mean.LOW","75")
+dbpf("B_livetime:mean.LSV","MAJOR")
+
+dbpf("B_IPM2C21A:rms.HIGH","2")
+dbpf("B_IPM2C21A:mean.LOW","5")
 

0559f624ce85f3b1cb7b698d2e384217f0a66204
baltzell <baltzell@gmx.com>
2018-03-21 19:08:05 -0400

    provide serial console logging for cRios, via moxa and telnet

---
+++ tools/telnetLog/telnetLog.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# log a non-interactive telnet session
+#
+# depending on the host, this may block the $port
+#
+# implemented to just log console messages from cRios
+#
+
+host=$1
+port=$2
+
+logDir=/usr/clas12/DATA/logs-notrotated
+logFile=$host:$port.log
+
+touch $logDir/$logFile
+
+script -f -a -c "telnet $host $port" $logDir/$logFile
+

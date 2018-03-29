f9152b9574fdd25663706a9b3bd11d85525bc3e5
baltzell <baltzell@gmx.com>
2018-03-29 17:01:20 -0400

    daq info:  standardize PV names

--- apps/iocBoot/ioctest/daq2epics.py
+++ apps/iocBoot/ioctest/daq2epics.py
@@ -6,17 +6,17 @@ os.putenv('EXPID','clasrun')
 os.putenv('SESSION','clasprod')
 
 CFG={
-'B_livetime':     {'ini':-1,   'cmd':['tcpClient','trig1','tsBusy']},
-'B_run_number':   {'ini':-1,   'cmd':['run_number']},
-'B_run_nfiles':   {'ini':-1,   'cmd':['run_nfiles']},
-'B_run_status':   {'ini':'UDF','cmd':['run_status']},
-'B_run_config':   {'ini':'UDF','cmd':['run_config']},
-'B_run_time':     {'ini':-1,   'cmd':['run_time']},
-'B_run_ndata':    {'ini':-1,   'cmd':['run_ndata']},
-'B_run_nevents':  {'ini':-1,   'cmd':['run_nevents']}
+'B_DAQ:livetime':     {'ini':-1,   'cmd':['tcpClient','trig1','tsBusy']},
+'B_DAQ:run_number':   {'ini':-1,   'cmd':['run_number']},
+'B_DAQ:run_nfiles':   {'ini':-1,   'cmd':['run_nfiles']},
+'B_DAQ:run_status':   {'ini':'UDF','cmd':['run_status']},
+'B_DAQ:run_config':   {'ini':'UDF','cmd':['run_config']},
+'B_DAQ:run_time':     {'ini':-1,   'cmd':['run_time']},
+'B_DAQ:run_ndata':    {'ini':-1,   'cmd':['run_ndata']},
+'B_DAQ:run_nevents':  {'ini':-1,   'cmd':['run_nevents']}
 }
 
-HBEAT=epics.pv.PV('B_livetime_heartbeat')
+HBEAT=epics.pv.PV('B_DAQ:livetime_heartbeat')
 
 nonNumber=re.compile(r'[^\d.]+')
 
@@ -34,7 +34,7 @@ while True:
       yy=xx
       if type(CFG[pvName]['ini']) is not str:
         yy=re.sub(nonNumber,'',xx)
-      if pvName=='B_livetime':
+      if pvName=='B_DAQ:livetime':
         if xx.find('Livetime')==0:
           jj=HBEAT.get()
           HBEAT.put(jj+1)
--- apps/iocBoot/ioctest/st-livetime.cmd
+++ apps/iocBoot/ioctest/st-livetime.cmd
@@ -8,8 +8,10 @@ cd ${TOP}
 dbLoadDatabase "dbd/mcc.dbd"
 mcc_registerRecordDeviceDriver pdbbase
 
-dbLoadRecords("db/tsBusy.db")
-dbLoadRecords("db/liveRollAvg.db","P=B_livetime,I=B_livetime,N=15,SCAN=2")
+#dbLoadRecords("db/tsBusy.db")
+dbLoadRecords("db/daq2epics.db","P=B_DAQ")
+
+dbLoadRecords("db/liveRollAvg.db","P=B_DAQ:livetime,I=B_DAQ:livetime,N=15,SCAN=2")
 dbLoadRecords("db/liveRollAvg.db","P=B_IPM2C21A,I=IPM2C21A,  N=30,SCAN=1")
 dbLoadRecords("db/tsBusy-alarm.db")
 
@@ -17,10 +19,10 @@ cd ${TOP}/iocBoot/${IOC}
 
 iocInit();
 
-dbpf("B_livetime:rms.HIGH","2")
-dbpf("B_livetime:rms.HSV","MAJOR")
-dbpf("B_livetime:mean.LOW","75")
-dbpf("B_livetime:mean.LSV","MAJOR")
+dbpf("B_DAQ:livetime:rms.HIGH","0.3")
+dbpf("B_DAQ:livetime:rms.HSV","MAJOR")
+dbpf("B_DAQ:livetime:mean.LOW","75")
+dbpf("B_DAQ:livetime:mean.LSV","MAJOR")
 
 dbpf("B_IPM2C21A:rms.HIGH","2")
 dbpf("B_IPM2C21A:mean.LOW","5")

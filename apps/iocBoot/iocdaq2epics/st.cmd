2961eb4ddb3c60a9fbaeeff0214cdd9cc3017879
baltzell <baltzell@gmx.com>
2018-04-30 10:58:10 -0400

    daq2epics:  move to procServ

---
+++ apps/iocBoot/iocdaq2epics/st.cmd
@@ -0,0 +1,29 @@
+#!../../bin/linux-x86_64/mcc
+
+< envPaths
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/mcc.dbd"
+mcc_registerRecordDeviceDriver pdbbase
+
+#dbLoadRecords("db/tsBusy.db")
+dbLoadRecords("db/daq2epics.db","P=B_DAQ")
+
+dbLoadRecords("db/liveRollAvg.db","P=B_DAQ:livetime,I=B_DAQ:livetime,N=15,SCAN=2")
+dbLoadRecords("db/liveRollAvg.db","P=B_IPM2C21A,I=IPM2C21A,  N=30,SCAN=1")
+dbLoadRecords("db/tsBusy-alarm.db")
+
+cd ${TOP}/iocBoot/${IOC}
+
+iocInit();
+
+dbpf("B_DAQ:livetime:rms.HIGH","0.7")
+dbpf("B_DAQ:livetime:rms.HSV","MINOR")
+dbpf("B_DAQ:livetime:mean.LOW","75")
+dbpf("B_DAQ:livetime:mean.LSV","MINOR")
+
+dbpf("B_IPM2C21A:rms.HIGH","2")
+dbpf("B_IPM2C21A:mean.LOW","5")
+

7fcfd8adf2bec3d8b7ff7c02a5c8812086d4ce78
baltzell <baltzell@gmx.com>
2018-08-04 19:23:10 -0400

    automoller v0

---
+++ apps/iocBoot/ioctest/st-automoller.cmd
@@ -0,0 +1,18 @@
+#!../../bin/linux-x86_64/mollerSetup
+
+< envPaths
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/mollerSetup.dbd"
+mollerSetup_registerRecordDeviceDriver pdbbase
+
+dbLoadRecords("db/automoller.db","P=B_MOLLER:,S=SIM:")
+
+cd ${TOP}/iocBoot/${IOC}
+
+iocInit();
+
+seq automoller "P=B_MOLLER:,S=SIM:"
+

3ac1cafe4455323579253d774b7ea5da2e30361b
baltzell <baltzell@gmx.com>
2018-09-24 18:50:31 -0400

    dgsIocRestartAll.sh:  tiling

--- bin/dgsIocRestartAll.sh
+++ bin/dgsIocRestartAll.sh
@@ -10,14 +10,14 @@ then
     exit
 fi
 
-xt='xterm -bg red -fg white'
+xt='xterm -bg red -fg white -geometry 90x30'
 
-$xt -e dgsIocRestart.sh 1 &
-$xt -e dgsIocRestart.sh 2 &
-$xt -e dgsIocRestart.sh 3 &
-$xt -e dgsIocRestart.sh 4 &
-$xt -e dgsIocRestart.sh 5 &
-$xt -e dgsIocRestart.sh 6 &
-$xt -e dgsIocRestart.sh C &
-$xt -e dgsIocRestart.sh T &
+$xt+0+0     -e dgsIocRestart.sh 1 &
+$xt+20+20   -e dgsIocRestart.sh 2 &
+$xt+40+40   -e dgsIocRestart.sh 3 &
+$xt+60+60   -e dgsIocRestart.sh 4 &
+$xt+80+80   -e dgsIocRestart.sh 5 &
+$xt+100+100 -e dgsIocRestart.sh 6 &
+$xt+120+120 -e dgsIocRestart.sh C &
+$xt+140+140 -e dgsIocRestart.sh T &
 

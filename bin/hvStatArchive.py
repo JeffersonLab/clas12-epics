c122b531eabea0c4ae9c6d5e22d7a5ca288e4d5a
baltzell <baltzell@gmx.com>
2018-10-14 11:39:38 -0400

    hvStatArchive.py:  add detector option

--- bin/hvStatArchive.py
+++ bin/hvStatArchive.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys,datetime,subprocess,epics
+import sys,datetime,subprocess,epics,argparse
 
 def getMyaTable(week,pv):
   cmd=['myget','-c',pv,'-b','-%dw'%(week),'-e','-%dw'%(week-1)]
@@ -23,7 +23,7 @@ def countBits(table):
   return bitCounts
 
 def getPVs():
-  pcal,ecal,ftof,ctof,htcc,ltcc,mvt=[],[],[],[],[],[],[]
+  pcal,ecal,ftof,ctof,htcc,ltcc,mvt,ftc=[],[],[],[],[],[],[],[]
   fmt='B_DET_PCAL_HV_SEC%d_%s_E%.2d:stat'
   for sector in range(1,7):
     for view in ['U','V','W']:
@@ -60,17 +60,25 @@ def getPVs():
     for layer in range(1,7):
       for sd in ['STRIP','DRIFT']:
         mvt.append({'pv':fmt%(sector,layer,sd)})
+  fmt='B_DET_FTC_HV_Q%dG%d:stat'
+  for quadrant in range(1,5):
+    for group in range(1,10):
+      ftc.append({'pv':fmt%(quadrant,group)})
 
   return {'PCAL':pcal,'ECAL':ecal,'FTOF':ftof,\
-          'CTOF':ctof,'HTCC':htcc,'LTCC':ltcc,'MVT ':mvt}
+          'CTOF':ctof,'HTCC':htcc,'LTCC':ltcc,\
+          'MVT':mvt,'FTC':ftc}
 
 
 if __name__ == '__main__':
 
-  usage='hvStatArchive.py [-a] [weekOffset]'
-  threshold=10 # counts per week of a bad bit
-  weekOffset=1
-  showAll=False
+  parser = argparse.ArgumentParser(description='Retrieve and analyze archive HV statuses.')
+  parser.add_argument('-a',dest='showAll',action='store_true',default=False,help='show all channels (i.e. ignore threshold)')
+  parser.add_argument('-w',dest='weekOffset',metavar='#',type=int, action='store', default=1, help='number of weeks offset (default=1)')
+  parser.add_argument('-t',dest='threshold', metavar='#',type=int, action='store', default=10, help='error count threshold (default=10)')
+  parser.add_argument('-d',dest='detectors',metavar='DET',action='append',default=[],help='detector name')
+  args = parser.parse_args()
+
   badBits=[1,2,3,4,5,6,7,8,9,10,11]
   bitDefs='''
   0 ON
@@ -87,24 +95,16 @@ if __name__ == '__main__':
  11 UNPLUG
 '''
 
-  # interpret command line args:
-  if len(sys.argv)>1:
-    for arg in sys.argv[1:]:
-      if arg == '-h': sys.exit(usage)
-      if arg == '-a': showAll=True
-    if len(sys.argv)>2 or showAll==False:
-      try:    weekOffset = int(sys.argv[len(sys.argv)-1])
-      except: sys.exit(usage+'\n+ERROR:  weekOffset must be a positive integer')
-
   DATA=getPVs()
 
   # retrieve and analyze archive info:
   print '\nRetrieving Hardware Aliases and Querying Archive ...'
   for det,data in DATA.iteritems():
-    print '\n',det,' ',
+    if len(args.detectors)>0 and det not in args.detectors: continue
+    print '\n%4s'%(det),' ',
     for ii in range(len(data)):
       data[ii]['hwname'] = epics.pv.PV(data[ii]['pv']+'.NAME').get()
-      data[ii]['bits'] = countBits(getMyaTable(weekOffset,data[ii]['pv']))
+      data[ii]['bits'] = countBits(getMyaTable(args.weekOffset,data[ii]['pv']))
       if ii%20==0:
         sys.stdout.write('.')
         sys.stdout.flush()
@@ -112,11 +112,12 @@ if __name__ == '__main__':
   # print results:
   print '\n'
   for det,data in DATA.iteritems():
+    if len(args.detectors)>0 and det not in args.detectors: continue
     for datum in data:
-      if showAll==True: print datum['pv'],str(datum['bits'])
+      if args.showAll==True: print datum['pv'],str(datum['bits'])
       else:
         for bb in badBits:
-          if datum['bits'][bb]>threshold:
+          if datum['bits'][bb]>args.threshold:
             print datum['pv'],datum['hwname'],str(datum['bits'])
             break
   print bitDefs+'\n'

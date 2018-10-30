35af990eace51487039f84fe1f85272fba9e114e
baltzell <baltzell@gmx.com>
2018-10-29 20:36:15 -0400

    cleanup testApp

--- apps/iocBoot/ioctest/a.cmd
+++ apps/iocBoot/ioctest/a.cmd
@@ -8,7 +8,10 @@ cd ${TOP}
 dbLoadDatabase "dbd/testApp.dbd"
 testApp_registerRecordDeviceDriver pdbbase
 
-dbLoadRecords("db/soeTimestamp.db","P=soe,R=,UPPER=358651,LOWER=-1426734475")
+#dbLoadRecords("db/soeTimestamp.db","P=soe,R=,UPPER=358651,LOWER=-1426734475")
+
+dbLoadRecords("db/soeTimestamp-sim.db","P=soe,R=,UVAL=358651,LVAL=-1426734475")
+dbLoadRecords("db/soeTimestamp.db","P=soe,R=,UPPER=soe:upper,LOWER=soe:lower")
 
 cd ${TOP}/iocBoot/${IOC}
 

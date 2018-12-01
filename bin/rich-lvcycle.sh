a7a027571ca571719ceece00097764a83e870986
baltzell <baltzell@gmx.com>
2018-12-01 17:22:51 -0500

    rich recovery updates

--- bin/rich-lvcycle.sh
+++ bin/rich-lvcycle.sh
@@ -52,14 +52,76 @@ function lvcycle {
   done
 }
 
+#function checkssh {
+#  hostname=$1
+#  maxtries=$2
+#  tries=0
+#  while [ 1 ]
+#  do
+#    let tries=$tries+1
+#    echo -n Attempting ssh ... | $log
+#    ssh -q $1 exit
+#    if [ $? -eq 0 ]
+#    then
+#        echo " SUCCESS." | $log
+#        break
+#    else
+#        echo " failed" | $log
+#    fi
+#    if [ $tries -gt $maxtries ]
+#    then
+#        echo "ERR:R  FAILED TO SSH" | $log
+#        exit
+#    fi
+#    sleep 1
+#  done
+#}
+
+
+
 date | $log
+
 echo -e "\n!!!!   RICH RECOVERY   !!!!\n\nTurning RICH LV OFF ...\n" | $log
+
 lvcycle B_DET_RICH_LV:OFF B_DET_RICH_LV:isOff
+
 echo -e "\nRICH LV OFF succecsfull.\n\nTurning RICH LV ON ...\n" | $log
+
 lvcycle B_DET_RICH_LV:ON B_DET_RICH_LV:isOn
-echo -e "\nRICH LV ON succesfull.\n\nRunning rich_init on rich4 ...\n" | $log
+
+echo -e "\nRICH LV ON succesfull.\n\nRebooting rich4 ...\n" | $log
+
+roc_reboot rich4 | $log
+
+echo -e "\nWaiting 1 minute on rich4 ...\n" | $log
+cnt=0
+while [ 1 ]
+do
+  echo -n '.' | $log
+  if [ $cnt -gt 60 ]
+  then
+      break
+  fi
+  let cnt=$cnt+1
+  sleep 1
+done
+ping -c 15 -W 10 rich4
 sleep 5
+ssh rich4 uptime
+
+#echo -e "\nWaiting 45 seconds before trying to connect to rich4 ...\n" |$log
+#for xx in `seq 45`
+#do
+#    echo -n '.' | $log
+#    sleep 1
+#done
+#checkssh rich4 60
+
+echo -e "\nrunning rich_init" | $log
 ssh rich4 rich_init | $log
+
+sleep 5
+softioc_console -R iocjscalersRICH | $log
 
 echo  | $log
 echo "####################################################" | $log

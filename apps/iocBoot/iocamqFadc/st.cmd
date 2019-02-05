283d8bab9ac09c73797defc68eba09c9ce2d18ae
baltzell <baltzell@gmx.com>
2019-02-04 21:06:46 -0500

    add deviocstats to iocamqFadc

--- apps/iocBoot/iocamqFadc/st.cmd
+++ apps/iocBoot/iocamqFadc/st.cmd
@@ -14,7 +14,18 @@ dbLoadTemplate("db/amq-adc-HTCC.substitutions")
 dbLoadTemplate("db/amq-adc-LTCC.substitutions")
 dbLoadTemplate("db/amq-adc-CND.substitutions")
 
+dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db","IOC=$(IOC)")
+dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:")
+
 cd ${TOP}/iocBoot/${IOC}
+
+< save_restore.cmd
+
+iocInit
+
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+create_monitor_set("info_settings.req", 30, "P=${IOC}:")
 
 dbLoadRecords("amq-adc-ECAL.alias")
 dbLoadRecords("amq-adc-PCAL.alias")
@@ -23,8 +34,6 @@ dbLoadRecords("amq-adc-CTOF.alias")
 dbLoadRecords("amq-adc-HTCC.alias")
 dbLoadRecords("amq-adc-LTCC.alias")
 dbLoadRecords("amq-adc-CND.alias")
-
-iocInit
 
 StartMQ()
 

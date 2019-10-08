65ec006c90cf6f7a994a70df28fc437aff0d22bc
baltzell <baltzell@gmx.com>
2019-10-08 19:41:15 -0400

    hdice:  update plc records, add gui

---
+++ apps/iocBoot/iochdice/st.cmd
@@ -0,0 +1,28 @@
+#!../../bin/linux-x86/plc2epics
+############################################################################
+< envPaths
+############################################################################
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase("dbd/plc2epics.dbd")
+plc2epics_registerRecordDeviceDriver(pdbbase)
+
+## Initialize EtherIP driver, define PLCs
+## NOTE: if buffer limit is left at 500, read errors will occur.
+EIP_buffer_limit(450)
+drvEtherIP_init()
+drvEtherIP_define_PLC("PLC_HDICE", "129.57.86.23", 0)
+
+## Debugging [7-10]
+#EIP_verbosity(7)
+
+## Load record instances
+dbLoadRecords("${DEVIOCSTATS}/db/iocAdminSoft.db","IOC=${IOC}")
+dbLoadRecords("db/hdice_solenoid.db")
+dbLoadRecords("db/hdice_solenoid_breakout.db")
+
+cd ${TOP}/iocBoot/${IOC}
+
+iocInit
+

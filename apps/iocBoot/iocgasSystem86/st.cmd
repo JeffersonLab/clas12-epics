085c729ada8ad33da73a3f180f155941bfbe522e
baltzell <baltzell@gmx.com>
2019-11-16 16:30:01 -0500

    first bonus hv

--- apps/iocBoot/iocgasSystem86/st.cmd
+++ apps/iocBoot/iocgasSystem86/st.cmd
@@ -1,29 +1,57 @@
-#!../../bin/linux-x86_64/cRio
-############################################################################
+#!../../bin/linux-x86_64/mpodLv
+
 < envPaths
+
 epicsEnvSet("EPICS_CA_ADDR_LIST", "129.57.163.255")
 epicsEnvSet("EPICS_CAS_BEACON_ADDR_LIST", "129.57.163.255")
 epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES","10000000")
-############################################################################
+
+epicsEnvSet("MIBDIRS","${TOP}/iocBoot/iocsoftsvtRX:/usr/share/snmp/mibs")
+epicsEnvSet("MIBS","ALL")
+epicsEnvSet("W","WIENER-CRATE-MIB::")
+
 cd "${TOP}"
 
 ## Register all support components
-dbLoadDatabase("dbd/cRio.dbd")
-cRio_registerRecordDeviceDriver(pdbbase)
+#dbLoadDatabase("dbd/cRio.dbd")
+#cRio_registerRecordDeviceDriver(pdbbase)
+dbLoadDatabase("dbd/mpodLv.dbd")
+mpodLv_registerRecordDeviceDriver(pdbbase)
 
-## Load record instances
+#devSnmpSetParam(DebugLevel,10)
+
+## IOC records:
 dbLoadRecords("${DEVIOCSTATS}/db/iocAdminSoft.db","IOC=${IOC}")
 dbLoadRecords("${AUTOSAVE}/asApp/Db/save_restoreStatus.db", "P=${IOC}:")
-## cRIO heartbeat alarms
+
+## cRIO heartbeat alarms:
 dbLoadRecords("db/cRIO_heartbeat.db",   "P=B_HW_, R=CRIO_DEV_,  DLY=60")
-# dbLoadRecords("db/cRIO_heartbeat.db",   "P=B_HW_, R=CRIO_HTCC_, DLY=60")
-#dbLoadRecords("db/cRIO_heartbeat.db",   "P=B_HW_, R=CRIO_SVT_,  DLY=60")
-## Detector Systems 
+
+## Gas systems:
 #dbLoadRecords("db/gas_cRIO_HTCC.db",    "P=B_DET_, R=HTCC_GAS_")
 #dbLoadRecords("db/gas_cRIO_SVT.db",     "P=B_DET_, R=SVT_GAS_")
 
+## BONUS Gas:
 dbLoadRecords("db/gas_cRIO_BONUS.db")
 
+## BONUS HV:
+dbLoadRecords("db/mpod-crate.db","HOST=hbmpod5")
+dbLoadRecords("db/iseg-module.db","HOST=129.57.86.179,HOSTNAME=hbmpod5,MX=1")
+dbLoadRecords("db/iseg-module.db","HOST=129.57.86.179,HOSTNAME=hbmpod5,MX=2")
+dbLoadRecords("db/iseg-channel.db","HVHOST=129.57.86.179,HV=B_BONUS_HV_Sl0_Ch0,HVCH=1")
+dbLoadRecords("db/iseg-channel.db","HVHOST=129.57.86.179,HV=B_BONUS_HV_Sl0_Ch1,HVCH=2")
+dbLoadRecords("db/iseg-channel.db","HVHOST=129.57.86.179,HV=B_BONUS_HV_Sl0_Ch2,HVCH=3")
+dbLoadRecords("db/iseg-channel.db","HVHOST=129.57.86.179,HV=B_BONUS_HV_Sl0_Ch3,HVCH=4")
+dbLoadRecords("db/iseg-channel.db","HVHOST=129.57.86.179,HV=B_BONUS_HV_Sl0_Ch4,HVCH=5")
+dbLoadRecords("db/iseg-channel.db","HVHOST=129.57.86.179,HV=B_BONUS_HV_Sl0_Ch5,HVCH=6")
+dbLoadRecords("db/iseg-channel.db","HVHOST=129.57.86.179,HV=B_BONUS_HV_Sl0_Ch6,HVCH=7")
+dbLoadRecords("db/iseg-channel.db","HVHOST=129.57.86.179,HV=B_BONUS_HV_Sl0_Ch7,HVCH=8")
+dbLoadRecords("db/iseg-channel.db","HVHOST=129.57.86.179,HV=B_BONUS_HV_Sl1_Ch0,HVCH=101")
+dbLoadRecords("db/iseg-channel.db","HVHOST=129.57.86.179,HV=B_BONUS_HV_Sl1_Ch1,HVCH=102")
+dbLoadRecords("db/iseg-channel.db","HVHOST=129.57.86.179,HV=B_BONUS_HV_Sl1_Ch2,HVCH=103")
+dbLoadRecords("db/iseg-channel.db","HVHOST=129.57.86.179,HV=B_BONUS_HV_Sl1_Ch3,HVCH=104")
+
+## waveform test for Brian:
 dbLoadRecords("db/criowf.db")
 
 cd "${TOP}/iocBoot/${IOC}"
---
+++ css_share/detectors/BONUS/scripts/bonus-hv-details.js
@@ -0,0 +1,58 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+//widget.removeAllChildren();
+
+var novice = PVUtil.getDouble(pvs[0]);
+
+var nch=0;
+
+var lc = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");
+lc.setPropertyValue("opi_file","/CLAS12_Share/apps/mpodLvApp/iseg-channel-header.opi");
+lc.setPropertyValue("auto_size",true);
+lc.setPropertyValue("zoom_to_fit",false);
+lc.setPropertyValue("border_style",0);
+lc.setPropertyValue("background_color","Header_Background");
+widget.addChildToBottom(lc);
+        
+        
+for (var slot=0; slot<2; slot++)
+{
+  var nchan=8;
+  if (slot>0) nchan=4;
+  for (var chan=0; chan<nchan; chan++)
+  {
+        var pvprefix="B_BONUS_HV_Sl"+slot+"_Ch"+chan;
+        java.lang.System.err.println(pvprefix);
+        
+        
+        var lc = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");
+        lc.setPropertyValue("opi_file","/CLAS12_Share/apps/mpodLvApp/iseg-channel-row.opi");
+        //if (novice>0) { lc.setPropertyValue("opi_file","/CLAS12_Share/apps/caenHvApp/caenhv_channel_novice.opi"); }
+        //else          { lc.setPropertyValue("opi_file","/CLAS12_Share/apps/caenHvApp/caenhv_channel-WITHTRIP.opi"); }
+        //try   { lc.setPropertyValue("resize_behaviour",1); }
+        //catch (err) { lc.setPropertyValue("auto_size",true); }
+        lc.setPropertyValue("auto_size",true);
+        lc.setPropertyValue("zoom_to_fit",false);
+        lc.setPropertyValue("border_style",0);
+        lc.setPropertyValue("background_color","Header_Background");
+        lc.addMacro("C",nch);
+        lc.addMacro("P",pvprefix);
+        lc.addMacro("M",":"+(slot+1));
+        lc.addMacro("N",chan+1);
+        widget.addChildToBottom(lc);
+        nch++;
+        
+  }
+}
+
+var lc = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");
+lc.setPropertyValue("opi_file","/CLAS12_Share/apps/mpodLvApp/mpod-crate.opi");
+lc.setPropertyValue("auto_size",true);
+lc.setPropertyValue("zoom_to_fit",false);
+lc.setPropertyValue("border_style",0);
+lc.setPropertyValue("background_color","Header_Background");
+lc.addMacro("HOST","hbmpod5");
+lc.addMacro("ioc","iocgasSystem86");
+lc.addMacro("TITLE","asdf");
+widget.addChildToBottom(lc);
+

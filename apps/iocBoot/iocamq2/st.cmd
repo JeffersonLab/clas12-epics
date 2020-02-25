6c3ac44ad94d69404d835a82435bac9004f7aa7c
baltzell <baltzell@gmx.com>
2020-02-25 16:47:44 -0500

    add new EB rates

--- apps/iocBoot/iocamq2/st.cmd
+++ apps/iocBoot/iocamq2/st.cmd
@@ -19,6 +19,10 @@ dbLoadRecords("db/amqRocsRate-sums.db","P=B_DAQ:STA")
 
 dbLoadTemplate("db/amqLatency.substitutions")
 
+dbLoadTemplate("db/amqDaq.substitutions")
+dbLoadRecords("db/amqDaq-aliases.db")
+
+
 cd ${TOP}/iocBoot/${IOC}
 
 #< save_restore.cmd
@@ -31,5 +35,9 @@ iocInit
 
 #Need this to start responding to messages after all records are defined
 StartMQ()
+
+dbpf("B_DAQ:EB6:stats:01.EGU","Hz")
+dbpf("B_DAQ:EB6:stats:02.EGU","MB")
+dbpf("B_DAQ:EB6:stats:03.EGU","MB/s")
 
 dbl > pv.list

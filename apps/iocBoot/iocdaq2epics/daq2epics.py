2c51f74c12c85e955594fa990d016bec77e06c87
baltzell <baltzell@gmx.com>
2020-03-25 19:19:37 -0400

    add CODA trigger file

--- apps/iocBoot/iocdaq2epics/daq2epics.py
+++ apps/iocBoot/iocdaq2epics/daq2epics.py
@@ -14,6 +14,7 @@ CFG={
 'B_DAQ:run_status':   {'ini':'UDF','cmd':['run_status']},
 'B_DAQ:run_config':   {'ini':'UDF','cmd':['run_config']},
 'B_DAQ:run_number':   {'ini':0,    'cmd':['run_number'],'max':1e9},
+'B_DAQ:trigger_file': {'ini':'UDF','cmd':['daq_config']},
 'B_DAQ:disk_free:clondaq7': {'ini':0, 'skip':60, 'scale':1e-9, 'cmd':['ssh','clondaq7','df','/data','|','grep','-v','Filesystem','|','awk','\'{print$4}\'']},
 'B_DAQ:disk_free:clondaq6': {'ini':0, 'skip':60, 'scale':1e-9, 'cmd':['ssh','clondaq6','df','/data','|','grep','-v','Filesystem','|','awk','\'{print$4}\'']},
 'B_DAQ:disk_free:clondaq5': {'ini':0, 'skip':60, 'scale':1e-9, 'cmd':['ssh','clondaq5','df','/data','|','grep','-v','Filesystem','|','awk','\'{print$4}\'']}
@@ -70,6 +71,11 @@ while True:
           CFG[pvName]['pv'].put(yy)
         else:
           CFG[pvName]['pv'].put(CFG[pvName]['ini'])
+
+      # strip trigger file name:
+      elif pvName=='B_DAQ:trigger_file':
+        yy=yy.split('/').pop()
+        CFG[pvName]['pv'].put(yy)
 
       # treat everything else uniformly:
       else:

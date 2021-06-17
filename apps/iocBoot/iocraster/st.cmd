50dda5467858f40e1043cd9317ac0b851abab47c
baltzell <baltzell@gmx.com>
2021-06-17 11:44:50 -0400

    add initializers, missing PVs, Hall B pattern selector

--- apps/iocBoot/iocraster/st.cmd
+++ apps/iocBoot/iocraster/st.cmd
@@ -22,7 +22,7 @@ drvAsynIPPortConfigure("L0",129.57.86.49:5025,0,0,0)
 #asynOctetSetOutputEos("L0",0,"\n")
 #asynOctetSetInputEos("L0",0,"\n")
 
-dbLoadRecords("db/asynRecord.db","P=rasterdriver,R=:ASYN,PORT=L0,ADDR=1,IMAX=2000,OMAX=2000")
+dbLoadRecords("db/asynRecord.db","P=B_RASTDRV,R=:ASYN,PORT=L0,ADDR=1,IMAX=2000,OMAX=2000")
 dbLoadRecords("db/A33500.db","TAG=B_RASTDRV,PORT=L0,L=HB"))
 cd ${TOP}/iocBoot/${IOC}
 

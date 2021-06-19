f94742324467a03a08605af15569cbda0b08a823
baltzell <baltzell@gmx.com>
2021-06-19 12:56:32 -0400

    add danfysik fault summaries and guis

--- apps/iocBoot/iocraster/st.cmd
+++ apps/iocBoot/iocraster/st.cmd
@@ -16,6 +16,8 @@ epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES","100000")
 ## Configure devices
 
 drvAsynIPPortConfigure("L0",129.57.86.49:5025,0,0,0)
+#drvAsynIPPortConfigure("L1",129.57.86.49:5026,0,0,0)
+#drvAsynIPPortConfigure("L2",129.57.86.49:5027,0,0,0)
 
 #asynSetTraceMask("L0",-1,0x09)
 #asynSetTraceIOMask("L0",-1,0x02)
@@ -25,8 +27,9 @@ drvAsynIPPortConfigure("L0",129.57.86.49:5025,0,0,0)
 dbLoadRecords("db/asynRecord.db","P=B_RASTER:DRV,R=:ASYN,PORT=L0,ADDR=1,IMAX=2000,OMAX=2000")
 dbLoadRecords("db/A33500.db","TAG=B_RASTER:DRV,PORT=L0,L=HB"))
 
-#dbLoadRecords("db/danfysik-8500.db","P=B_RASTER:PS:")
-dbLoadTemplate("db/danfysik-8500-bitcalc.substitutions")
+dbLoadRecords("db/asynRecord.db","P=B_RASTER:PS:X,R=:ASYN,PORT=L1,ADDR=1,IMAX=2000,OMAX=2000")
+dbLoadRecords("db/asynRecord.db","P=B_RASTER:PS:Y,R=:ASYN,PORT=L2,ADDR=1,IMAX=2000,OMAX=2000")
+dbLoadTemplate("db/danfysik-8500.substitutions")
 
 cd ${TOP}/iocBoot/${IOC}
 

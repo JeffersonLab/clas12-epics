d3d8ec81a0284cab3d4460f0114c777cae97a5fc
baltzell <baltzell@gmx.com>
2022-04-02 12:14:26 -0400

    prep moller dynapower

--- apps/iocBoot/iocmoellerPS/st.cmd
+++ apps/iocBoot/iocmoellerPS/st.cmd
@@ -10,16 +10,22 @@ dbLoadDatabase("dbd/SCE410.dbd")
 SCE410_registerRecordDeviceDriver(pdbbase)
 
 drvAsynIPPortConfigure("SER1", "hallb-moxa6:4001")
+#drvAsynIPPortConfigure("SER2", "hallb-moxa6:4009")
 
 ## debugging...
 #asynSetTraceMask("SER1",-1,0x09)
 #asynSetTraceIOMask("SER1",-1,0x02)
+#asynSetTraceMask("SER2",-1,0x09)
+#asynSetTraceIOMask("SER2",-1,0x02)
 
 ## Load record instances
 dbLoadRecords("${DEVIOCSTATS}/db/iocAdminSoft.db", "IOC=${IOC}")
 dbLoadRecords("${AUTOSAVE}/asApp/Db/save_restoreStatus.db", "P=${IOC}:")
+
 #dbLoadRecords("db/SCE410.db", "P=B_DET_,R=MOELLER1_,PORT=SER1,ADDR=1")
 dbLoadRecords("db/SCE410.db", "P=B_MOLLER_,R=HELMHOLTZ_,PORT=SER1,ADDR=1")
+
+#dbLoadRecords("db/dynapower-2022.db","P=B_MOLLER_QUADS:,PORT=SER2")
 
 cd "${TOP}/iocBoot/${IOC}"
 

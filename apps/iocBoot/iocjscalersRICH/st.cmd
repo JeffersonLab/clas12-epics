25883e818ecf24fc93020553c4bd1a6246fdb33a
baltzell <baltzell@gmx.com>
2023-03-30 18:14:28 -0400

    disable a rich pmt

--- apps/iocBoot/iocjscalersRICH/st.cmd
+++ apps/iocBoot/iocjscalersRICH/st.cmd
@@ -69,8 +69,8 @@ seq waveform, "P=B_DET_RICH2_SCALERS_,R=PMTS:"
 ##< rich-setDesc.cmd
 
 # disable one LV channel:
-#dbpf("B_DET_RICH2_SSP:data:nFibers:alarm.INPC","134")
-#dbpf("B_DET_RICH2_SSP:data:nFibers:alarm.C","134")
-#dbpf("B_DET_RICH_SCALERS:alarm.LSV","NO_ALARM")
+dbpf("B_DET_RICH2_SSP:data:nFibers:alarm.INPC","137")
+dbpf("B_DET_RICH2_SSP:data:nFibers:alarm.C","137")
+dbpf("B_DET_RICH2_SCALERS:alarm.LSV","NO_ALARM")
 
 dbl > pv.list

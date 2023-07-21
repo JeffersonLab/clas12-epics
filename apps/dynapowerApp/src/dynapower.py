c6c2836e06ff4e083c0e4f2dcea9a384de1b4971
baltzell <baltzell@gmx.com>
2023-07-21 15:44:30 -0400

    final dynapower controls, with ramping

--- apps/dynapowerApp/src/dynapower.py
+++ apps/dynapowerApp/src/dynapower.py
@@ -64,42 +64,41 @@ class DynapowerMessage():
                     self.data[k] = int(match.group(v.group),16)
                 else:
                     self.data[k] = float(match.group(v.group))/10
-            except:
-                pass
+            except Exception as e:
+                print(e)
 
-    def _publish(self, match=None):
-        if match is not None:
-            self._update(match)
+    def _publish(self):
         for k,v in DynapowerMessage.pvs.items():
             try:
                 v.pv.put(self.data[k])
-            except:
-                pass
+            except Exception as e:
+                print(e)
 
     def parse_and_publish(self, byte_string):
         m = DynapowerMessage.parse(byte_string)
-        if m is None:
-            return False
-        else:
-            self._publish(m)
-            return True
+        if m is not None:
+          self._update(m)
+          self._publish()
+        return m is not None
 
 if __name__ == '__main__':
 
     cli = argparse.ArgumentParser(description='asdf')
-    cli.add_argument('-logging', help='logging level', type=str, default='WARNING', choices=['DEBUG','INFO','WARNING','CRITICAL'])
+    cli.add_argument('-logging', help='logging level', type=str, default='INFO', choices=['DEBUG','INFO','WARNING','CRITICAL'])
     cli.add_argument('-host', help='host name', type=str, default='hallb-moxa6')
     cli.add_argument('-port', help='port number', type=int, default=4009)
     args = cli.parse_args(sys.argv[1:])
 
     if args.logging == 'CRITICAL':
-        logging.basicConfig(level=logging.CRITICAL, format='%(message)s')
+      level = logging.CRITICAL
     elif args.logging == 'WARNING':
-        logging.basicConfig(level=logging.WARNING, format='%(message)s')
+      level = logging.WARNING
     elif args.logging == 'INFO':
-        logging.basicConfig(level=logging.INFO, format='%(message)s')
+      level = logging.INFO
     elif args.logging == 'DEBUG':
-        logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+      level = logging.DEBUG
+
+    logging.basicConfig(level=level, format='%(message)s')
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((args.host,args.port))
@@ -117,11 +116,10 @@ if __name__ == '__main__':
 
             if dyna.parse_and_publish(data):
 
-                if dyna.timestamp is not None:
-                    msg = 'Update: ' + str(dyna.timestamp)
-                    if dyna.previous_timestamp is not None:
-                        msg += ' ==> Period: ' + str(dyna.timestamp-dyna.previous_timestamp)
-                    logging.getLogger(__name__).info(msg)
+                msg = 'Update: ' + str(dyna.timestamp)
+                if dyna.previous_timestamp is not None:
+                    msg += ' ==> Period: ' + str(dyna.timestamp-dyna.previous_timestamp)
+                logging.getLogger(__name__).info(msg)
 
             data = b''
 

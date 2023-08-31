46286f60d99f86e0b0abe583bb0d6f29322c1deb
baltzell <baltzell@gmx.com>
2023-08-31 15:30:50 -0400

    more widgets converted, float to int for points

--- css_share/bob2opi.py
+++ css_share/bob2opi.py
@@ -18,30 +18,51 @@ widget_types = {
     'action_button':'org.csstudio.opibuilder.widgets.ActionButton',
     'meter':'org.csstudio.opibuilder.widgets.Meter',
     'label':'org.csstudio.opibuilder.widgets.Label',
-    'spinner':'org.csstudio.opibuilder.widgets.Spinner',
+    'spinner':'org.csstudio.opibuilder.widgets.spinner',
     'tank':'org.csstudio.opibuilder.widgets.tank',
     'scrollbar':'org.csstudio.opibuilder.widgets.ScrollBar',
     'choice':'org.csstudio.opibuilder.widgets.MenuButton',
-    'led':'org.csstudio.opibuilder.widgets.LED'
+    'led':'org.csstudio.opibuilder.widgets.LED',
+    'rectangle':'org.csstudio.opibuilder.widgets.Rectangle',
+    'group':'org.csstudio.opibuilder.widgets.groupingContainer',
+    'combo':'org.csstudio.opibuilder.widgets.combo',
+    'polyline':'org.csstudio.opibuilder.widgets.polyline',
+    'picture':'org.csstudio.opibuilder.widgets.Image',
+    'slide_button':'org.csstudio.opibuilder.widgets.scaledslider'
+    # don't work or untested: 
     #'stripchart':'org.csstudio.opibuilder.widgets.StripChart',
     #'byte_monitor':'org.csstudio.opibuilder.widgets.ByteMonitor',
     #'thermometer':'org.csstudio.opibuilder.widgets.Thermometer',
     #'gauge':'org.csstudio.opibuilder.widgets.Gauge',
-    #'combo':'org.csstudio.opibuilder.widgets.Combo',
 }
 
 removals = set()
 
-for widget in root.findall('widget'):
-  if widget.get('type') in widget_types:
-    widget.set('typeId',widget_types[widget.get('type')])
-  elif args.d:
-    removals.add(widget.get('type'))
-    root.remove(widget)
-  else:
-    sys.exit('Unknown widget type:  '+widget.get('type'))
+# recursive function to modify all sub-widgets:
+def munge(widget):
+  for w in widget.findall('pv_name'):
+    w.text = w.text.replace('pva://','')
+  for w in widget.findall('widget'):
+    # polyline points must be ints in cs-studio:
+    if w.get('type') == 'polyline':
+      for points in w.findall('points'):
+        for p in points.findall('point'):
+          p.set('x','%d'%float(p.get('x')))
+          p.set('y','%d'%float(p.get('y')))
+    # cs-studio requires the class name:
+    if w.get('type') in widget_types:
+      w.set('typeId',widget_types[w.get('type')])
+    # remove unknown widget types:
+    elif args.d:
+      removals.add(w.get('type'))
+      widget.remove(w)
+    else:
+      sys.exit('Unknown widget type:  '+w.get('type'))
+    munge(w)
 
-# put the encoding line back at the top:
+munge(root)
+
+# put the darn encoding line back at the top:
 for line in minidom.parseString(ET.tostring(root)).toprettyxml(indent='  ',encoding='utf-8').decode('utf-8').split('\n'):
   if len(line.strip()) != 0:
     print(line)

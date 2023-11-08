f50c0653efebb1f0a2fe60d355d3881e5412ee5a
baltzell <baltzell@gmx.com>
2023-11-08 09:07:49 -0500

    more picoammeters for urwell

--- apps/iocBoot/iocuRWell/st.cmd
+++ apps/iocBoot/iocuRWell/st.cmd
@@ -10,16 +10,33 @@ dbLoadDatabase("dbd/dynapower.dbd")
 dynapower_registerRecordDeviceDriver(pdbbase)
 
 drvAsynIPPortConfigure("SER1", "hallb-keithley2470:5025")
+drvAsynIPPortConfigure("SER2", "129.57.86.249:4001")
+drvAsynIPPortConfigure("SER3", "129.57.86.249:4002")
 
 #asynSetTraceMask("SER1",-1,0x09)
 #asynSetTraceIOMask("SER1",-1,0x03)
 asynOctetSetOutputEos("SER1",0,"\n")
 asynOctetSetInputEos("SER1",0,"\n")
 
+#asynSetTraceMask("SER2",-1,0x09)
+#asynSetTraceIOMask("SER2",-1,0x03)
+asynOctetSetOutputEos("SER2",0,"\n")
+asynOctetSetInputEos("SER2",0,"\n")
+
+#asynSetTraceMask("SER3",-1,0x09)
+#asynSetTraceIOMask("SER3",-1,0x03)
+asynOctetSetOutputEos("SER3",0,"\n")
+asynOctetSetInputEos("SER3",0,"\n")
+
 dbLoadRecords("${DEVIOCSTATS}/db/iocAdminSoft.db", "IOC=${IOC}")
 
 dbLoadRecords("db/asynRecord.db","P=B_uRWell,R=:ASYN,PORT=SER1,ADDR=1,IMAX=2000,OMAX=2000")
+dbLoadRecords("db/asynRecord.db","P=B_uRWell,R=:ASYN2,PORT=SER2,ADDR=1,IMAX=2000,OMAX=2000")
+dbLoadRecords("db/asynRecord.db","P=B_uRWell,R=:ASYN3,PORT=SER3,ADDR=1,IMAX=2000,OMAX=2000")
+
 dbLoadRecords("db/keithley2470.db","P=B_uRWell,R=:,PORT=SER1,ADDR=1")
+dbLoadRecords("db/keithley6485.db","P=B_uRWell,R=:2:,PORT=SER2,ADDR=1")
+dbLoadRecords("db/keithley6487.db","P=B_uRWell,R=:3:,PORT=SER3,ADDR=1")
 
 cd "${TOP}/iocBoot/${IOC}"
 

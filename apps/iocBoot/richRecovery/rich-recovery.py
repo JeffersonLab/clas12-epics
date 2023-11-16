6b8f6815a1fa739ab8ce590ccea1b4cec7a43fdd
baltzell <baltzell@gmx.com>
2023-11-16 13:00:36 -0500

    message on missing tiles

--- apps/iocBoot/richRecovery/rich-recovery.py
+++ apps/iocBoot/richRecovery/rich-recovery.py
@@ -243,7 +243,7 @@ def recover(alllv=False):
                         PV_CLOCK.put(0)
                     break
                 else:
-                    set_status(3,'Missing fibers, retry ...')
+                    set_status(4,'Missing fibers, retry ...')
             n_attempts += 1
             time.sleep(1)
     if not status:

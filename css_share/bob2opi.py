d07ac3e326371e5753601119c49c32e938b6a85b
baltzell <baltzell@gmx.com>
2024-02-27 09:11:05 -0500

    cleanup

--- css_share/bob2opi.py
+++ css_share/bob2opi.py
@@ -8,9 +8,6 @@ cli.add_argument('input',help='path to .bob file')
 cli.add_argument('-d',help='delete unknown widget types',default=False,action='store_true')
 args = cli.parse_args(sys.argv[1:])
 
-root = ET.parse(args.input).getroot()
-root.set('typeId','org.csstudio.opibuilder.Display')
-
 widget_types = {
     'textentry':'org.csstudio.opibuilder.widgets.TextInput',
     'textupdate':'org.csstudio.opibuilder.widgets.TextUpdate',
@@ -38,36 +35,37 @@ widget_types = {
     #'gauge':'org.csstudio.opibuilder.widgets.Gauge',
 }
 
+# recursive bob-to-opi converter:
+def munge(bob, removals):
+    # remove PVA prefix:
+    for w in bob.findall('pv_name'):
+        w.text = w.text.replace('pva://','')
+    for w in bob.findall('widget'):
+        # polyline points must be ints:
+        if w.get('type') == 'polyline':
+            for points in w.findall('points'):
+                for p in points.findall('point'):
+                    p.set('x','%d'%float(p.get('x')))
+                    p.set('y','%d'%float(p.get('y')))
+        # add the class name:
+        if w.get('type') in widget_types:
+            w.set('typeId',widget_types[w.get('type')])
+        # remove unknown widget types:
+        elif args.d:
+            removals.add(w.get('type'))
+            bob.remove(w)
+        else:
+            sys.exit('Unknown widget type:  '+w.get('type'))
+        munge(w, removals)
+
+root = ET.parse(args.input).getroot()
+root.set('typeId','org.csstudio.opibuilder.Display')
 removals = set()
+munge(root, removals)
 
-# recursive function to modify all sub-widgets:
-def munge(widget):
-  for w in widget.findall('pv_name'):
-    w.text = w.text.replace('pva://','')
-  for w in widget.findall('widget'):
-    # polyline points must be ints in cs-studio:
-    if w.get('type') == 'polyline':
-      for points in w.findall('points'):
-        for p in points.findall('point'):
-          p.set('x','%d'%float(p.get('x')))
-          p.set('y','%d'%float(p.get('y')))
-    # cs-studio requires the class name:
-    if w.get('type') in widget_types:
-      w.set('typeId',widget_types[w.get('type')])
-    # remove unknown widget types:
-    elif args.d:
-      removals.add(w.get('type'))
-      widget.remove(w)
-    else:
-      sys.exit('Unknown widget type:  '+w.get('type'))
-    munge(w)
-
-munge(root)
-
-# put the darn encoding line back at the top:
-for line in minidom.parseString(ET.tostring(root)).toprettyxml(indent='  ',encoding='utf-8').decode('utf-8').split('\n'):
-  if len(line.strip()) != 0:
-    print(line)
+# put the necessary encoding line back at the top and print:
+xml = minidom.parseString(ET.tostring(root))
+print(xml.toprettyxml(indent='  ',newl='\r',encoding='utf-8').decode('utf-8'))
 
 if args.d:
   print('\n\n<!--\nREMOVALS:\n'+'\n'.join(removals)+'\n-->')

3b1ba905dc17e10c77f4d1f123e261d2edc9fada
baltzell <baltzell@gmx.com>
2024-02-27 19:21:20 -0500

    cleanup unused stuff

--- apps/iocBoot/iocmcc/st.cmd
+++ apps/iocBoot/iocmcc/st.cmd
@@ -17,6 +17,7 @@ dbLoadRecords("db/mcc_bpm.db")
 dbLoadRecords("db/mcc_cryo.db")
 dbLoadRecords("db/mcc_tagger.db")
 dbLoadRecords("db/mcc_fsd.db")
+
 dbLoadRecords("db/hall_target.db")
 
 dbLoadTemplate("db/alarm_bpm.substitutions")
@@ -25,16 +26,6 @@ dbLoadTemplate("db/alarm_bpm.substitutions")
 dbLoadTemplate("db/hallb_ia.substitutions")
 dbLoadRecords("db/hallb_ia.db")
 
-# string parse
-#dbLoadRecords("db/mcc_locks.db")
-
-# calculate running averages:
-dbLoadTemplate("db/mcc_fsd.substitutions")
-
-# breakout FSD bits into single records:
-#dbLoadRecords("db/mcc_fsd_bits.template","P=B_FSD_2H001:,R=MASK,I=ISD2H001HALOMASKr,ZSV=NO_ALARM,OSV=MAJOR")
-#dbLoadRecords("db/mcc_fsd_bits.template","P=B_FSD_2H001:,R=TRIP,I=ISD2H001HALOTESTr,ZSV=NO_ALARM,OSV=MAJOR")
-#dbLoadRecords("db/mcc_fsd_aliases.db")
 dbLoadRecords("db/mcc_fsdGlobal.db","P=B_FSD")
 
 dbLoadRecords("db/bta_suppl.db","hall=B,ioc=classc6");
@@ -54,10 +45,4 @@ create_monitor_set("info_positions.req", 5, "P=${IOC}:")
 create_monitor_set("info_settings.req", 30, "P=${IOC}:")
 
 dbl > pv.list
-
-#epicsThreadSleep(2)
-#dbpf "B_IA_C1068_QDAC07:init.PROC", "1"
-#dbpf "B_IA_C1068_QDAC08:init.PROC", "1"
-#dbpf "B_IA_C1068_QDAC09:init.PROC", "1"
-#dbpf "B_IA_C1068_QDAC10:init.PROC", "1"
 

2416ef4d8ae62dd448b1b05ed4df27af78d16725
baltzell <baltzell@gmx.com>
2024-07-02 14:51:23 -0400

    setup krister

--- apps/iocBoot/iocplcTest/st.cmd
+++ apps/iocBoot/iocplcTest/st.cmd
@@ -1,4 +1,7 @@
 #!../../bin/linux-x86_64/mmGas
+
+# "mmGas" already includes the necessary dependencies, Modbus TCP/IP for Siemens S7
+
 < envPaths
 epicsEnvSet("IOC","iocplcTest")
 cd "${TOP}"
@@ -6,7 +9,6 @@ cd "${TOP}"
 dbLoadDatabase("dbd/mmGas.dbd")
 mmGas_registerRecordDeviceDriver(pdbbase)
 
-# bufferdewar-plc:
 drvAsynIPPortConfigure("TCP505","129.57.160.84:505",0,0,1)
 modbusInterposeConfig("TCP505",0,5000,0)
 
@@ -17,7 +19,7 @@ drvModbusAsynConfigure("R505", "TCP505", 0, 3,  0, 33, 0, 1000, "Siemens")
 drvModbusAsynConfigure("W505", "TCP505", 0, 6,  32, 1, 0, 1000, "Siemens")
 drvModbusAsynConfigure("W505A","TCP505", 0,16,  0, 33, 0, 0,    "Siemens")
 
-#dbLoadTemplate("db/mmgas_bmt.substitutions")
+dbLoadTemplate("${TOP}/iocBoot/${IOC}/bufferdewar.substitutions")
 
 cd "${TOP}/iocBoot/${IOC}"
 

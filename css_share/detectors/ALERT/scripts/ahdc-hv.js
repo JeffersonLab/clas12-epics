5f44c8a72c0beca70077727309149d802597e5c0
baltzell <baltzell@gmx.com>
2024-11-07 09:54:54 -0500

    add ahdc hv gui

---
+++ css_share/detectors/ALERT/scripts/ahdc-hv.js
@@ -0,0 +1,28 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+var novice = PVUtil.getDouble(pvs[0]);
+
+var prefix = "B_DET_AHDC_HV_";
+
+function makeWidget(pv, n)
+{
+    var lc = WidgetUtil.createWidgetModel("org.csstudio.opibuilder.widgets.linkingContainer");
+    if (novice>0) { lc.setPropertyValue("opi_file","/CLAS12_Share/apps/caenHvApp/caenhv_channel_novice-nA.opi"); }
+    else          { lc.setPropertyValue("opi_file","/CLAS12_Share/apps/caenHvApp/caenhv_channel-nA.opi"); }
+    lc.setPropertyValue("auto_size",true);
+    lc.setPropertyValue("zoom_to_fit",false);
+    lc.setPropertyValue("border_style",0);
+    lc.setPropertyValue("background_color","Header_Background");
+    lc.addMacro("C",n);
+    lc.addMacro("P",pv);
+    widget.addChildToBottom(lc);
+}
+
+
+var n=1;
+for (var s=1; s<=9; ++s) {
+    for (var w=1; w<=3; ++w) {
+        makeWidget("B_DET_AHDC_HV_S" + s + "-" + w, n++);
+    }
+}
+

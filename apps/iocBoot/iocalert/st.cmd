c3dfeced9087e8d6b8e9e8954233a2c05912a336
baltzell <baltzell@gmx.com>
2024-11-13 16:36:42 -0500

    remove test setup

--- apps/iocBoot/iocalert/st.cmd
+++ apps/iocBoot/iocalert/st.cmd
@@ -2,8 +2,6 @@
 
 < envPaths
 
-< lvCrateAddresses.env
-
 epicsEnvSet("MIBDIRS","$(DEVSNMP)/mibs:/usr/share/snmp/mibs")
 epicsEnvSet("MIBS","ALL")
 
@@ -17,25 +15,28 @@ cd "${TOP}"
 dbLoadDatabase "dbd/mpodLv.dbd"
 mpodLv_registerRecordDeviceDriver pdbbase
 
-dbLoadRecords("${DEVIOCSTATS}/db/iocAdminSoft.db","IOC=${IOC}")
-dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:")
-
 #devSnmpSetParam(DebugLevel,10)
 #MpodStatusParserDebug=1
+
+# Determine IP address:
+system 'nslookup lvatof | awk "/^Address: / {print\"epicsEnvSet(lvatof,\"\$2\")\"}" > ip.iocsh'
+< ip.iocsh
 
 # ATOF:
 dbLoadTemplate("db/atof-hvlv.substitutions")
 dbLoadTemplate("db/atof-seq.substitutions")
 dbLoadRecords("db/caenhv_genericStat.db","P=B_DET_ATOF")
 
-# AHDC:
-dbLoadTemplate("db/mmtb-lv.substitutions")
-#dbLoadTemplate("db/ahdc-lv.substitutions")
-#dbLoadTemplate("db/ahdc-intlk.substitutions")
-dbLoadRecords("db/gas_cRIO_AHDC.db")
-
-# TGT:
+# GAS:
 #dbLoadRecords("db/gas_cRIO_ALERT_TGT.db")
+dbLoadRecords("db/gas_cRIO_AHDC.db")
+dbLoadRecords("db/cRIO_heartbeat.db","P=B_HW_,R=CRIO_ALERT_,DLY=60")
+
+# Note, AHDC HV and LV are in separate IOCs (ioccaenhv_HVMVT and iocmvtlv)
+
+# Standard IOC stuff:
+dbLoadRecords("${DEVIOCSTATS}/db/iocAdminSoft.db","IOC=${IOC}")
+dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:")
 
 cd "${TOP}/iocBoot/${IOC}"
 

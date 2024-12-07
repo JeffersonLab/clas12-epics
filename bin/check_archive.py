76c8ca556a59409cca69ec5fe210e79d5197fd83
baltzell <baltzell@gmx.com>
2024-12-07 16:07:26 -0500

    2to3

--- bin/check_archive.py
+++ bin/check_archive.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Lists PVs that are NOT currently being archived.
 #
@@ -53,22 +53,22 @@ else:
             arg += "*.txt"
             files = glob.glob(arg)
         else:
-            print "Warning: " + arg + " not found, assuming it's a pv"
+            print("Warning: " + arg + " not found, assuming it's a pv")
             clipvs.append(arg)
 
 if len(files) == 0:
-    print "No files to be processed"
+    print("No files to be processed")
 
 else:
-    print "Processing files:"
+    print("Processing files:")
     files.sort(key=str.lower)
 
     for file in files:
         if os.stat(file).st_size == 0:
-            print "Warning: empty file, skipping " + file
+            print("Warning: empty file, skipping " + file)
             continue
 
-        print "  " + file
+        print("  " + file)
         pvlist = open(file, 'r')
         for pv in pvlist:
             pv = pv.split(' ', 1)[0]
@@ -84,44 +84,44 @@ else:
 
                 else:
                     p = subprocess.check_output(["archive", pv])
-                    if "not archived" in p:
+                    if "not archived" in str(p):
                         not_archived.append(pv)
                     else:
                         is_archived.append(pv)
 
 for pv in clipvs:
     p = subprocess.check_output(["archive", pv])
-    if "not archived" in p:
+    if "not archived" in str(p):
         not_archived.append(pv)
     else:
         is_archived.append(pv)
 
 if printDisconnected:
     if len(is_disconnected) == 0:
-        print "All PVs are connected"
+        print("All PVs are connected")
         exit(0)
     else:
-        print "PVs are disconnected:"
-        print '\n'.join(is_disconnected)
+        print("PVs are disconnected:")
+        print('\n'.join(is_disconnected))
         exit(1)
 
 elif printMissing:
     if len(not_archived) == 0:
-        print "All PVs are archived"
+        print("All PVs are archived")
         exit(0)
     else:
-        print "PVs not archived:"
+        print("PVs not archived:")
         not_archived.sort()
-        print '\n'.join(not_archived)
+        print('\n'.join(not_archived))
         exit(1)
 else:
     if len(is_archived) == 0:
-        print "No PVs are archived"
+        print("No PVs are archived")
         exit(0)
     else:
-        print "PVs are archived:"
+        print("PVs are archived:")
         is_archived.sort()
-        print '\n'.join(is_archived)
+        print('\n'.join(is_archived))
         exit(1)
 
 

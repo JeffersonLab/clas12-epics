049a1dab22342ffbbaf7ed89ba87960ae37df4c6
baltzell <baltzell@gmx.com>
2025-02-03 12:10:35 -0500

    add waveform

--- apps/iocBoot/iochydra/st.cmd
+++ apps/iocBoot/iochydra/st.cmd
@@ -1,18 +1,18 @@
-#!../../bin/linux-x86_64/monitor
+#!../../bin/linux-x86_64/amq
 
 < envPaths
 
 cd ${TOP}
 
 ## Register all support components
-dbLoadDatabase "dbd/monitor.dbd"
-monitor_registerRecordDeviceDriver pdbbase
+dbLoadDatabase "dbd/amq.dbd"
+amq_registerRecordDeviceDriver pdbbase
 
 dbLoadRecords("db/iocAdminSoft.db","IOC=${IOC}")
 dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:")
 
+dbLoadRecords("db/waveformApp.db","P=HB:monitoring:ai:,R=,FNAME=./waveform.txt,PERIOD=10,NELM=87,FTVL=DOUBLE")
 dbLoadTemplate("db/hydra.substitutions")
-dbLoadRecords("db/hydra-sum.db")
 
 cd ${TOP}/iocBoot/${IOC}
 
@@ -23,6 +23,8 @@ iocInit();
 makeAutosaveFiles()
 create_monitor_set("info_positions.req", 5, "P=${IOC}:")
 create_monitor_set("info_settings.req", 30, "P=${IOC}:")
+
+seq waveform, "P=HB:monitoring:ai:,R="
 
 dbl > pv.list
 

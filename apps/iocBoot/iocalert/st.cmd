e7212ca015ee307415374be1ea907589653dffcb
baltzell <baltzell@gmx.com>
2025-03-18 11:03:58 -0400

    order matters

--- apps/iocBoot/iocalert/st.cmd
+++ apps/iocBoot/iocalert/st.cmd
@@ -34,8 +34,8 @@ dbLoadRecords("db/caenhv_genericStat.db","P=B_DET_ATOF")
 # GAS:
 dbLoadRecords("db/gas_cRIO_BONUS.db")
 dbLoadRecords("db/gas_cRIO_AHDC.db")
-dbLoadRecords("db/gas_cRIO_ALERT_alias.db")
 dbLoadRecords("db/cRIO_heartbeat_bi.db","P=B_HW_,R=CRIO_BONUS_,DLY=60")
+dbLoadRecords("db/gas_cRIO_ALERT_alias.db")
 
 # Note, AHDC is sharing hardware with MVT, in separate IOCs:
 # ioccaenhv_HVMVT, iocmvtlv, iocmmfeuMVT

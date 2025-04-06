616b890237014335067b1c09688dc8d4a2264b32
baltzell <baltzell@gmx.com>
2025-04-06 16:32:29 -0400

    override environment

--- tools/harp_fitter/harp_fitter.sh
+++ tools/harp_fitter/harp_fitter.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export JAVA_HOME=/usr/clas12/offline/jdk/21.0.2
+export PATH=$JAVA_HOME/bin:$PATH
 motorName=$1
 dataDir=/home/epics/DATA/HARP_SCANS
 lastFile=`ls -lht $dataDir/$motorName | grep $motorName | awk '{print$9}' | head -n1`

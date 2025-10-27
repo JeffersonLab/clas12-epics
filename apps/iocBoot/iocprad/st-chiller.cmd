a1fcfa679f38fb11a5a95152b50acbc8689ac977
baltzell <baltzell@gmx.com>
2025-10-27 18:33:50 -0400

    basic modbus comms with HyCal chiller working

--- apps/iocBoot/iocprad/st-chiller.cmd
+++ apps/iocBoot/iocprad/st-chiller.cmd
@@ -7,9 +7,11 @@ cd ${TOP}
 dbLoadDatabase "dbd/c370.dbd"
 c370_registerRecordDeviceDriver pdbbase
 
-drvAsynIPPortConfigure("ETH1","hallb-moxa8:4001",0,0,1)
+drvAsynIPPortConfigure("ETH1","hallb-moxa8:4001",0,0,0)
 #asynSetTraceMask("ETH1",-1,0x09)
 #asynSetTraceIOMask("ETH1",-1,0x02)
+
+#dbLoadRecords("db/asynRecord.db","PORT=ETH1,ADDR=1,IMAX=2000,OMAX=2000,P=hycal:chiller:,R=asyn")
 
 # modbusInterposeConfig(portName, linkType, timeoutMsec, writeDelayMsec)
 #   linkType = 0/1/2 = TCPIP/RTU/ASCII
@@ -19,28 +21,41 @@ modbusInterposeConfig("ETH1",1,1000,0)
 #  "portName", "tcpPortName", slaveAddress, modbusFunction,
 #  modbusStartAddress, modbusLength, dataType, pollMsec,"plcType")
 
-# 32-bit floats:
-#drvModbusAsynConfigure("C370x0600","ETH1", 4, 3, 0x0600, 40, 0, 2000, "C370")
+drvModbusAsynConfigure("AIx000","ETH1", 4, 3, 0, 100, 0, 2000, "DeltaT")
+drvModbusAsynConfigure("AIx900","ETH1", 4, 3, 900, 100, 0, 2000, "DeltaT")
 
-# bits:
-drvModbusAsynConfigure("C370x1880","ETH1", 4, 1, 0x0, 128, 0, 2000, "C370")
+drvModbusAsynConfigure("BIx000","ETH1", 4, 1, 0x0, 1000, 0, 2000, "DeltaT")
 
-dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db", "IOC=$(IOC)")
-dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:")
+dbLoadRecords("db/modbus-ai.db","P=hycal:chiller:,R=temp:set:rbk, PORT=AIx000,OFFSET=0")
+dbLoadRecords("db/modbus-ai.db","P=hycal:chiller:,R=temp:act,     PORT=AIx000,OFFSET=30")
+dbLoadRecords("db/modbus-ai.db","P=hycal:chiller:,R=temp:out,     PORT=AIx000,OFFSET=82")
+
+dbLoadRecords("db/modbus-ai.db","P=hycal:chiller:,R=dewpoint,     PORT=AIx900,OFFSET=12")
+dbLoadRecords("db/modbus-ai.db","P=hycal:chiller:,R=ambient,      PORT=AIx900,OFFSET=14")
+dbLoadRecords("db/modbus-ai.db","P=hycal:chiller:,R=temp:in,      PORT=AIx900,OFFSET=16")
+dbLoadRecords("db/modbus-ai.db","P=hycal:chiller:,R=set:ctrl,     PORT=AIx900,OFFSET=30")
+dbLoadRecords("db/modbus-ai.db","P=hycal:chiller:,R=start:ctrl,   PORT=AIx900,OFFSET=31")
+dbLoadRecords("db/modbus-ai.db","P=hycal:chiller:,R=cool:demand,  PORT=AIx900,OFFSET=10")
+dbLoadRecords("db/modbus-ai.db","P=hycal:chiller:,R=tank:level,   PORT=AIx900,OFFSET=26")
+
+#dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db", "IOC=$(IOC)")
+#dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:")
+
+#dbLoadTemplate("db/prad-chiller.substitutions")
 
 cd ${TOP}/iocBoot/${IOC}
 
-< save_restore.cmd
+#< save_restore.cmd
 
-asSetFilename("../acf/cas.acf")
+#asSetFilename("../acf/cas.acf")
 
 iocInit()
 
-caPutLogInit("clonioc1:7011")
+#caPutLogInit("clonioc1:7011")
 
-makeAutosaveFiles()
-create_monitor_set("info_positions.req","","")
-create_monitor_set("info_settings.req","","")
+#makeAutosaveFiles()
+#create_monitor_set("info_positions.req","","")
+#create_monitor_set("info_settings.req","","")
 
 dbl > pv.list
 

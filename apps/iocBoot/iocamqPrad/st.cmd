a3a3acfcdb6b6b079b0cd23f790a1d0c491e6e33
baltzell <baltzell@gmx.com>
2026-03-28 17:02:48 -0400

    add hycal scalers ioc

---
+++ apps/iocBoot/iocamqPrad/st.cmd
@@ -0,0 +1,27 @@
+#!../../bin/linux-x86_64/amq
+< envPaths
+cd ${TOP}
+dbLoadDatabase "dbd/amq.dbd"
+amq_registerRecordDeviceDriver pdbbase
+
+ConnectMQ("tcp://clon00:61616","clasrun.clasprod.scalers.fadc")
+
+dbLoadTemplate("db/amq-hycal.substitutions")
+dbLoadRecords("db/amq-hycal-aliases.db")
+
+dbLoadRecords("db/waveformApp.db","P=B_HW_HYCAL_FADC,R=:c:,NELM=2016,FTVL=FLOAT,PERIOD=5,FNAME=hw-wf.txt")
+#dbLoadRecords("db/waveformApp.db","P=B_DET_HYCAL_FADC,R=:c:,NELM=20,FTVL=FLOAT,PERIOD=5,FNAME=wf.txt")
+
+dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db","IOC=$(IOC)")
+
+cd ${TOP}/iocBoot/${IOC}
+
+iocInit
+
+StartMQ()
+
+dbl > pv.list
+
+seq waveform, "P=B_HW_HYCAL_FADC,R=:c:"
+#seq waveform, "P=B_DET_HYCAL_FADC,R=:c:"
+

70af8fe78100873148bc9055a1e1bf3aa42f6156
baltzell <baltzell@gmx.com>
2026-03-30 16:02:44 -0400

    move prad cRio to final IOC location

--- apps/iocBoot/iocprad/st.cmd
+++ apps/iocBoot/iocprad/st.cmd
@@ -10,6 +10,8 @@ ioccaen_registerRecordDeviceDriver(pdbbase)
 
 dbLoadRecords("db/crio-hycal-temps.db")
 dbLoadRecords("db/crio-hycal-filter.db")
+dbLoadRecords("db/prad-target.db")
+
 dbLoadRecords("db/cRIO_heartbeat.db","P=B_HW_,R=CRIO_PRAD_,DLY=60")
 
 cd ${TOP}/iocBoot/${IOC}

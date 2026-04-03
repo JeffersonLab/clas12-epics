0f9c655c5e00ba7a4e1b3854bfe06a3fda283f49
baltzell <baltzell@gmx.com>
2026-04-03 13:52:15 -0400

    PRAD trigger scalers

--- apps/iocBoot/iocamqPrad/st.cmd
+++ apps/iocBoot/iocamqPrad/st.cmd
@@ -6,8 +6,13 @@ amq_registerRecordDeviceDriver pdbbase
 
 ConnectMQ("tcp://clon00:61616","clasrun.clasprod.daq.HallB_DAQ")
 
-dbLoadRecords("db/amqDoubleArray.db","P=B_DAQ:PRAD:triggers,K=SSPPRAD_SLOT9_TRIGGERBITS,N=32,TH=0,THH=30,HSV=NO_ALARM,HHSV=MAJOR")
-dbLoadRecords("db/amqDoubleArray.db","P=B_DAQ:PRAD:prescales,K=SSPPRAD_SLOT9_PRESCALES,N=32,TH=0,THH=30,HSV=NO_ALARM,HHSV=MAJOR")
+dbLoadRecords("db/amqDoubleArray.db","P=B_DAQ_PRAD:triggers,K=SSPPRAD_SLOT9_TRIGGERBITS,N=32,TH=0,THH=30,HSV=NO_ALARM,HHSV=MAJOR")
+dbLoadRecords("db/amqDoubleArray.db","P=B_DAQ_PRAD:prescales,K=SSPPRAD_SLOT9_PRESCALES,N=32,TH=0,THH=30,HSV=NO_ALARM,HHSV=MAJOR")
+
+dbLoadRecords("db/array-to-scalar-8.template","INP=B_DAQ_PRAD:triggers,P=B_DAQ_PRAD:,R=trigger:,S=")
+dbLoadRecords("db/array-to-scalar-9.template","INP=B_DAQ_PRAD:triggers,P=B_DAQ_PRAD:,R=prescale:,S=")
+
+dbLoadTemplate("db/amq-prad-trigger.substitutions")
 
 dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db","IOC=$(IOC)")
 

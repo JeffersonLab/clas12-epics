bebf702c3fecaf0f312d5fc0bc3c195d5f268945
baltzell <baltzell@gmx.com>
2026-04-06 21:06:45 -0400

    add autosave for PRAD cRio/LabView IOCs

--- apps/iocBoot/iocprad/st.cmd
+++ apps/iocBoot/iocprad/st.cmd
@@ -9,6 +9,7 @@ dbLoadDatabase("dbd/ioccaen.dbd")
 ioccaen_registerRecordDeviceDriver(pdbbase)
 
 dbLoadRecords("${DEVIOCSTATS}/db/iocAdminSoft.db","IOC=${IOC}")
+dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:")
 
 dbLoadRecords("db/crio-hycal-temps.db")
 dbLoadRecords("db/crio-hycal-filter.db")
@@ -18,5 +19,13 @@ dbLoadRecords("db/cRIO_heartbeat.db","P=B_HW_,R=CRIO_PRAD_,DLY=60")
 
 cd ${TOP}/iocBoot/${IOC}
 
+< save_restore.cmd
+
 iocInit()
+
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5, "P=${IOC}:")
+create_monitor_set("info_settings.req", 30, "P=${IOC}:")
+
+dbl > pv.list
 
